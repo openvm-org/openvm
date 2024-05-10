1d1dbaf828b1289445170a84918e84458d443839
Osama Alkhodairy <osama.khodairy@hotmail.com>
2024-05-10 15:10:32 -0400

    wip

--- chips/src/lib.rs
+++ chips/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod list;
 /// Chip to range check a value has less than a fixed number of bits
 pub mod range;
--- chips/src/range/mod.rs
+++ chips/src/range/mod.rs
@@ -5,13 +5,14 @@ pub mod chip;
 pub mod columns;
 pub mod trace;
 
-use std::collections::BTreeMap;
+use std::sync::Arc;
+use std::{collections::BTreeMap, sync::atomic::AtomicU32};
 
 #[derive(Default)]
 pub struct RangeCheckerChip<const MAX: u32> {
     /// The index for the Range Checker bus.
     bus_index: usize,
-    pub count: BTreeMap<u32, u32>,
+    pub count: BTreeMap<u32, Arc<AtomicU32>>,
 }
 
 impl<const MAX: u32> RangeCheckerChip<MAX> {
@@ -24,5 +25,12 @@ impl<const MAX: u32> RangeCheckerChip<MAX> {
 
     pub fn bus_index(&self) -> usize {
         self.bus_index
+    }
+
+    pub fn add_count(&mut self, val: u32) {
+        self.count
+            .entry(val)
+            .or_insert_with(|| Arc::new(AtomicU32::new(0)))
+            .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
     }
 }
