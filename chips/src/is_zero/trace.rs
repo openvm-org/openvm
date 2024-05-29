d722a0e66186d5dadc0b9e8b2cd0a01c9df01f5c
Alex Zhao <alexzhao@intrinsictech.com>
2024-05-29 14:43:40 -0400

    adjusting is_zero to follow guidelines: request, vector, etc

--- chips/src/is_zero/trace.rs
+++ chips/src/is_zero/trace.rs
@@ -1,12 +1,24 @@
+use super::columns::NUM_COLS;
+use super::IsZeroChip;
 use p3_field::PrimeField32;
-use p3_matrix::dense::RowMajorMatrix;
+use p3_matrix::dense::RowMajorMatrix; // Import the constant from columns.rs
 
-const NUM_IS_ZERO_COLS: usize = 3; // Assuming a constant value for the number of columns
+impl IsZeroChip {
+    pub fn generate_trace_rows<F: PrimeField32>(&self) -> RowMajorMatrix<F> {
+        let rows = self
+            .x
+            .iter()
+            .map(|x| {
+                let answer = if *x == 0 { 1 } else { 0 };
+                let inv = F::from_canonical_u32(*x + answer).inverse();
+                vec![
+                    F::from_canonical_u32(*x),
+                    F::from_canonical_u32(answer),
+                    inv,
+                ]
+            })
+            .collect::<Vec<_>>();
 
-pub fn generate_trace_rows<F: PrimeField32>(x: u32) -> RowMajorMatrix<F> {
-    let answer = if x == 0 { 1 } else { 0 };
-    let inv = F::from_canonical_u32(x + answer).inverse();
-    let rows = vec![F::from_canonical_u32(x), F::from_canonical_u32(answer), inv];
-
-    RowMajorMatrix::new(rows, NUM_IS_ZERO_COLS)
+        RowMajorMatrix::new(rows.concat(), NUM_COLS)
+    }
 }
