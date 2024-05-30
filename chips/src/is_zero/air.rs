399261f1995057fa03810791b7f9f9c5aaa5d94c
Alex Zhao <alexzhao@intrinsictech.com>
2024-05-30 13:40:20 -0400

    remove pi

--- chips/src/is_zero/air.rs
+++ chips/src/is_zero/air.rs
@@ -16,16 +16,9 @@ impl<F: Field> BaseAir<F> for IsZeroChip {
 impl<AB: AirBuilderWithPublicValues> Air<AB> for IsZeroChip {
     fn eval(&self, builder: &mut AB) {
         let main = builder.main();
-        let pis = builder.public_values();
-
-        let x = pis[0];
-        let is_zero = pis[1];
 
         let local = main.row_slice(0);
         let local: &IsZeroCols<AB::Var> = (*local).borrow();
-
-        builder.assert_eq(local.x, x);
-        builder.assert_eq(local.is_zero, is_zero);
 
         builder.assert_eq((local.x + local.is_zero) * local.inv, AB::F::one());
         builder.assert_eq(local.is_zero * local.is_zero, local.is_zero);
--- chips/src/is_zero/tests.rs
+++ chips/src/is_zero/tests.rs
@@ -1,59 +1,59 @@
 use super::IsZeroChip;
 // use crate::is_zero::air::IsZeroChip;
 use p3_baby_bear::BabyBear;
-use p3_field::AbstractField;
 
 use afs_stark_backend::prover::USE_DEBUG_BUILDER;
 use afs_stark_backend::verifier::VerificationError;
-use afs_test_utils::config::baby_bear_poseidon2::run_simple_test;
+use afs_test_utils::config::baby_bear_poseidon2::run_simple_test_no_pis;
+use p3_field::AbstractField;
 
 #[test]
 fn test_single_is_zero() {
     // Public inputs:
     let x = 97u32;
-    let is_zero = 0u32;
 
     type Val = BabyBear;
-    let pis = [x, is_zero].map(BabyBear::from_canonical_u32).to_vec();
     let chip = IsZeroChip::new(vec![x]); // Create an instance of IsZeroChip
 
     let trace = chip.generate_trace::<Val>(); // Use the instance
 
-    run_simple_test(vec![&chip], vec![trace], vec![pis]).expect("Verification failed");
+    assert_eq!(trace.values[1], AbstractField::from_canonical_u32(0));
+
+    run_simple_test_no_pis(vec![&chip], vec![trace]).expect("Verification failed");
 }
 
 #[test]
 fn test_single_is_zero2() {
     // Public inputs:
     let x = 0u32;
-    let is_zero = 1u32;
 
     type Val = BabyBear;
-    let pis = [x, is_zero].map(BabyBear::from_canonical_u32).to_vec();
     let chip = IsZeroChip::new(vec![x]); // Create an instance of IsZeroChip
 
     let trace = chip.generate_trace::<Val>(); // Use the instance
 
-    run_simple_test(vec![&chip], vec![trace], vec![pis]).expect("Verification failed");
+    assert_eq!(trace.values[1], AbstractField::from_canonical_u32(1));
+
+    run_simple_test_no_pis(vec![&chip], vec![trace]).expect("Verification failed");
 }
 
 #[test]
 fn test_single_is_zero_fail() {
     // Public inputs:
     let x = 187u32;
-    let is_zero = 1u32;
 
     type Val = BabyBear;
-    let pis = [x, is_zero].map(BabyBear::from_canonical_u32).to_vec();
+
     let chip = IsZeroChip::new(vec![x]); // Create an instance of IsZeroChip
 
-    let trace = chip.generate_trace::<Val>(); // Use the instance
+    let mut trace = chip.generate_trace::<Val>(); // Use the instance
+    trace.values[1] = AbstractField::from_canonical_u32(1);
 
     USE_DEBUG_BUILDER.with(|debug| {
         *debug.lock().unwrap() = false;
     });
     assert_eq!(
-        run_simple_test(vec![&chip], vec![trace], vec![pis]),
+        run_simple_test_no_pis(vec![&chip], vec![trace]),
         Err(VerificationError::OodEvaluationMismatch),
         "Expected constraint to fail"
     );
@@ -63,19 +63,19 @@ fn test_single_is_zero_fail() {
 fn test_single_is_zero_fail2() {
     // Public inputs:
     let x = 0u32;
-    let is_zero = 0u32;
 
     type Val = BabyBear;
-    let pis = [x, is_zero].map(BabyBear::from_canonical_u32).to_vec();
+
     let chip = IsZeroChip::new(vec![x]); // Create an instance of IsZeroChip
 
-    let trace = chip.generate_trace::<Val>(); // Use the instance
+    let mut trace = chip.generate_trace::<Val>(); // Use the instance
+    trace.values[1] = AbstractField::from_canonical_u32(0);
 
     USE_DEBUG_BUILDER.with(|debug| {
         *debug.lock().unwrap() = false;
     });
     assert_eq!(
-        run_simple_test(vec![&chip], vec![trace], vec![pis]),
+        run_simple_test_no_pis(vec![&chip], vec![trace]),
         Err(VerificationError::OodEvaluationMismatch),
         "Expected constraint to fail"
     );
