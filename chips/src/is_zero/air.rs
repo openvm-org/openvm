dead2962c4c3f4e0ed704191e4ec866b18bb138c
Alex Zhao <alexzhao@intrinsictech.com>
2024-05-30 13:58:36 -0400

    added subair eval for iszerochip

--- chips/src/is_zero/air.rs
+++ chips/src/is_zero/air.rs
@@ -2,7 +2,8 @@ use std::borrow::Borrow;
 
 use super::columns::{IsZeroCols, NUM_COLS};
 use super::IsZeroChip;
-use p3_air::{Air, AirBuilderWithPublicValues, BaseAir};
+use crate::sub_chip::SubAir;
+use p3_air::{Air, AirBuilder, AirBuilderWithPublicValues, BaseAir};
 use p3_field::AbstractField;
 use p3_field::Field;
 use p3_matrix::Matrix;
@@ -18,9 +19,20 @@ impl<AB: AirBuilderWithPublicValues> Air<AB> for IsZeroChip {
         let main = builder.main();
 
         let local = main.row_slice(0);
-        let local: &IsZeroCols<AB::Var> = (*local).borrow();
+        let local: &[AB::Var] = (*local).borrow();
 
-        builder.assert_eq(local.x * local.is_zero, AB::F::zero());
-        builder.assert_eq(local.is_zero + local.x * local.inv, AB::F::one());
+        let is_zero_cols = IsZeroCols::from_slice(local);
+
+        SubAir::<AB>::eval(self, builder, is_zero_cols);
+    }
+}
+
+impl<AB: AirBuilder> SubAir<AB> for IsZeroChip {
+    fn eval(&self, builder: &mut AB, is_zero_cols: Self::Cols<AB::Var>) {
+        builder.assert_eq(is_zero_cols.x * is_zero_cols.is_zero, AB::F::zero());
+        builder.assert_eq(
+            is_zero_cols.is_zero + is_zero_cols.x * is_zero_cols.inv,
+            AB::F::one(),
+        );
     }
 }
--- chips/src/is_zero/columns.rs
+++ chips/src/is_zero/columns.rs
@@ -10,9 +10,17 @@ pub struct IsZeroCols<F> {
     pub inv: F,
 }
 
-impl<F> IsZeroCols<F> {
+impl<F: Clone> IsZeroCols<F> {
     pub const fn new(x: F, is_zero: F, inv: F) -> IsZeroCols<F> {
         IsZeroCols { x, is_zero, inv }
+    }
+
+    pub fn from_slice(slc: &[F]) -> IsZeroCols<F> {
+        IsZeroCols {
+            x: slc[0].clone(),
+            is_zero: slc[1].clone(),
+            inv: slc[2].clone(),
+        }
     }
 
     pub fn get_width() -> usize {
