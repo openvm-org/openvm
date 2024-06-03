a40f8cbdab97bc63d073c87e2d0c670cafc19336
Alex Zhao <alexzhao@intrinsictech.com>
2024-06-03 10:36:14 -0400

    feat: vector tests for is_zero

--- chips/src/is_zero/tests.rs
+++ chips/src/is_zero/tests.rs
@@ -20,13 +20,19 @@ fn test_single_is_zero() {
 
 #[test]
 fn test_single_is_zero2() {
-    let x = AbstractField::from_canonical_u32(0);
+    let x_vec = [0, 1, 2, 7]
+        .iter()
+        .map(|x| AbstractField::from_canonical_u32(*x))
+        .collect();
 
     let chip = IsZeroChip {};
 
-    let trace = chip.generate_trace(vec![x]);
+    let trace = chip.generate_trace(x_vec);
 
     assert_eq!(trace.values[1], AbstractField::from_canonical_u32(1));
+    assert_eq!(trace.values[4], AbstractField::from_canonical_u32(0));
+    assert_eq!(trace.values[7], AbstractField::from_canonical_u32(0));
+    assert_eq!(trace.values[10], AbstractField::from_canonical_u32(0));
 
     run_simple_test_no_pis(vec![&chip], vec![trace]).expect("Verification failed");
 }
@@ -52,18 +58,33 @@ fn test_single_is_zero_fail() {
 
 #[test]
 fn test_single_is_zero_fail2() {
-    let x = AbstractField::from_canonical_u32(0);
+    let x_vec = [1, 2, 7, 0]
+        .iter()
+        .map(|x| AbstractField::from_canonical_u32(*x))
+        .collect();
 
     let chip = IsZeroChip {};
 
-    let mut trace = chip.generate_trace(vec![x]);
-    trace.values[1] = AbstractField::from_canonical_u32(0);
+    let mut trace = chip.generate_trace(x_vec);
+    trace.values[1] = AbstractField::from_canonical_u32(1);
 
     USE_DEBUG_BUILDER.with(|debug| {
         *debug.lock().unwrap() = false;
     });
     assert_eq!(
-        run_simple_test_no_pis(vec![&chip], vec![trace]),
+        run_simple_test_no_pis(vec![&chip], vec![trace.clone()]),
+        Err(VerificationError::OodEvaluationMismatch),
+        "Expected constraint to fail"
+    );
+
+    trace.values[1] = AbstractField::from_canonical_u32(0);
+    trace.values[10] = AbstractField::from_canonical_u32(0);
+
+    USE_DEBUG_BUILDER.with(|debug| {
+        *debug.lock().unwrap() = false;
+    });
+    assert_eq!(
+        run_simple_test_no_pis(vec![&chip], vec![trace.clone()]),
         Err(VerificationError::OodEvaluationMismatch),
         "Expected constraint to fail"
     );
