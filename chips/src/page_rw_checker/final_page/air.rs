fcb762be725dfb902d737eeb65f5b11abbadfd6e
Alex Zhao <alexzhao@intrinsictech.com>
2024-06-13 11:33:40 -0400

    merged final_page from PR #45 (page r/w checker)

---
+++ chips/src/page_rw_checker/final_page/air.rs
@@ -0,0 +1,130 @@
+use afs_stark_backend::air_builders::PartitionedAirBuilder;
+use p3_air::{Air, AirBuilder, BaseAir};
+use p3_field::{AbstractField, Field};
+use p3_matrix::Matrix;
+
+use super::{
+    columns::{FinalPageAuxCols, FinalPageCols},
+    FinalPageAir,
+};
+use crate::{
+    is_less_than_tuple::{
+        columns::{IsLessThanTupleCols, IsLessThanTupleIOCols},
+        IsLessThanTupleAir,
+    },
+    page_rw_checker::page::columns::PageCols,
+    sub_chip::{AirConfig, SubAir},
+};
+
+impl<F: Field> BaseAir<F> for FinalPageAir {
+    fn width(&self) -> usize {
+        self.air_width()
+    }
+}
+
+impl AirConfig for FinalPageAir {
+    type Cols<T> = FinalPageCols<T>;
+}
+
+impl<AB: PartitionedAirBuilder> Air<AB> for FinalPageAir
+where
+    AB::M: Clone,
+{
+    fn eval(&self, builder: &mut AB) {
+        let page_trace: &<AB as AirBuilder>::M = &builder.partitioned_main()[0].clone();
+        let aux_trace: &<AB as AirBuilder>::M = &builder.partitioned_main()[1].clone();
+
+        let (page_local, page_next) = (page_trace.row_slice(0), page_trace.row_slice(1));
+
+        let page_local_cols =
+            PageCols::<AB::Var>::from_slice(&page_local, self.idx_len, self.data_len);
+        let page_next_cols =
+            PageCols::<AB::Var>::from_slice(&page_next, self.idx_len, self.data_len);
+
+        // The auxiliary columns to compare local index and next index are stored in the next row
+        let (aux_local, aux_next) = (aux_trace.row_slice(0), aux_trace.row_slice(1));
+
+        let aux_local_cols = FinalPageAuxCols::from_slice(
+            &aux_local,
+            self.idx_limb_bits,
+            self.idx_decomp,
+            self.idx_len,
+        );
+
+        let aux_next_cols = FinalPageAuxCols::from_slice(
+            &aux_next,
+            self.idx_limb_bits,
+            self.idx_decomp,
+            self.idx_len,
+        );
+
+        SubAir::eval(
+            self,
+            builder,
+            [page_local_cols, page_next_cols],
+            [aux_local_cols, aux_next_cols],
+        );
+    }
+}
+
+impl<AB: AirBuilder> SubAir<AB> for FinalPageAir {
+    type IoView = [PageCols<AB::Var>; 2];
+    type AuxView = [FinalPageAuxCols<AB::Var>; 2];
+
+    /// Ensuring the page is in the proper format: allocated rows come first
+    /// and are sorted by idx, which are distinct
+    fn eval(&self, builder: &mut AB, io: Self::IoView, aux: Self::AuxView) {
+        let (page_local, page_next) = (&io[0], &io[1]);
+        let (aux_local, aux_next) = (&aux[0], &aux[1]);
+
+        // Helpers
+        let or = |a: AB::Expr, b: AB::Expr| a.clone() + b.clone() - a * b;
+        let implies = |a: AB::Expr, b: AB::Expr| or(AB::Expr::one() - a, b);
+
+        // Ensuring rcv_mult is always 0, 1, or 3
+        builder.assert_zero(
+            aux_local.rcv_mult
+                * (aux_local.rcv_mult - AB::Expr::one())
+                * (aux_local.rcv_mult - AB::Expr::from_canonical_u8(3)),
+        );
+
+        // Ensuring that is_alloc is always bool
+        builder.assert_bool(page_local.is_alloc);
+
+        // Ensuring that all unallocated rows are at the bottom
+        builder.when_transition().assert_one(implies(
+            page_next.is_alloc.into(),
+            page_local.is_alloc.into(),
+        ));
+
+        // Ensuring that rows are sorted by (1-is_alloc, idx)
+        let lt_cols = IsLessThanTupleCols {
+            io: IsLessThanTupleIOCols {
+                x: page_local.idx.clone(),
+                y: page_next.idx.clone(),
+                tuple_less_than: aux_next.lt_out,
+            },
+            aux: aux_next.lt_cols.clone(),
+        };
+
+        let lt_air = IsLessThanTupleAir::new(
+            self.range_bus_index,
+            1 << self.idx_limb_bits,
+            vec![self.idx_limb_bits; self.idx_len],
+            self.idx_decomp,
+        );
+
+        SubAir::eval(
+            &lt_air,
+            &mut builder.when_transition(),
+            lt_cols.io,
+            lt_cols.aux,
+        );
+
+        // Ensuring the keys are strictly sorted (for allocated rows)
+        builder.when_transition().assert_one(or(
+            AB::Expr::one() - page_next.is_alloc,
+            aux_next.lt_out.into(),
+        ));
+    }
+}
---
+++ chips/src/page_rw_checker/final_page/mod.rs
@@ -0,0 +1,56 @@
+use crate::is_less_than_tuple::columns::IsLessThanTupleAuxCols;
+
+pub mod air;
+pub mod bridge;
+pub mod columns;
+pub mod trace;
+
+#[cfg(test)]
+pub mod tests;
+
+pub struct FinalPageAir {
+    page_bus_index: usize,
+    range_bus_index: usize,
+
+    idx_len: usize,
+    data_len: usize,
+
+    idx_limb_bits: usize,
+    idx_decomp: usize,
+}
+
+impl FinalPageAir {
+    pub fn new(
+        page_bus_index: usize,
+        range_bus_index: usize,
+        idx_len: usize,
+        data_len: usize,
+        idx_limb_bits: usize,
+        idx_decomp: usize,
+    ) -> Self {
+        Self {
+            page_bus_index,
+            range_bus_index,
+            idx_len,
+            data_len,
+            idx_limb_bits,
+            idx_decomp,
+        }
+    }
+
+    pub fn page_width(&self) -> usize {
+        1 + self.idx_len + self.data_len
+    }
+
+    pub fn aux_width(&self) -> usize {
+        IsLessThanTupleAuxCols::<usize>::get_width(
+            vec![self.idx_limb_bits; self.idx_len],
+            self.idx_decomp,
+            self.idx_len,
+        ) + 2
+    }
+
+    pub fn air_width(&self) -> usize {
+        self.page_width() + self.aux_width()
+    }
+}
