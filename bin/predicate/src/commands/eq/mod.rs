d7b37914feced06d4b3fdd59fc888eee2e650109
Yu Jiang Tham <yoojbruin@gmail.com>
2024-06-13 18:25:36 -0400

    Predicate

---
+++ bin/predicate/src/commands/eq/mod.rs
@@ -0,0 +1,42 @@
+use clap::Parser;
+use color_eyre::eyre::Result;
+
+#[derive(Debug, Parser)]
+pub struct EqCommand {
+    #[arg(
+        long = "table-id",
+        short = 't',
+        help = "The table id to run the predicate on",
+        required = true
+    )]
+    table_id: String,
+
+    #[arg(
+        long = "value",
+        short = 'v',
+        help = "The value to compare against",
+        required = true
+    )]
+    value: String,
+
+    #[arg(
+        long = "output-file",
+        short = 'o',
+        help = "Save the output to file",
+        required = false,
+        default_value = "output/eq.csv"
+    )]
+    pub output_file: Option<String>,
+}
+
+/// `eq` command
+impl EqCommand {
+    /// Execute the `eq` command
+    pub fn execute(self) -> Result<()> {
+        println!("Running eq command on table: {}", self.table_id);
+
+        // For each row in the Table, check the predicate on the row's data
+
+        Ok(())
+    }
+}
