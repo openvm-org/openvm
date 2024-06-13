b86515add86d5d4a98030c99b703e093709c0ce8
Yu Jiang Tham <yoojbruin@gmail.com>
2024-06-13 19:15:36 -0400

    Add to_page for Table

--- logical-interface/src/table/mod.rs
+++ logical-interface/src/table/mod.rs
@@ -7,6 +7,7 @@ use crate::{
     mock_db::MockDbTable,
     table::codec::fixed_bytes::FixedBytesCodec,
     types::{Data, Index},
+    utils::fixed_bytes_to_field_vec,
 };
 use std::collections::HashMap;
 use types::{TableId, TableMetadata};
@@ -53,6 +54,26 @@ impl<I: Index, D: Data> Table<I, D> {
             metadata: TableMetadata::new(Self::SIZE_I, Self::SIZE_D),
             body,
         }
+    }
+
+    pub fn to_page(&self, page_size: usize) -> Vec<Vec<u32>> {
+        let codec =
+            FixedBytesCodec::<I, D>::new(self.metadata.index_bytes, self.metadata.data_bytes);
+        self.body
+            .iter()
+            .enumerate()
+            .map(|(i, (index, data))| {
+                let enabled: Vec<u32> = if i >= page_size { vec![0] } else { vec![1] };
+                let index_bytes = codec.index_to_fixed_bytes(index.clone());
+                let index_fields = fixed_bytes_to_field_vec(index_bytes);
+                let data_bytes = codec.data_to_fixed_bytes(data.clone());
+                let data_fields = fixed_bytes_to_field_vec(data_bytes);
+                let mut page = enabled;
+                page.extend(index_fields);
+                page.extend(data_fields);
+                page
+            })
+            .collect()
     }
 
     pub fn get_id(&self) -> TableId {
--- logical-interface/src/table/tests.rs
+++ logical-interface/src/table/tests.rs
@@ -14,3 +14,25 @@ pub fn test_create_new_table() {
     let table = Table::<u32, u64>::new(table_id, TableMetadata::new(4, 8));
     assert_eq!(table.id, string_to_table_id("1".to_string()));
 }
+
+#[test]
+pub fn test_convert_to_page() {
+    let table_id = TableId::new([0; 32]);
+    let mut table = Table::<u32, u64>::new(table_id, TableMetadata::new(4, 8));
+    table.body.insert(1, 2);
+    table.body.insert(2, 4);
+    table.body.insert(4, 8);
+    table.body.insert(8, 16);
+    table.body.insert(16, 32);
+    table.body.insert(32, 64);
+    table.body.insert(64, 128);
+    table.body.insert(128, 256);
+    table.body.insert(1000, 65536);
+    table.body.insert(2000, 65792);
+    let page = table.to_page(4);
+    assert_eq!(page.len(), 10);
+    for (i, row) in page.iter().enumerate() {
+        println!("{:?}", row);
+        assert_eq!(row[0], if i < 4 { 1 } else { 0 });
+    }
+}
