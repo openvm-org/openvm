e7f1846c0bb336bf5d000771daac508df2e9e213
Alex Zhao <alexzhao@intrinsictech.com>
2024-06-20 15:57:56 -0400

    chore: un-expand constraints using AB::Expr::one()

--- vm/src/au/air.rs
+++ vm/src/au/air.rs
@@ -28,33 +28,32 @@ impl<AB: AirBuilder> Air<AB> for FieldArithmeticAir {
 
         builder.assert_bool(aux.opcode_0bit);
         builder.assert_bool(aux.opcode_1bit);
+
         builder.assert_eq(
             io.opcode,
             aux.opcode_0bit
-                + aux.opcode_1bit * AB::F::two()
+                + aux.opcode_1bit * AB::Expr::two()
                 + AB::F::from_canonical_u8(FieldArithmeticAir::BASE_OP),
         );
 
         builder.assert_eq(
             aux.is_mul,
-            aux.opcode_1bit - aux.opcode_1bit * aux.opcode_0bit,
+            aux.opcode_1bit * (AB::Expr::one() - aux.opcode_0bit),
         );
-
         builder.assert_eq(aux.is_div, aux.opcode_1bit * aux.opcode_0bit);
 
         builder.assert_eq(aux.mul_result, io.x * io.y);
-
         builder.assert_eq(aux.div_result * io.y, io.x);
-
         builder.assert_eq(
             au_cols.aux.lin_term,
-            io.x + io.y - aux.opcode_0bit * io.y - aux.opcode_0bit * io.y,
+            io.x + io.y - AB::Expr::two() * aux.opcode_0bit * io.y,
         );
 
         builder.assert_eq(
             io.z,
-            aux.is_mul * aux.mul_result + aux.is_div * aux.div_result + aux.lin_term
-                - aux.opcode_1bit * aux.lin_term,
+            aux.is_mul * aux.mul_result
+                + aux.is_div * aux.div_result
+                + aux.lin_term * (AB::Expr::one() - aux.opcode_1bit),
         );
     }
 }
