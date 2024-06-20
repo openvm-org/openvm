becd9a6ac20d84773ac406aa979051e847c06fe8
TlatoaniHJ <TlatoaniHJ@users.noreply.github.com>
2024-06-20 17:32:27 -0400

    Attempt to write integration test

--- vm/tests/integration_test.rs
+++ vm/tests/integration_test.rs
@@ -1,37 +1,71 @@
-/*use afs_test_utils::config::baby_bear_poseidon2::run_simple_test_no_pis;
-use p3_baby_bear::BabyBear;
-use p3_field::AbstractField;
+use std::sync::Arc;
 
-use stark_vm::au::AUAir;
+use afs_chips::range_gate::RangeCheckerGateChip;
+use afs_test_utils::config::baby_bear_poseidon2::run_simple_test_no_pis;
+use p3_baby_bear::BabyBear;
+
+use stark_vm::au::FieldArithmeticAir;
 use stark_vm::cpu::trace::Instruction;
 use stark_vm::cpu::CPUChip;
 use stark_vm::cpu::OpCode::*;
+use stark_vm::cpu::RANGE_CHECKER_BUS;
+use stark_vm::memory::offline_checker::OfflineChecker;
+use stark_vm::memory::MemoryAccess;
 use stark_vm::program::ProgramAir;
+
+const DATA_LEN: usize = 1;
+const ADDR_SPACE_LIMB_BITS: usize = 8;
+const POINTER_LIMB_BITS: usize = 8;
+const CLK_LIMB_BITS: usize = 8;
+const DECOMP: usize = 4;
+const RANGE_MAX: u32 = 1 << DECOMP;
+
+const MEMORY_TRACE_DEGREE: usize = 32;
 
 fn air_test(is_field_arithmetic_enabled: bool, program: Vec<Instruction<BabyBear>>) {
     let cpu_chip = CPUChip::new(is_field_arithmetic_enabled);
     let execution = cpu_chip.generate_trace(program);
 
-    let program_air = ProgramAir::new(program);
+    let program_air = ProgramAir::new(execution.program.clone());
     let program_trace = program_air.generate_trace(&execution);
 
-    let memory_chip = MemoryChip::new();
-    let memory_trace = memory_chip.generate_trace(&execution);
+    let range_checker = Arc::new(RangeCheckerGateChip::new(RANGE_CHECKER_BUS, RANGE_MAX));
+    let offline_checker = OfflineChecker::new(
+        DATA_LEN,
+        ADDR_SPACE_LIMB_BITS,
+        POINTER_LIMB_BITS,
+        CLK_LIMB_BITS,
+        DECOMP,
+    );
 
-    let field_arithmetic_air = AUAir::new();
+    let ops = execution
+        .memory_accesses
+        .iter()
+        .map(|access| MemoryAccess {
+            address: access.address,
+            op_type: access.op_type,
+            address_space: access.address_space,
+            clock: access.clock,
+            data: vec![access.data],
+        })
+        .collect::<Vec<MemoryAccess<BabyBear>>>();
+    let offline_checker_trace =
+        offline_checker.generate_trace(ops, range_checker.clone(), MEMORY_TRACE_DEGREE);
+
+    let field_arithmetic_air = FieldArithmeticAir::new();
     let field_arithmetic_trace = field_arithmetic_air.generate_trace(&execution);
 
     run_simple_test_no_pis(
         vec![
             &cpu_chip.air,
             &program_air,
-            &memory_air,
+            &offline_checker,
             &field_arithmetic_air,
         ],
         vec![
             execution.trace(),
             program_trace,
-            memory_trace,
+            offline_checker_trace,
             field_arithmetic_trace,
         ],
     )
@@ -39,80 +73,58 @@ fn air_test(is_field_arithmetic_enabled: bool, program: Vec<Instruction<BabyBear
 }
 
 #[test]
-fn test_cpu() {
-    let zero = BabyBear::zero();
-    let one = BabyBear::one();
-    let two = BabyBear::two();
-    let six = BabyBear::from_canonical_u32(6);
-
-    let neg = BabyBear::neg_one();
-    let neg_two = neg * two;
-    let neg_four = neg * BabyBear::from_canonical_u32(4);
-
-    let n = 20;
-    let nf = AbstractField::from_canonical_u64(n);
-
+fn test_vm_1() {
+    let n = 2;
     /*
     Instruction 0 assigns word[0]_1 to n.
-    Instruction 1 repeats this to make the trace height a power of 2.
-    Instruction 2 assigns word[1]_1 to 1 for use in later arithmetic operations.
+    Instruction 1 assigns word[1]_1 to 1 for use in later arithmetic operations.
+    Instruction 5 terminates
     The remainder is a loop that decrements word[0]_1 until it reaches 0, then terminates.
-    Instruction 3 checks if word[0]_1 is 0 yet, and if so terminates (by setting pc to -1)
-    Instruction 4 decrements word[0]_1 (using word[1]_1)
-    Instruction 5 uses JAL as a simple jump to go back to instruction 3 (repeating the loop).
+    Instruction 2 checks if word[0]_1 is 0 yet, and if so sets pc to 5 in order to terminate
+    Instruction 3 decrements word[0]_1 (using word[1]_1)
+    Instruction 4 uses JAL as a simple jump to go back to instruction 3 (repeating the loop).
      */
     let program = vec![
         // word[0]_1 <- word[n]_0
-        Instruction::new(STOREW, nf, zero, zero, zero, one),
-        // word[0]_1 <- word[n]_0
-        Instruction::new(STOREW, nf, zero, zero, zero, one),
+        Instruction::from_isize(STOREW, n, 0, 0, 0, 1),
         // word[1]_1 <- word[1]_1
-        Instruction::new(STOREW, one, one, zero, zero, one),
-        // if word[0]_1 == 0 then pc -= 4
-        Instruction::new(BEQ, zero, zero, neg_four, one, zero),
+        Instruction::from_isize(STOREW, 1, 1, 0, 0, 1),
+        // if word[0]_1 == 0 then pc += 3
+        Instruction::from_isize(BEQ, 0, 0, 3, 1, 0),
         // word[0]_1 <- word[0]_1 - word[1]_1
-        Instruction::new(FSUB, zero, zero, one, one, one),
+        Instruction::from_isize(FSUB, 0, 0, 1, 1, 1),
         // word[2]_1 <- pc + 1, pc -= 2
-        Instruction::new(JAL, two, neg_two, zero, one, zero),
+        Instruction::from_isize(JAL, 2, -2, 0, 1, 0),
+        // terminate
+        Instruction::from_isize(TERMINATE, 0, 0, 0, 0, 0),
     ];
 
     air_test(true, program);
 }
 
 #[test]
-fn test_cpu_without_field_arithmetic() {
+fn test_vm_without_field_arithmetic() {
     let field_arithmetic_enabled = false;
-
-    let zero = BabyBear::zero();
-    let one = BabyBear::one();
-    let two = BabyBear::two();
-    let four = BabyBear::from_canonical_u32(4);
-    let five = BabyBear::from_canonical_u32(5);
-
-    let neg = BabyBear::neg_one();
-    let neg_two = neg * two;
-    let neg_five = neg * BabyBear::from_canonical_u32(5);
 
     /*
     Instruction 0 assigns word[0]_1 to 5.
-    Instruction 1 assigns word[0]_1 to 5 (repeat to make the trace height a power of 2)
-    Instruction 2 checks if word[0]_1 is *not* 4, and if so jumps to instruction 4.
-    Instruction 3 is never run.
-    Instruction 4 checks if word[0]_1 is 5, and if so terminates (by setting pc to -1)
+    Instruction 1 checks if word[0]_1 is *not* 4, and if so jumps to instruction 4.
+    Instruction 2 is never run.
+    Instruction 3 terminates.
+    Instruction 4 checks if word[0]_1 is 5, and if so jumps to instruction 3 to terminate.
      */
     let program = vec![
         // word[0]_1 <- word[5]_0
-        Instruction::new(STOREW, five, zero, zero, zero, one),
-        // word[0]_1 <- word[5]_0
-        Instruction::new(STOREW, five, zero, zero, zero, one),
+        Instruction::from_isize(STOREW, 5, 0, 0, 0, 1),
         // if word[0]_1 != 4 then pc += 2
-        Instruction::new(BNE, zero, four, two, one, zero),
+        Instruction::from_isize(BNE, 0, 4, 3, 1, 0),
         // word[2]_1 <- pc + 1, pc -= 2
-        Instruction::new(JAL, two, neg_two, zero, one, zero),
-        // if word[0]_1 == 5 then pc -= 5
-        Instruction::new(BEQ, zero, five, neg_five, one, zero),
+        Instruction::from_isize(JAL, 2, -2, 0, 1, 0),
+        // terminate
+        Instruction::from_isize(TERMINATE, 0, 0, 0, 0, 0),
+        // if word[0]_1 == 5 then pc -= 1
+        Instruction::from_isize(BEQ, 0, 5, -1, 1, 0),
     ];
 
     air_test(field_arithmetic_enabled, program);
 }
-*/
