39be7cf2553f82c515914b0c803751546269a24d
TlatoaniHJ <TlatoaniHJ@users.noreply.github.com>
2024-06-25 13:21:14 -0400

    Move arithmetic col generation to trace.rs

--- vm/src/field_arithmetic/columns.rs
+++ vm/src/field_arithmetic/columns.rs
@@ -1,8 +1,5 @@
-use crate::cpu::OpCode;
 use afs_derive::AlignedBorrow;
 use p3_field::Field;
-
-use super::FieldArithmeticAir;
 
 /// Columns for field arithmetic chip.
 ///
@@ -38,44 +35,6 @@ where
     pub const NUM_COLS: usize = 11;
     pub const NUM_IO_COLS: usize = 4;
     pub const NUM_AUX_COLS: usize = 6;
-
-    /// Constructs a new set of columns (including auxiliary columns) given inputs.
-    pub fn new(op: OpCode, x: T, y: T) -> Self {
-        let opcode = op as u32;
-        let opcode_value = opcode - FieldArithmeticAir::BASE_OP as u32;
-        let opcode_lo_u32 = opcode_value % 2;
-        let opcode_hi_u32 = opcode_value / 2;
-        let opcode_lo = T::from_canonical_u32(opcode_lo_u32);
-        let opcode_hi = T::from_canonical_u32(opcode_hi_u32);
-        let is_div = T::from_bool(op == OpCode::FDIV);
-        let is_mul = T::from_bool(op == OpCode::FMUL);
-        let sum_or_diff = x + y - T::two() * opcode_lo * y;
-        let product = x * y;
-        let quotient = if y == T::zero() {
-            T::zero()
-        } else {
-            x * y.inverse()
-        };
-        let z = is_mul * product + is_div * quotient + (T::one() - opcode_hi) * sum_or_diff;
-
-        Self {
-            io: FieldArithmeticIOCols {
-                opcode: T::from_canonical_u32(opcode),
-                x,
-                y,
-                z,
-            },
-            aux: FieldArithmeticAuxCols {
-                opcode_lo,
-                opcode_hi,
-                is_mul,
-                is_div,
-                sum_or_diff,
-                product,
-                quotient,
-            },
-        }
-    }
 
     pub fn get_width() -> usize {
         FieldArithmeticIOCols::<T>::get_width() + FieldArithmeticAuxCols::<T>::get_width()
