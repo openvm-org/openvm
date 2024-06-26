8621df969eb4bb1b23cafb13790bf7c8bcded071
Alex Zhao <alexzhao@intrinsictech.com>
2024-06-26 14:26:59 -0400

    feat: receiving interactions, did not update tests

---
+++ vm/src/poseidon2/bridge.rs
@@ -0,0 +1,23 @@
+use afs_stark_backend::interaction::{AirBridge, Interaction};
+use p3_air::VirtualPairCol;
+use p3_field::Field;
+
+use crate::poseidon2::{columns::Poseidon2Cols, Poseidon2Air};
+
+// No interactions
+impl<const WIDTH: usize, F: Field> AirBridge<F> for Poseidon2Air<WIDTH, F> {
+    fn receives(&self) -> Vec<Interaction<F>> {
+        let index_map = Poseidon2Cols::<WIDTH, F>::index_map(self);
+        vec![Interaction {
+            fields: (index_map
+                .input
+                .collect::<Vec<_>>()
+                .into_iter()
+                .chain(index_map.output.collect::<Vec<_>>()))
+            .map(VirtualPairCol::single_main)
+            .collect(),
+            count: VirtualPairCol::one(),
+            argument_index: self.BUS_INDEX,
+        }]
+    }
+}
