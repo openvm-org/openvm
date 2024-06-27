1c85d487a52ab6bd9e8be60be25ce6ab25ed8321
Alex Zhao <alexzhao@intrinsictech.com>
2024-06-27 12:55:23 -0400

    chore: remove debug changes

--- stark-backend/src/air_builders/symbolic/mod.rs
+++ stark-backend/src/air_builders/symbolic/mod.rs
@@ -222,8 +222,6 @@ impl<F: Field> AirBuilder for SymbolicRapBuilder<F> {
 
     fn assert_zero<I: Into<Self::Expr>>(&mut self, x: I) {
         self.constraints.push(x.into());
-        // dbg!(self.constraints.last().unwrap());
-        // panic!()
     }
 }
 
