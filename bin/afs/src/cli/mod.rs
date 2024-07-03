436d619adcb8d7c7fd796d202bce4a98935646a9
Yu Jiang Tham <yoojbruin@gmail.com>
2024-07-03 00:58:10 -0400

    WIP: still unable to match engine types

--- bin/afs/src/cli/mod.rs
+++ bin/afs/src/cli/mod.rs
@@ -2,13 +2,28 @@ use crate::commands::{cache, keygen, mock, prove, verify};
 use afs_stark_backend::config::Com;
 use afs_stark_backend::config::PcsProof;
 use afs_stark_backend::config::PcsProverData;
+use afs_test_utils::config::baby_bear_blake3::BabyBearBlake3Config;
+use afs_test_utils::config::baby_bear_blake3::BabyBearBlake3Engine;
+use afs_test_utils::config::baby_bear_bytehash::engine_from_byte_hash;
+use afs_test_utils::config::baby_bear_keccak::BabyBearKeccakConfig;
+use afs_test_utils::config::baby_bear_keccak::BabyBearKeccakEngine;
+use afs_test_utils::config::baby_bear_poseidon2::config_from_perm;
+use afs_test_utils::config::baby_bear_poseidon2::default_engine;
+use afs_test_utils::config::baby_bear_poseidon2::engine_from_perm;
+use afs_test_utils::config::baby_bear_poseidon2::random_perm;
+use afs_test_utils::config::baby_bear_poseidon2::BabyBearPoseidon2Config;
+use afs_test_utils::config::baby_bear_poseidon2::BabyBearPoseidon2Engine;
+use afs_test_utils::config::EngineType;
 use afs_test_utils::engine::StarkEngine;
 use afs_test_utils::page_config::PageConfig;
 use clap::Parser;
 use clap::Subcommand;
+use p3_blake3::Blake3;
 use p3_field::PrimeField64;
+use p3_keccak::Keccak256Hash;
 use p3_uni_stark::Domain;
 use p3_uni_stark::{StarkGenericConfig, Val};
+use p3_util::log2_strict_usize;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
@@ -50,14 +65,45 @@ impl<SC: StarkGenericConfig, E: StarkEngine<SC>> Cli<SC, E>
 where
     Val<SC>: PrimeField64,
     PcsProverData<SC>: Serialize + DeserializeOwned + Send + Sync,
-    Domain<SC>: Send + Sync,
-    SC::Pcs: Sync,
-    PcsProverData<SC>: Send + Sync,
-    Com<SC>: Send + Sync,
-    SC::Challenge: Send + Sync,
     PcsProof<SC>: Send + Sync,
+    Domain<SC>: Send + Sync,
+    Com<SC>: Send + Sync,
+    SC::Pcs: Sync,
+    SC::Challenge: Send + Sync,
+    BabyBearBlake3Engine: StarkEngine<BabyBearBlake3Config>,
+    BabyBearKeccakEngine: StarkEngine<BabyBearKeccakConfig>,
+    BabyBearPoseidon2Engine: StarkEngine<BabyBearPoseidon2Config>,
 {
-    pub fn run(config: &PageConfig, engine: &E) -> Self {
+    pub fn run(config: &PageConfig) -> Self {
+        let pcs_log_degree = log2_strict_usize(config.page.height);
+        let fri_params = config.fri_params;
+        let engine_type = config.stark_engine.engine;
+        match engine_type {
+            EngineType::BabyBearBlake3 => {
+                let engine: BabyBearBlake3Engine =
+                    engine_from_byte_hash(Blake3, pcs_log_degree, fri_params);
+                Self::run_with_engine(config, &engine)
+            }
+
+            EngineType::BabyBearKeccak => {
+                let engine: BabyBearKeccakEngine =
+                    engine_from_byte_hash(Keccak256Hash, pcs_log_degree, fri_params);
+                Self::run_with_engine(config, &engine)
+            }
+            EngineType::BabyBearPoseidon2 => {
+                let perm = random_perm();
+                // let config = config_from_perm(&perm, pcs_log_degree, fri_params);
+                let engine: BabyBearPoseidon2Engine =
+                    engine_from_perm(perm, pcs_log_degree, fri_params);
+                Self::run_with_engine(config, &engine)
+            }
+        }
+    }
+
+    pub fn run_with_engine(config: &PageConfig, engine: &E) -> Self
+    where
+        E: StarkEngine<SC>,
+    {
         let cli = Self::parse();
         match &cli.command {
             CliCommand::Mock(mock) => {
--- bin/afs/src/main.rs
+++ bin/afs/src/main.rs
@@ -1,5 +1,5 @@
 use afs::cli::Cli;
-use afs_test_utils::{config::setup_tracing, engine::engine_from_params, page_config::PageConfig};
+use afs_test_utils::{config::setup_tracing, page_config::PageConfig};
 use p3_util::log2_strict_usize;
 
 fn main() {
@@ -7,10 +7,6 @@ fn main() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
     let config = PageConfig::read_config_file("config.toml");
-    let pcs_log_degree = log2_strict_usize(config.page.height);
-    let engine_type = config.stark_engine.engine;
-    let fri_params = config.fri_params;
-    let engine = engine_from_params(engine_type, fri_params, pcs_log_degree).as_ref();
     setup_tracing();
-    let _cli = Cli::run(&config, &engine);
+    let _cli = Cli::run(&config);
 }
