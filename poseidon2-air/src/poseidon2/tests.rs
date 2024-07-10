09cd582df56be0a436c0577f368fb777835b00a0
Alex Zhao <alexzhao@intrinsictech.com>
2024-07-10 12:31:42 -0400

    feat: completed default config setup, added test to check default behavior against Poseidon2::new()

--- poseidon2-air/src/poseidon2/tests.rs
+++ poseidon2-air/src/poseidon2/tests.rs
@@ -1,3 +1,5 @@
+use super::{HL_BABYBEAR_EXT_CONST_16, HL_BABYBEAR_INT_CONST_16};
+use super::{HL_MDS_MAT_4, MDS_MAT_4};
 use afs_stark_backend::{prover::USE_DEBUG_BUILDER, verifier::VerificationError};
 use afs_test_utils::config::{
     baby_bear_poseidon2::{engine_from_perm, random_perm},
@@ -28,6 +30,104 @@ use zkhash::poseidon2::poseidon2::Poseidon2 as HorizenPoseidon2;
 use zkhash::poseidon2::poseidon2_instance_babybear::POSEIDON2_BABYBEAR_16_PARAMS;
 
 use crate::poseidon2::Poseidon2Air;
+
+#[test]
+fn test_poseidon2_default() {
+    // config
+    let num_rows = 1 << 4;
+    let num_ext_rounds = 8;
+    let num_int_rounds = 13;
+
+    // random constants, state generation
+    let mut rng = create_seeded_rng();
+    let states: Vec<[BabyBear; 16]> = (0..num_rows)
+        .map(|_| {
+            let vec: Vec<BabyBear> = (0..16)
+                .map(|_| BabyBear::from_canonical_u32(rng.next_u32() % (1 << 30)))
+                .collect();
+            vec.try_into().unwrap()
+        })
+        .collect();
+
+    // air and trace generation
+    let poseidon2_air = Poseidon2Air::<16, BabyBear>::default();
+
+    let mut poseidon2_trace = poseidon2_air.generate_trace(states.clone());
+    let mut outputs = states.clone();
+    let poseidon2: Poseidon2<
+        BabyBear,
+        Poseidon2ExternalMatrixGeneral,
+        DiffusionMatrixBabyBear,
+        16,
+        7,
+    > = Poseidon2::new(
+        num_ext_rounds,
+        HL_BABYBEAR_EXT_CONST_16.to_vec(),
+        Poseidon2ExternalMatrixGeneral,
+        num_int_rounds,
+        HL_BABYBEAR_INT_CONST_16.to_vec(),
+        DiffusionMatrixBabyBear,
+    );
+    for output in outputs.iter_mut() {
+        poseidon2.permute_mut(output);
+    }
+
+    // dummy interaction air and trace generation
+    let page_requester = DummyInteractionAir::new(2 * 16, true, poseidon2_air.bus_index);
+    let dummy_trace = RowMajorMatrix::new(
+        states
+            .into_iter()
+            .zip(outputs.iter())
+            .flat_map(|(state, output)| {
+                [BabyBear::one()]
+                    .into_iter()
+                    .chain(state.to_vec())
+                    .chain(output.to_vec())
+                    .collect::<Vec<_>>()
+            })
+            .collect(),
+        2 * 16 + 1,
+    );
+
+    let traces = vec![poseidon2_trace.clone(), dummy_trace.clone()];
+
+    // engine generation
+    let max_trace_height = traces.iter().map(|trace| trace.height()).max().unwrap();
+    let max_log_degree = log2_strict_usize(max_trace_height);
+    let perm = random_perm();
+    let fri_params = fri_params_with_80_bits_of_security()[1];
+    let engine = engine_from_perm(perm, max_log_degree, fri_params);
+
+    // positive test
+    engine
+        .run_simple_test(
+            vec![&poseidon2_air, &page_requester],
+            traces,
+            vec![vec![]; 2],
+        )
+        .expect("Verification failed");
+
+    // negative test
+    USE_DEBUG_BUILDER.with(|debug| {
+        *debug.lock().unwrap() = false;
+    });
+    for _ in 0..10 {
+        let width = rng.gen_range(0..poseidon2_air.get_width());
+        let height = rng.gen_range(0..num_rows);
+        let rand = BabyBear::from_canonical_u32(rng.gen_range(1..=1 << 27));
+        poseidon2_trace.row_mut(height)[width] += rand;
+        assert_eq!(
+            engine.run_simple_test(
+                vec![&poseidon2_air, &page_requester],
+                vec![poseidon2_trace.clone(), dummy_trace.clone()],
+                vec![vec![]; 2],
+            ),
+            Err(VerificationError::OodEvaluationMismatch),
+            "Expected constraint to fail"
+        );
+        poseidon2_trace.row_mut(height)[width] -= rand;
+    }
+}
 
 #[test]
 fn test_poseidon2() {
@@ -62,7 +162,7 @@ fn test_poseidon2() {
     let poseidon2_air = Poseidon2Air::<16, BabyBear>::new(
         external_constants.clone(),
         internal_constants.clone(),
-        Poseidon2Air::<16, BabyBear>::MDS_MAT_4,
+        MDS_MAT_4,
         POSEIDON2_INTERNAL_MATRIX_DIAG_16_BABYBEAR_MONTY,
         BabyBear::from_wrapped_u64(1u64 << 32).inverse(), // 943718400
         0,
@@ -153,7 +253,7 @@ fn test_horizen_poseidon2() {
     let mut air_permut = Poseidon2Air::<16, BabyBear>::new(
         external_round_constants,
         internal_round_constants,
-        Poseidon2Air::<16, BabyBear>::HL_MDS_MAT_4,
+        HL_MDS_MAT_4,
         horizen_int_diag,
         BabyBear::one(),
         0,
@@ -196,7 +296,7 @@ where
     let mut poseidon2air = Poseidon2Air::<16, BabyBear>::new(
         external_constants.clone(),
         internal_constants.clone(),
-        Poseidon2Air::<16, BabyBear>::MDS_MAT_4,
+        MDS_MAT_4,
         POSEIDON2_INTERNAL_MATRIX_DIAG_16_BABYBEAR_MONTY,
         BabyBear::from_wrapped_u64(1u64 << 32).inverse(), // 943718400
         0,
