e17cac3a68bfe094e8d9fecc22362d0b148a563d
TlatoaniHJ <TlatoaniHJ@users.noreply.github.com>
2024-07-19 11:14:42 -0400

    Rustfmt

--- vm/src/memory/expand/tests/mod.rs
+++ vm/src/memory/expand/tests/mod.rs
@@ -9,9 +9,9 @@ use afs_test_utils::config::baby_bear_blake3::run_simple_test_no_pis;
 use afs_test_utils::interaction::dummy_interaction_air::DummyInteractionAir;
 use afs_test_utils::utils::create_seeded_rng;
 
-use crate::memory::expand::{EXPAND_BUS, ExpandChip};
 use crate::memory::expand::columns::ExpandCols;
 use crate::memory::expand::tests::util::HashTestChip;
+use crate::memory::expand::{ExpandChip, EXPAND_BUS};
 use crate::memory::tree::trees_from_full_memory;
 
 mod util;
