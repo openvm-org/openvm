513c06712ce73f7de6754db706dba5c915768085
TlatoaniHJ <TlatoaniHJ@users.noreply.github.com>
2024-07-26 11:47:51 -0400

    Refactor interactions further

--- vm/src/memory/expand/bridge.rs
+++ vm/src/memory/expand/bridge.rs
@@ -8,26 +8,6 @@ use crate::memory::expand::{
     air::ExpandAir, columns::ExpandCols, EXPAND_BUS, POSEIDON2_DIRECT_REQUEST_BUS,
 };
 
-fn push_expand_send<const CHUNK: usize, AB: InteractionBuilder>(
-    builder: &mut AB,
-    sends: impl Into<AB::Expr>,
-    is_final: impl Into<AB::Expr>,
-    address_space: impl Into<AB::Expr>,
-    label: impl Into<AB::Expr>,
-    height: impl Into<AB::Expr>,
-    hash: [impl Into<AB::Expr>; CHUNK],
-) {
-    let fields = [
-        is_final.into(),
-        address_space.into(),
-        height.into(),
-        label.into(),
-    ]
-    .into_iter()
-    .chain(hash.into_iter().map(Into::into));
-    builder.push_send(EXPAND_BUS, fields, sends);
-}
-
 impl<const CHUNK: usize> ExpandAir<CHUNK> {
     pub fn eval_interactions<AB: InteractionBuilder>(
         &self,
@@ -38,32 +18,43 @@ impl<const CHUNK: usize> ExpandAir<CHUNK> {
         // direction = -1   => parent_is_final = 1
         let parent_is_final = (AB::Expr::one() - local.direction) * AB::F::two().inverse();
 
-        push_expand_send(
-            builder,
+        builder.push_send(
+            EXPAND_BUS,
+            [
+                parent_is_final.clone(),
+                local.address_space.into(),
+                local.parent_height.into(),
+                local.parent_label.into(),
+            ]
+            .into_iter()
+            .chain(local.parent_hash.into_iter().map(Into::into)),
             -local.direction.into(),
-            parent_is_final.clone(),
-            local.address_space,
-            local.parent_label,
-            local.parent_height,
-            local.parent_hash,
         );
-        push_expand_send(
-            builder,
-            local.direction,
-            parent_is_final.clone() + local.left_is_final,
-            local.address_space,
-            local.parent_label * AB::F::two(),
-            local.parent_height - AB::F::one(),
-            local.left_child_hash,
+
+        builder.push_receive(
+            EXPAND_BUS,
+            [
+                parent_is_final.clone() + local.left_is_final,
+                local.address_space.into(),
+                local.parent_height - AB::F::one(),
+                local.parent_label * AB::F::two(),
+            ]
+            .into_iter()
+            .chain(local.left_child_hash.into_iter().map(Into::into)),
+            -local.direction.into(),
         );
-        push_expand_send(
-            builder,
-            local.direction,
-            parent_is_final.clone() + local.right_is_final,
-            local.address_space,
-            (local.parent_label * AB::F::two()) + AB::F::one(),
-            local.parent_height - AB::F::one(),
-            local.right_child_hash,
+
+        builder.push_receive(
+            EXPAND_BUS,
+            [
+                parent_is_final.clone() + local.right_is_final,
+                local.address_space.into(),
+                local.parent_height - AB::F::one(),
+                (local.parent_label * AB::F::two()) + AB::F::one(),
+            ]
+            .into_iter()
+            .chain(local.right_child_hash.into_iter().map(Into::into)),
+            -local.direction.into(),
         );
 
         let hash_fields = iter::empty()
