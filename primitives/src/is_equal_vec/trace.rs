7a4fb46e0c2724af17cc2d6dbf5d365d86eaa40a
Osama Alkhodairy <osama.khodairy@hotmail.com>
2024-07-29 12:52:05 -0400

    add IsEqualVecColsMut

--- primitives/src/is_equal_vec/trace.rs
+++ primitives/src/is_equal_vec/trace.rs
@@ -1,9 +1,7 @@
-use std::iter::zip;
-
 use p3_field::Field;
 use p3_matrix::dense::RowMajorMatrix;
 
-use crate::sub_chip::LocalTraceInstructions;
+use crate::{is_equal_vec::columns::IsEqualVecColsMut, sub_chip::LocalTraceInstructions};
 
 use super::{columns::IsEqualVecCols, IsEqualVecAir};
 
@@ -14,14 +12,63 @@ impl IsEqualVecAir {
         assert!(height.is_power_of_two());
         assert_eq!(x.len(), y.len());
 
-        let rows: Vec<_> = zip(x, y)
-            .flat_map(|(x_row, y_row)| {
-                let row = self.generate_trace_row((x_row, y_row));
-                row.flatten()
+        let mut rows_concat = vec![F::zero(); width * x.len()];
+        for (i, (x, y)) in x.iter().zip(y.iter()).enumerate() {
+            let is_equal_cols =
+                IsEqualVecColsMut::from_slice(&mut rows_concat[i * width..(i + 1) * width], self);
+
+            self.generate_trace_row(x, y, is_equal_cols);
+        }
+
+        RowMajorMatrix::new(rows_concat, width)
+    }
+
+    fn generate_trace_row<F: Field>(&self, x: &[F], y: &[F], is_equal_cols: IsEqualVecColsMut<F>) {
+        is_equal_cols.io.x.clone_from_slice(x);
+        is_equal_cols.io.y.clone_from_slice(y);
+
+        self.generate_trace_row_aux_and_out(x, y, is_equal_cols);
+    }
+
+    // TODO: should the input always be u32s?
+    fn generate_trace_row_aux_and_out<F: Field>(
+        &self,
+        x_row: &[F],
+        y_row: &[F],
+        is_equal_cols: IsEqualVecColsMut<F>,
+    ) {
+        let vec_len = self.vec_len;
+        let mut transition_index = 0;
+        while transition_index < vec_len && x_row[transition_index] == y_row[transition_index] {
+            transition_index += 1;
+        }
+
+        // TODO: test if initializing a mut vec and editting it is more efficient
+        let prods: Vec<F> = (0..vec_len - 1)
+            .map(|i| {
+                if i < transition_index {
+                    F::one()
+                } else {
+                    F::zero()
+                }
             })
             .collect();
 
-        RowMajorMatrix::new(rows, width)
+        let is_equal = if vec_len - 1 < transition_index {
+            F::one()
+        } else {
+            F::zero()
+        };
+
+        let mut invs = vec![F::zero(); vec_len];
+        if transition_index != vec_len {
+            invs[transition_index] = (x_row[transition_index] - y_row[transition_index]).inverse();
+        }
+
+        // Filling out is_equal_cols
+        *is_equal_cols.io.is_equal = is_equal;
+        is_equal_cols.aux.prods.clone_from_slice(&prods);
+        is_equal_cols.aux.invs.clone_from_slice(&invs);
     }
 }
 
