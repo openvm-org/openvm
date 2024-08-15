a36082fc5def42c9fc59f4769c32e340f4cba306
Alexander Golovanov <alex@axiom.xyz>
2024-08-15 13:50:27 +0200

    Expect a specific error in each negative test, also add some comments

--- vm/src/long_arithmetic/tests.rs
+++ vm/src/long_arithmetic/tests.rs
@@ -1,3 +1,4 @@
+use afs_stark_backend::{prover::USE_DEBUG_BUILDER, verifier::VerificationError};
 use afs_test_utils::{
     config::baby_bear_poseidon2::run_simple_test_no_pis, utils::create_seeded_rng,
 };
@@ -50,6 +51,10 @@ fn add_long_rand_air_test() {
     .expect("Verification failed");
 }
 
+// Given a fake trace of a single addition, which mostly consists of zeroes
+// except for a single position, setup a chip and provide this trace for it.
+// The chip will do what it would normally do for this addition query,
+// except the generated trace will be what we provide.
 fn setup_bad_long_addition_test(
     x1: u32,
     y1: u32,
@@ -83,39 +88,45 @@ fn setup_bad_long_addition_test(
     (chip, trace)
 }
 
-fn run_bad_long_addition_test(chip: &LongAdditionChip<256, 16>, trace: RowMajorMatrix<BabyBear>) {
+fn run_bad_long_addition_test(
+    chip: &LongAdditionChip<256, 16>,
+    trace: RowMajorMatrix<BabyBear>,
+    expected_error: VerificationError,
+) {
     let range_trace = chip.range_checker_chip.generate_trace::<BabyBear>();
 
-    run_simple_test_no_pis(
-        vec![&chip.air, &chip.range_checker_chip.air],
-        vec![trace, range_trace],
-    )
-    .expect("Verification failed");
+    USE_DEBUG_BUILDER.with(|debug| {
+        *debug.lock().unwrap() = false;
+    });
+    assert_eq!(
+        run_simple_test_no_pis(
+            vec![&chip.air, &chip.range_checker_chip.air],
+            vec![trace, range_trace],
+        ),
+        Err(expected_error),
+        "Expected verification to fail, but it passed"
+    );
 }
 
-#[should_panic]
 #[test]
 fn add_long_wrong_carry_air_test() {
     let (chip, trace) = setup_bad_long_addition_test(1, 1, 3, 1, 1);
-    run_bad_long_addition_test(&chip, trace);
+    run_bad_long_addition_test(&chip, trace, VerificationError::OodEvaluationMismatch);
 }
 
-#[should_panic]
 #[test]
 fn add_long_out_of_range_air_test() {
     let (chip, trace) = setup_bad_long_addition_test(65_000, 65_000, 130_000, 0, 1);
-    run_bad_long_addition_test(&chip, trace);
+    run_bad_long_addition_test(&chip, trace, VerificationError::NonZeroCumulativeSum);
 }
 
-#[should_panic]
 #[test]
 fn add_long_wrong_addition_air_test() {
     let (chip, trace) = setup_bad_long_addition_test(65_000, 65_000, 130_000 - (1 << 16), 0, 1);
-    run_bad_long_addition_test(&chip, trace);
+    run_bad_long_addition_test(&chip, trace, VerificationError::OodEvaluationMismatch);
 }
 
 // We NEED to check that the carry is 0 or 1
-#[should_panic]
 #[test]
 fn add_long_invalid_carry_air_test() {
     let bad_carry = BabyBear::from_canonical_u32(1 << 16)
@@ -145,5 +156,5 @@ fn add_long_invalid_carry_air_test() {
         .collect::<Vec<_>>();
     let trace = RowMajorMatrix::new(trace, 64);
 
-    run_bad_long_addition_test(&chip, trace);
+    run_bad_long_addition_test(&chip, trace, VerificationError::OodEvaluationMismatch);
 }
