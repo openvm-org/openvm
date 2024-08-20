51583e6a19f40bb4a1a283c44b08f5517dca5a21
Zach Langley <zach@axiom.xyz>
2024-08-19 22:38:46 -0400

    Use MemoryBridge for ext chip

--- vm/src/field_extension/bridge.rs
+++ vm/src/field_extension/bridge.rs
@@ -1,54 +1,56 @@
 use std::array;
 
 use afs_stark_backend::interaction::InteractionBuilder;
-use itertools::Itertools;
 use p3_field::AbstractField;
 
 use super::{columns::FieldExtensionArithmeticCols, FieldExtensionArithmeticAir, EXTENSION_DEGREE};
 use crate::{
     cpu::FIELD_EXTENSION_BUS,
     field_extension::columns::FieldExtensionArithmeticAuxCols,
-    memory::{
-        manager::{access_cell::AccessCell, operation::MemoryOperation},
-        offline_checker::{bridge::NewMemoryOfflineChecker, columns::MemoryOfflineCheckerAuxCols},
-    },
 };
+use crate::memory::MemoryAddress;
+use crate::memory::offline_checker::bridge::MemoryBridge;
 
 #[allow(clippy::too_many_arguments)]
 fn eval_rw_interactions<AB: InteractionBuilder, const WORD_SIZE: usize>(
-    builder: &mut AB,
-    mem_oc: &NewMemoryOfflineChecker,
+    memory_bridge: &mut MemoryBridge<AB::Var, WORD_SIZE>,
     clk_offset: &mut usize,
     is_write: bool,
     start_timestamp: AB::Var,
     addr_space: AB::Var,
     address: AB::Var,
     ext: [AB::Var; EXTENSION_DEGREE],
-    mem_aux_cols: [MemoryOfflineCheckerAuxCols<WORD_SIZE, AB::Var>; EXTENSION_DEGREE],
 ) {
-    for (i, (element, aux_cols)) in ext.into_iter().zip_eq(mem_aux_cols.into_iter()).enumerate() {
+    for (i, element) in ext.into_iter().enumerate() {
         let pointer = address + AB::F::from_canonical_usize(i * WORD_SIZE);
-        let data = array::from_fn(|i| {
-            if i == 0 {
-                element.into()
-            } else {
-                AB::Expr::zero()
-            }
-        });
 
         let clk = start_timestamp + AB::Expr::from_canonical_usize(*clk_offset);
         *clk_offset += 1;
 
-        let op = MemoryOperation::<WORD_SIZE, AB::Expr> {
-            addr_space: addr_space.into(),
-            pointer,
-            op_type: AB::Expr::from_bool(is_write),
-            cell: AccessCell { data, clk },
-            enabled: AB::Expr::one(),
-        };
-
-        mem_oc.subair_eval(builder, op, aux_cols);
+        if is_write {
+            memory_bridge.write(
+                MemoryAddress::new(addr_space, pointer),
+                emb(element.into()),
+                clk,
+            );
+        } else {
+            memory_bridge.read(
+                MemoryAddress::new(addr_space, pointer),
+                emb(element.into()),
+                clk,
+            );
+        }
     }
+}
+
+fn emb<F: AbstractField, const WORD_SIZE: usize>(element: F) -> [F; WORD_SIZE] {
+    array::from_fn(|j| {
+        if j == 0 {
+            element.clone()
+        } else {
+            F::zero()
+        }
+    })
 }
 
 impl<const WORD_SIZE: usize> FieldExtensionArithmeticAir<WORD_SIZE> {
@@ -73,48 +75,40 @@ impl<const WORD_SIZE: usize> FieldExtensionArithmeticAir<WORD_SIZE> {
             ..
         } = aux;
 
-        let mut mem_aux_cols_iter = mem_oc_aux_cols.into_iter();
+        let mut memory_bridge = MemoryBridge::new(self.mem_oc, mem_oc_aux_cols);
 
         // Reads for x
-        eval_rw_interactions(
-            builder,
-            &self.mem_oc,
+        eval_rw_interactions::<AB, WORD_SIZE>(
+            &mut memory_bridge,
             &mut clk_offset,
             false,
             start_timestamp,
             d,
             op_b,
             io.x,
-            array::from_fn(|_| mem_aux_cols_iter.next().unwrap()),
         );
 
         // Reads for y
-        eval_rw_interactions(
-            builder,
-            &self.mem_oc,
+        eval_rw_interactions::<AB, WORD_SIZE>(
+            &mut memory_bridge,
             &mut clk_offset,
             false,
             start_timestamp,
             e,
             op_c,
             io.y,
-            array::from_fn(|_| mem_aux_cols_iter.next().unwrap()),
         );
 
         // Writes for z
-        eval_rw_interactions(
-            builder,
-            &self.mem_oc,
+        eval_rw_interactions::<AB, WORD_SIZE>(
+            &mut memory_bridge,
             &mut clk_offset,
             true,
             start_timestamp,
             d,
             op_a,
             io.z,
-            array::from_fn(|_| mem_aux_cols_iter.next().unwrap()),
         );
-
-        debug_assert!(mem_aux_cols_iter.next().is_none());
 
         // Receives all IO columns from another chip on bus 3 (FIELD_EXTENSION_BUS)
         builder.push_receive(
