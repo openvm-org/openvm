9f6d20fb7c66a3ee7f099ff099b5b641e2c9badf
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-09-09 13:59:26 -0700

    chore: add comments around input validation

--- ecc/src/ecdsa.rs
+++ ecc/src/ecdsa.rs
@@ -10,6 +10,9 @@ use crate::{
 /// Return 1 if the ECDSA verification succeeds. Otherwise, return 0.
 /// **Assumption**: `input` is a valid ECDSA input.
 /// Reference: https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm#Signature_verification_algorithm
+///
+/// **Caution:** This function does not perform input validation. This should be done separately via `input.is_valid`.
+/// This is not done in this function since the input may share a public key so the input validation can be shared.
 pub fn verify_ecdsa_secp256k1<C: Config>(
     builder: &mut Builder<C>,
     input: &ECDSAInputVariable<C>,
@@ -19,8 +22,10 @@ where
     C::N: PrimeField64,
 {
     let z = &input.msg_hash;
+    // TODO: make sure input validation checks that r,s < modulus of scalar field
     let u1 = builder.secp256k1_scalar_div(z, &input.sig.s);
     let u2 = builder.secp256k1_scalar_div(&input.sig.r, &input.sig.s);
+    // TODO: do we need to enforce u1, u2 are < modulus of scalar field?
     let generator = load_generator_secp256k1(builder);
     let u1_g = scalar_multiply_secp256k1(builder, &generator, u1, window_bits);
     let u2_qa = scalar_multiply_secp256k1(builder, &input.pubkey, u2, window_bits);
@@ -30,8 +35,6 @@ where
 
     let x1_is_0 = builder.secp256k1_coord_is_zero(&x1);
     let y1_is_0 = builder.secp256k1_coord_is_zero(&y1);
-    builder.print_v(x1_is_0);
-    builder.print_v(y1_is_0);
     builder.if_eq(x1_is_0 * y1_is_0, C::N::one()).then_or_else(
         |builder| {
             builder.assign(&ret, C::N::zero());
