04482e8b77c13fce10adab58ff9d7f56e7542686
Yi Sun <yi-sun@users.noreply.github.com>
2024-09-19 13:50:59 -0400

    feat: add negative test

--- vm/src/ui/tests.rs
+++ vm/src/ui/tests.rs
@@ -27,33 +27,56 @@ fn solve_lui_sanity_test() {
 fn prepare_lui_write_execute(
     tester: &mut MachineChipTestBuilder<F>,
     chip: &mut UiChip<F>,
-    rng: &mut StdRng,
+    a: usize,
+    b: usize,
 ) {
-    let address_range = || 0usize..1 << 29;
+    let x = UiChip::<F>::solve_lui(b as u32);
 
-    let address_a = rng.gen_range(address_range()); // op_a
-    let imm = rng.gen_range(0usize..1 << 20); // op_b
+    tester.execute(chip, Instruction::from_usize(Opcode::LUI, [a, b]));
+    assert_eq!(x.map(F::from_canonical_u32), tester.read::<4>(1usize, a));
+}
 
-    let x = UiChip::<F>::solve_lui(imm as u32);
+fn generate_random_input(rng: &mut StdRng, address_bits: usize, imm_bits: usize) -> (usize, usize) {
+    let address_range = || 0usize..1 << address_bits;
+    let imm_range = || 0usize..1 << imm_bits;
 
-    tester.execute(chip, Instruction::from_usize(Opcode::LUI, [address_a, imm]));
-    assert_eq!(
-        x.map(F::from_canonical_u32),
-        tester.read::<4>(1usize, address_a)
-    );
+    let a = rng.gen_range(address_range());
+    let b = rng.gen_range(imm_range());
+
+    (a, b)
 }
 
 #[test]
 fn lui_test() {
     let mut rng = create_seeded_rng();
+
     let mut tester = MachineChipTestBuilder::default();
     let mut chip = UiChip::<F>::new(tester.execution_bus(), tester.memory_chip());
-    let num_tests: usize = 1;
+    let num_tests: usize = 10;
 
     for _ in 0..num_tests {
-        prepare_lui_write_execute(&mut tester, &mut chip, &mut rng);
+        let (a, b) = generate_random_input(&mut rng, 29, 20);
+        prepare_lui_write_execute(&mut tester, &mut chip, a, b);
     }
 
     let tester = tester.build().load(chip).finalize();
     tester.simple_test().expect("Verification failed");
+}
+
+#[test]
+fn negative_lui_invalid_imm_test() {
+    let mut rng = create_seeded_rng();
+    let mut tester = MachineChipTestBuilder::default();
+    let mut chip = UiChip::<F>::new(tester.execution_bus(), tester.memory_chip());
+
+    // (1 << 21) exceeds the 20-bit bound
+    prepare_lui_write_execute(&mut tester, &mut chip, 3, 1 << 21);
+
+    let tester = tester.build().load(chip).finalize();
+    disable_debug_builder();
+    assert_eq!(
+        tester.simple_test().err(),
+        Some(VerificationError::NonZeroCumulativeSum),
+        "Expected verification to fail, but it didn't"
+    );
 }
