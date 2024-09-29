ae9bcb40ee3ce439b5f813b041c122bab1bc3d0d
Andrew Gu <67293785+MonkeyKing-1@users.noreply.github.com>
2024-09-29 16:05:44 -0400

    finish fixed scalar multiply

--- ecc/lib/src/tests/mod.rs
+++ ecc/lib/src/tests/mod.rs
@@ -4,8 +4,19 @@ use afs_compiler::{asm::AsmBuilder, util::execute_program};
 use num_bigint_dig::BigUint;
 use p3_baby_bear::BabyBear;
 use p3_field::extension::BinomialExtensionField;
+use snark_verifier_sdk::snark_verifier::{
+    halo2_base::{
+        halo2_proofs::halo2curves::secp256k1::{self, Secp256k1Affine},
+        utils::ScalarField,
+    },
+    util::arithmetic::CurveAffine,
+};
 
-use crate::{ec_mul::scalar_multiply_secp256k1, types::ECPoint};
+use crate::{
+    ec_fixed_scalar_multiply::{fixed_scalar_multiply_secp256k1, CachedPoints},
+    ec_mul::scalar_multiply_secp256k1,
+    types::ECPoint,
+};
 
 mod ecdsa;
 
@@ -97,6 +108,135 @@ fn test_compiler_ec_mul_full_period() {
 fn test_compiler_ec_mul_zero() {
     test_ec_mul(
         (BigUint::from(2u64), BigUint::from(1u64)),
+        BigUint::from(0u64),
+        (BigUint::from(0u64), BigUint::from(0u64)),
+        4,
+    );
+}
+
+fn test_fixed_ec_mul(
+    base: (BigUint, BigUint),
+    scalar: BigUint,
+    expected: (BigUint, BigUint),
+    window_bits: usize,
+) {
+    type F = BabyBear;
+    type EF = BinomialExtensionField<BabyBear, 4>;
+    type Fp = secp256k1::Fp;
+    let mut builder = AsmBuilder::<F, EF>::bigint_builder();
+    let base = Secp256k1Affine::from_xy(
+        Fp::from_bytes_le(&base.0.to_bytes_le()),
+        Fp::from_bytes_le(&base.1.to_bytes_le()),
+    )
+    .unwrap();
+    let expected = ECPoint {
+        x: expected.0,
+        y: expected.1,
+    }
+    .load_const(&mut builder, SECP256K1_COORD_BITS);
+
+    let s = builder.eval_biguint(scalar);
+    let cached_points = CachedPoints::new(&mut builder, base, window_bits, SECP256K1_COORD_BITS);
+    let res = fixed_scalar_multiply_secp256k1(&mut builder, &cached_points, s);
+    builder.assert_var_array_eq(&res.affine, &expected.affine);
+    builder.halt();
+
+    let program = builder.clone().compile_isa();
+    execute_program(program, vec![]);
+}
+
+#[test]
+fn test_compiler_fixed_ec_mul_simple() {
+    test_fixed_ec_mul(
+        (
+            BigUint::from_str(
+                "55066263022277343669578718895168534326250603453777594175500187360389116729240",
+            )
+            .unwrap(),
+            BigUint::from_str(
+                "32670510020758816978083085130507043184471273380659243275938904335757337482424",
+            )
+            .unwrap(),
+        ),
+        BigUint::from(1u64),
+        (
+            BigUint::from_str(
+                "55066263022277343669578718895168534326250603453777594175500187360389116729240",
+            )
+            .unwrap(),
+            BigUint::from_str(
+                "32670510020758816978083085130507043184471273380659243275938904335757337482424",
+            )
+            .unwrap(),
+        ),
+        4,
+    );
+}
+
+#[test]
+fn test_compiler_fixed_ec_mul_double() {
+    test_fixed_ec_mul(
+        (
+            BigUint::from_str(
+                "55066263022277343669578718895168534326250603453777594175500187360389116729240",
+            )
+            .unwrap(),
+            BigUint::from_str(
+                "32670510020758816978083085130507043184471273380659243275938904335757337482424",
+            )
+            .unwrap(),
+        ),
+        BigUint::from(2u64),
+        (
+            BigUint::from_str(
+                "89565891926547004231252920425935692360644145829622209833684329913297188986597",
+            )
+            .unwrap(),
+            BigUint::from_str(
+                "12158399299693830322967808612713398636155367887041628176798871954788371653930",
+            )
+            .unwrap(),
+        ),
+        4,
+    );
+}
+
+#[test]
+fn test_compiler_fixed_ec_mul_full_period() {
+    let secp256k1_scalar = BigUint::from_str(
+        "115792089237316195423570985008687907852837564279074904382605163141518161494337",
+    )
+    .unwrap();
+    test_fixed_ec_mul(
+        (
+            BigUint::from_str(
+                "55066263022277343669578718895168534326250603453777594175500187360389116729240",
+            )
+            .unwrap(),
+            BigUint::from_str(
+                "32670510020758816978083085130507043184471273380659243275938904335757337482424",
+            )
+            .unwrap(),
+        ),
+        secp256k1_scalar,
+        (BigUint::from(0u64), BigUint::from(0u64)),
+        4,
+    );
+}
+
+#[test]
+fn test_compiler_fixed_ec_mul_zero() {
+    test_fixed_ec_mul(
+        (
+            BigUint::from_str(
+                "55066263022277343669578718895168534326250603453777594175500187360389116729240",
+            )
+            .unwrap(),
+            BigUint::from_str(
+                "32670510020758816978083085130507043184471273380659243275938904335757337482424",
+            )
+            .unwrap(),
+        ),
         BigUint::from(0u64),
         (BigUint::from(0u64), BigUint::from(0u64)),
         4,
