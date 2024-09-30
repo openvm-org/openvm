f25f151f509a62a8da694c84fe41a732f4306c8c
luffykai <lunkai@intrinsictech.xyz>
2024-09-30 16:37:59 -0700

    fp2

--- ecc/primitives/src/field_expression/tests.rs
+++ ecc/primitives/src/field_expression/tests.rs
@@ -1,74 +1,19 @@
-use std::{cell::RefCell, rc::Rc, sync::Arc};
-
-use afs_primitives::{
-    bigint::{check_carry_mod_to_zero::CheckCarryModToZeroSubAir, utils::*},
-    ecc::SampleEcPoints,
-    sub_chip::LocalTraceInstructions,
-    var_range::{bus::VariableRangeCheckerBus, VariableRangeCheckerChip},
-};
+use afs_primitives::{bigint::utils::*, ecc::SampleEcPoints, sub_chip::LocalTraceInstructions};
 use ax_sdk::{
     any_rap_box_vec, config::baby_bear_blake3::BabyBearBlake3Engine, engine::StarkFriEngine,
-    utils::create_seeded_rng,
 };
 use num_bigint_dig::BigUint;
 use p3_air::BaseAir;
 use p3_baby_bear::BabyBear;
 use p3_matrix::dense::RowMajorMatrix;
-use rand::RngCore;
 
-use super::{super::utils::*, ExprBuilder, FieldExprChip, FieldVariableConfig, SymbolicExpr};
+use super::{
+    super::{test_utils::*, utils::*},
+    ExprBuilder, FieldExprChip, SymbolicExpr,
+};
 use crate::field_expression::FieldVariable;
 
 const LIMB_BITS: usize = 8;
-
-pub fn generate_random_biguint(prime: &BigUint) -> BigUint {
-    let mut rng = create_seeded_rng();
-    let len = 32;
-    let x = (0..len).map(|_| rng.next_u32()).collect();
-    let x = BigUint::new(x);
-    x % prime
-}
-
-fn setup(
-    prime: &BigUint,
-) -> (
-    CheckCarryModToZeroSubAir,
-    Arc<VariableRangeCheckerChip>,
-    Rc<RefCell<ExprBuilder>>,
-) {
-    let field_element_bits = 30;
-    let range_bus = 1;
-    let range_decomp = 17; // double needs 17, rests need 16.
-    let range_checker = Arc::new(VariableRangeCheckerChip::new(VariableRangeCheckerBus::new(
-        range_bus,
-        range_decomp,
-    )));
-    let subair = CheckCarryModToZeroSubAir::new(
-        prime.clone(),
-        LIMB_BITS,
-        range_bus,
-        range_decomp,
-        field_element_bits,
-    );
-    let builder = ExprBuilder::new(prime.clone(), LIMB_BITS, 32, range_checker.range_max_bits());
-    (subair, range_checker, Rc::new(RefCell::new(builder)))
-}
-
-#[derive(Clone)]
-pub struct TestConfig;
-impl FieldVariableConfig for TestConfig {
-    fn canonical_limb_bits() -> usize {
-        LIMB_BITS
-    }
-
-    fn max_limb_bits() -> usize {
-        29
-    }
-
-    fn num_limbs_per_field_element() -> usize {
-        32
-    }
-}
 
 #[test]
 fn test_add() {
--- ecc/primitives/src/lib.rs
+++ ecc/primitives/src/lib.rs
@@ -1,3 +1,6 @@
 pub mod field_expression;
+pub mod field_extension;
 
 pub mod utils;
+
+pub mod test_utils;
