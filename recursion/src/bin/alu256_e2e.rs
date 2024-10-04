c60e0ea1ecfe410f73f6e74f87ccedd4d1fbbff3
Alexander Golovanov <Golovanov12345@gmail.com>
2024-10-04 21:37:04 +0200

    Add two redundant clones (mainly to trigger more checks on github)

--- recursion/src/bin/alu256_e2e.rs
+++ recursion/src/bin/alu256_e2e.rs
@@ -154,7 +154,8 @@ fn main() {
 
         #[cfg(feature = "static-verifier")]
         info_span!("Recursive Verify e2e", group = "recursive_verify_e2e").in_scope(|| {
-            let (program, witness_stream) = build_verification_program(vdata, compiler_options);
+            let (program, witness_stream) =
+                build_verification_program(vdata, compiler_options.clone());
             let outer_verifier_sft = gen_vm_program_stark_for_test(
                 program,
                 witness_stream,
