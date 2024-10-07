148f501860d0f297241bf3fafe2dbe6ff6854aa3
Stephen Hwang <stephenh@intrinsictech.xyz>
2024-10-07 18:33:39 -0400

    feat: Adapter + Integration runtime impl for shift and LT

--- vm/src/arch/instructions.rs
+++ vm/src/arch/instructions.rs
@@ -195,3 +195,26 @@ pub enum AluOpcode {
     OR,
     AND,
 }
+
+#[derive(
+    Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, EnumCount, EnumIter, FromRepr, UsizeOpcode,
+)]
+#[opcode_offset = 0x310]
+#[repr(usize)]
+#[allow(non_camel_case_types)]
+pub enum ShiftOpcode {
+    SLL,
+    SRL,
+    SRA,
+}
+
+#[derive(
+    Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, EnumCount, EnumIter, FromRepr, UsizeOpcode,
+)]
+#[opcode_offset = 0x320]
+#[repr(usize)]
+#[allow(non_camel_case_types)]
+pub enum LessThanOpcode {
+    SLT,
+    SLTU,
+}
