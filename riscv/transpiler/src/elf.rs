63f89b3a185d8df280cf0e40987a06b83e62f2dc
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-10-08 18:05:25 -0400

    feat: add decode test

--- riscv/transpiler/src/elf.rs
+++ riscv/transpiler/src/elf.rs
@@ -160,3 +160,20 @@ impl Elf {
         Ok(Elf::new(instructions, entry, base_address, image))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{fs::read, path::PathBuf};
+
+    use axvm_platform::memory::MEM_SIZE;
+    use color_eyre::eyre::Result;
+
+    #[test]
+    fn test_decode_elf() -> Result<()> {
+        let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let data = read(dir.join("data/rv32im-empty-program-elf"))?;
+        let elf = super::Elf::decode(&data, MEM_SIZE as u32)?;
+        dbg!(elf);
+        Ok(())
+    }
+}
