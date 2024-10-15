ec44e27cf3e0a3488abc94af8942856220c95a67
Yi Sun <yi-sun@users.noreply.github.com>
2024-10-15 16:18:50 -0400

    feat: move field_extension to Integration API

---
+++ vm/src/kernels/adapters/mod.rs
@@ -0,0 +1,6 @@
+pub mod native_vectorized_adapter;
+
+use crate::arch::BasicAdapterInterface;
+
+pub type NativeAdapterInterface<T> = BasicAdapterInterface<T, 2, 1, 1, 1>;
+pub type NativeVectorizedAdapterInterface<T, const N: usize> = BasicAdapterInterface<T, 2, 1, N, N>;
