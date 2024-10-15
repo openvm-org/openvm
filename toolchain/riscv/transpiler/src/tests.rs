b2d28d5a9fce582c0761af2070d653548e7069d8
Alexander Golovanov <Golovanov12345@gmail.com>
2024-10-15 20:51:01 +0200

    Add a new rv32 elf and the corresponding runtime test (doesn't work yet because we don't initialize the memory according to the executable)

--- toolchain/riscv/transpiler/src/tests.rs
+++ toolchain/riscv/transpiler/src/tests.rs
@@ -1,5 +1,6 @@
 use std::{fs::read, path::PathBuf};
 
+use ax_sdk::config::setup_tracing;
 use axvm_platform::memory::MEM_SIZE;
 use color_eyre::eyre::Result;
 use p3_baby_bear::BabyBear;
@@ -33,15 +34,55 @@ fn test_generate_program() -> Result<()> {
 }
 
 #[test]
-fn test_runtime() -> Result<()> {
+fn test_tiny_asm_runtime() -> Result<()> {
     let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let data = read(dir.join("data/rv32im-empty-program-elf"))?;
+    let data = read(dir.join("data/rv32im-fib-from-as"))?;
     let elf = Elf::decode(&data, MEM_SIZE as u32)?;
     let instructions = transpile::<BabyBear>(&elf.instructions);
     for instruction in instructions.iter() {
         println!("{:?}", instruction);
     }
-    let program = Program::from_instructions_and_step(&instructions, 4);
+    let program = Program::from_instructions_and_step(
+        &instructions,
+        4,
+        elf.pc_start as usize,
+        elf.pc_base as usize,
+    );
+    let config = VmConfig::rv32();
+    let vm = VirtualMachine::new(config, program, vec![]);
+
+    // TODO: use "execute_and_generate" when it's implemented
+    /*
+    let perm = default_perm();
+    let fri_params = FriParameters::standard_fast();
+
+    let result = vm.execute_and_generate()?;
+
+    for segment_result in result.segment_results {
+        let engine = engine_from_perm(perm.clone(), segment_result.max_log_degree(), fri_params);
+        engine
+            .run_test_impl(&segment_result.air_infos)
+            .expect("Verification failed");
+    }
+    */
+
+    vm.execute()?;
+    Ok(())
+}
+
+#[test]
+fn test_runtime() -> Result<()> {
+    let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let data = read(dir.join("data/rv32im-empty-program-elf-1"))?;
+    let elf = Elf::decode(&data, MEM_SIZE as u32)?;
+    let instructions = transpile::<BabyBear>(&elf.instructions);
+    setup_tracing();
+    let program = Program::from_instructions_and_step(
+        &instructions,
+        4,
+        elf.pc_start as usize,
+        elf.pc_base as usize,
+    );
     let config = VmConfig::rv32();
     let vm = VirtualMachine::new(config, program, vec![]);
 
