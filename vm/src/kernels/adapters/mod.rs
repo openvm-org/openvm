eae4dc67368cdd3c1124a3f178cd9a6d56f125a7
Yi Sun <yi-sun@users.noreply.github.com>
2024-10-20 13:51:54 -0400

    chore: remove native_basic_adapter

--- vm/src/kernels/adapters/mod.rs
+++ vm/src/kernels/adapters/mod.rs
@@ -1,7 +1,5 @@
 // 2 reads, 1 write, imm support
 pub mod native_adapter;
-// 2 reads, 1 write, arbitrary read size, arbitrary write size, no imm support
-pub mod native_basic_adapter;
 // 1 read, 1 write, arbitrary read size, arbitrary write size, no imm support
 pub mod convert_adapter;
 // 2 reads, 1 write, read size = write size = N, no imm support, read/write to address space d
