1d9e9ccda42b348756affdfcc60958ec11eb19ce
Arayi <arayik@intrinsictech.xyz>
2024-10-29 16:19:07 -0400

    feat: port the source files and utility files

--- toolchain/riscv/transpiler/src/tests.rs
+++ toolchain/riscv/transpiler/src/tests.rs
@@ -61,39 +61,6 @@ fn test_rv32im_runtime(elf_path: &str) -> Result<()> {
     Ok(())
 }
 
-#[test]
-fn test_rv32im_riscv_tests_runtime() -> Result<()> {
-    let skip_list = [
-        "rv32ui-p-sh",
-        "rv32ui-p-sb",
-        "rv32ui-p-lh",
-        "rv32ui-p-lb",
-        "rv32ui-p-lbu",
-        "rv32ui-p-lhu",
-    ];
-    let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("rv32im-tests");
-    for entry in read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_file() && path.extension().unwrap_or_default() != "dump" {
-            let file_name = path.file_name().unwrap().to_str().unwrap();
-            if skip_list.contains(&file_name) {
-                continue;
-            }
-            println!("Running: {}", file_name);
-            let result = std::panic::catch_unwind(|| test_rv32im_runtime(path.to_str().unwrap()));
-
-            match result {
-                Ok(Ok(_)) => println!("Passed!: {}", file_name),
-                Ok(Err(e)) => println!("Failed: {} with error: {}", file_name, e),
-                Err(_) => panic!("Panic occurred while running: {}", file_name),
-            }
-        }
-    }
-
-    Ok(())
-}
-
 #[test_case("data/rv32im-fibonacci-program-elf-release")]
 fn test_rv32i_prove(elf_path: &str) -> Result<()> {
     let config = VmConfig::rv32i();
@@ -128,5 +95,57 @@ fn test_terminate_runtime() -> Result<()> {
     let config = VmConfig::rv32i();
     let (vm, exe) = setup_vm_from_elf("data/rv32im-terminate-from-as", config)?;
     air_test(vm, exe);
+    Ok(())
+}
+
+#[test]
+fn test_rv32im_riscv_vector_runtime() -> Result<()> {
+    let skip_list = ["rv32i-p-ma_data", "rv32i-p-fence_i"];
+    let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("rv32im-test-vectors/tests");
+    for entry in read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() && path.extension().unwrap_or_default() == "" {
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            if skip_list.contains(&file_name) {
+                continue;
+            }
+            println!("Running: {}", file_name);
+            let result = std::panic::catch_unwind(|| test_rv32im_runtime(path.to_str().unwrap()));
+
+            match result {
+                Ok(Ok(_)) => println!("Passed!: {}", file_name),
+                Ok(Err(e)) => println!("Failed: {} with error: {}", file_name, e),
+                Err(_) => panic!("Panic occurred while running: {}", file_name),
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_rv32im_riscv_vector_prove() -> Result<()> {
+    let skip_list = ["rv32i-p-ma_data", "rv32i-p-fence_i", "rv32i-p-lui", "rv32i-p-sra"];
+    let dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("rv32im-test-vectors/tests");
+    for entry in read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() && path.extension().unwrap_or_default() == "" {
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            if skip_list.contains(&file_name) {
+                continue;
+            }
+            println!("Running: {}", file_name);
+            let result = std::panic::catch_unwind(|| test_rv32i_prove(path.to_str().unwrap()));
+
+            match result {
+                Ok(Ok(_)) => println!("Passed!: {}", file_name),
+                Ok(Err(e)) => println!("Failed: {} with error: {}", file_name, e),
+                Err(_) => panic!("Panic occurred while running: {}", file_name),
+            }
+        }
+    }
+
     Ok(())
 }
