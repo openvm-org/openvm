522ff15cea64b525c1a71b21f7b4053702160d07
luffykai <lunkai@intrinsictech.xyz>
2024-10-29 21:40:59 -0700

    more lint

--- vm/src/intrinsics/ecc/fp12/sub.rs
+++ vm/src/intrinsics/ecc/fp12/sub.rs
@@ -1,8 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use ax_circuit_primitives::{
-    var_range::VariableRangeCheckerBus,
-};
+use ax_circuit_primitives::var_range::VariableRangeCheckerBus;
 use ax_ecc_primitives::{
     field_expression::{ExprBuilder, ExprBuilderConfig, FieldExpr},
     field_extension::Fp12,
