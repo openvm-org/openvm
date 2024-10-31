61f9c25fd2641b78dc124ed838af4b89b12f0cf7
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-10-30 18:51:33 -0700

    fix: flamegraphs group by

--- ci/scripts/metric_unify/flamegraph.py
+++ ci/scripts/metric_unify/flamegraph.py
@@ -6,22 +6,27 @@ import subprocess
 from utils import FLAMEGRAPHS_DIR, create_flamegraph_dir, get_git_root, create_bench_metrics_dir
 
 
-def get_stack_lines(metrics_file, stack_keys, metric_name):
+def get_stack_lines(metrics_dict, group_by_kvs, stack_keys, metric_name):
     """
-    Filters a metrics_file json for entries that look like:
+    Filters a metrics_dict obtained from json for entries that look like:
         [ { labels: [["key1", "span1;span2"], ["key2", "span3"]], "metric": metric_name, "value": 2 } ]
 
     It will find entries that have all of stack_keys as present in the labels and then concatenate the corresponding values into a single flat stack entry and then add the value at the end.
     It will write a file with one line each for flamegraph.pl or inferno-flamegraph to consume.
     """
     lines = []
-    with open(metrics_file, 'r') as f:
-        data = json.load(f)
 
     # Process counters
-    for counter in data.get('counter', []):
+    for counter in metrics_dict.get('counter', []):
         # list of pairs -> dict
         labels = dict(counter['labels'])
+        filter = False
+        for key, value in group_by_kvs:
+            if key not in labels or labels[key] != value:
+                filter = True
+                break
+        if filter:
+            continue
 
         stack_values = []
         for key in stack_keys:
@@ -41,19 +46,16 @@ def get_stack_lines(metrics_file, stack_keys, metric_name):
     # Currently cycle tracker does not use gauge
     return lines
 
+def create_flamegraph(fname, metrics_dict, group_by_kvs, stack_keys, metric_name, reverse=False):
+    lines = get_stack_lines(metrics_dict, group_by_kvs, stack_keys, metric_name)
 
-def create_flamegraph(metrics_file, stack_keys, metric_name, reverse=False):
-    lines = get_stack_lines(metrics_file, stack_keys, metric_name)
-
-    stack_keys.remove("cycle_tracker_span")
+    suffixes = [key for key in stack_keys if key != "cycle_tracker_span"]
 
     git_root = get_git_root()
     os.chdir(git_root)
     create_flamegraph_dir()
 
-    fname = os.path.splitext(os.path.basename(metrics_file))[0]
-
-    path_prefix = f"{FLAMEGRAPHS_DIR}{fname}.{'.'.join(stack_keys)}.{metric_name}{'.reverse' if reverse else ''}"
+    path_prefix = f"{FLAMEGRAPHS_DIR}{fname}.{'.'.join(suffixes)}.{metric_name}{'.reverse' if reverse else ''}"
     stacks_path = f"{path_prefix}.stacks"
     flamegraph_path = f"{path_prefix}.svg"
 
@@ -62,7 +64,7 @@ def create_flamegraph(metrics_file, stack_keys, metric_name, reverse=False):
             f.write(f"{line}\n")
 
     with open(flamegraph_path, 'w') as f:
-        command = ["inferno-flamegraph", "--title", f"{' '.join(stack_keys)} {metric_name}", stacks_path]
+        command = ["inferno-flamegraph", "--title", f"{fname} {' '.join(suffixes)} {metric_name}", stacks_path]
         if reverse:
             command.append("--reverse")
 
@@ -70,10 +72,31 @@ def create_flamegraph(metrics_file, stack_keys, metric_name, reverse=False):
         print (f"Created flamegraph at {flamegraph_path}")
 
 
-def create_custom_flamegraphs(metrics_file):
+def create_flamegraphs(metrics_file, group_by, stack_keys, metric_name, reverse=False):
+    fname_prefix = os.path.splitext(os.path.basename(metrics_file))[0]
+
+    with open(metrics_file, 'r') as f:
+        metrics_dict = json.load(f)
+    # get different group_by values
+    group_by_values_list = []
+    for counter in metrics_dict.get('counter', []):
+        labels = dict(counter['labels'])
+        try:
+            group_by_values_list.append(tuple([labels[group_by_key] for group_by_key in group_by]))
+        except KeyError:
+            continue
+    # deduplicate group_by values
+    group_by_values_list = list(set(group_by_values_list))
+    for group_by_values in group_by_values_list:
+        group_by_kvs = list(zip(group_by, group_by_values))
+        fname = fname_prefix + '-' + '-'.join(group_by_values)
+        create_flamegraph(fname, metrics_dict, group_by_kvs, stack_keys, metric_name, reverse=reverse)
+
+
+def create_custom_flamegraphs(metrics_file, group_by=["group"]):
     for reverse in [False, True]:
-        create_flamegraph(metrics_file, ["cycle_tracker_span", "dsl_ir", "opcode"], "frequency", reverse=reverse)
-        create_flamegraph(metrics_file, ["cycle_tracker_span", "dsl_ir", "opcode", "air_name"], "cells_used", reverse=reverse)
+        create_flamegraphs(metrics_file, group_by, ["cycle_tracker_span", "dsl_ir", "opcode"], "frequency", reverse=reverse)
+        create_flamegraphs(metrics_file, group_by, ["cycle_tracker_span", "dsl_ir", "opcode", "air_name"], "cells_used", reverse=reverse)
 
 
 def main():
