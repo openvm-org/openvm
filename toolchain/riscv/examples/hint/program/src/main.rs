2b448236a1520593de99efa83a4e3347243e5803
Alexander Golovanov <Golovanov12345@gmail.com>
2024-10-31 03:11:40 +0100

    Fix rust-v hint program

--- toolchain/riscv/examples/hint/program/src/main.rs
+++ toolchain/riscv/examples/hint/program/src/main.rs
@@ -5,7 +5,7 @@ axvm::entry!(main);
 
 pub fn main() {
     axvm::intrinsics::io::hint_input();
-    let vec = axvm::intrinsics::io::read_size_and_vec();
+    let vec = axvm::intrinsics::io::read_vec();
     if vec.iter().sum::<u8>() == 0 {
         axvm::intrinsics::panic();
     }
