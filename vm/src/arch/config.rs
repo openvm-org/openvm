8dd648901397e0f1a1b12188e8082748dda722eb
TlatoaniHJ <dannymittal@gmail.com>
2024-11-01 18:18:44 -0400

    Add FriFold as executor for aggregation configuration

--- vm/src/arch/config.rs
+++ vm/src/arch/config.rs
@@ -205,6 +205,7 @@ impl VmConfig {
         .add_executor(ExecutorName::FieldArithmetic)
         .add_executor(ExecutorName::FieldExtension)
         .add_executor(ExecutorName::Poseidon2)
+        .add_executor(ExecutorName::FriFold)
     }
 
     pub fn read_config_file(file: &str) -> Result<Self, String> {
