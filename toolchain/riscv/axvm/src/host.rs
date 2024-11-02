6f196987033dc2de015292318f03625cc01d55d1
Alexander Golovanov <Golovanov12345@gmail.com>
2024-11-02 02:11:31 +0100

    Add some test

--- toolchain/riscv/axvm/src/host.rs
+++ toolchain/riscv/axvm/src/host.rs
@@ -12,7 +12,19 @@ pub static HINT_STREAM: RefCell<Vec<u8>> = RefCell::new(Vec::new());
 
 /// Set the hints and reset the current hint stream.
 pub fn set_hints(hints: Vec<Vec<u8>>) {
-    HINTS.replace(hints.into_iter().rev().collect());
+    HINTS.replace(
+        hints
+            .into_iter()
+            .rev()
+            .map(|v| {
+                let len = v.len() as u32;
+                len.to_le_bytes()
+                    .into_iter()
+                    .chain(v.iter().cloned())
+                    .collect()
+            })
+            .collect(),
+    );
     HINT_STREAM.replace(Vec::new());
 }
 
@@ -31,4 +43,22 @@ pub fn read_n_bytes(n: usize) -> Vec<u8> {
 pub fn read_u32() -> u32 {
     let bytes: Vec<u8> = read_n_bytes(4);
     u32::from_le_bytes(bytes.try_into().unwrap())
+}
+
+#[cfg(all(test, not(target_os = "zkvm")))]
+mod tests {
+    use alloc::vec;
+
+    use super::*;
+    use crate::io::read_vec;
+
+    #[test]
+    fn test_read_hints() {
+        set_hints(vec![vec![1, 2, 3, 4]; 3]);
+        hint_input();
+        assert_eq!(read_u32(), 4);
+        hint_input();
+        assert_eq!(read_n_bytes(8), vec![4, 0, 0, 0, 1, 2, 3, 4]);
+        assert_eq!(read_vec(), vec![1, 2, 3, 4]);
+    }
 }
