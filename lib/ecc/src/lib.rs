5cab69d07f20145293b404d5be10caae720956ee
luffykai <lunkai@intrinsictech.xyz>
2024-11-05 15:17:11 -0800

    move

--- lib/ecc/src/lib.rs
+++ lib/ecc/src/lib.rs
@@ -1,1 +1,14 @@
 pub mod sw;
+
+use axvm::intrinsics::IntModN;
+#[derive(Eq, PartialEq, Clone)]
+pub struct EcPoint {
+    pub x: IntModN,
+    pub y: IntModN,
+}
+
+impl EcPoint {
+    pub fn is_identity(&self) -> bool {
+        self.x == IntModN::zero() && self.y == IntModN::zero()
+    }
+}
--- lib/ecc/src/sw.rs
+++ lib/ecc/src/sw.rs
@@ -1,16 +1,6 @@
 use axvm::intrinsics::IntModN;
 
-#[derive(Eq, PartialEq, Clone)]
-pub struct EcPoint {
-    pub x: IntModN,
-    pub y: IntModN,
-}
-
-impl EcPoint {
-    pub fn is_identity(&self) -> bool {
-        self.x == IntModN::zero() && self.y == IntModN::zero()
-    }
-}
+use super::EcPoint;
 
 // Two points can be equal or not.
 pub fn add(p1: &EcPoint, p2: &EcPoint) -> EcPoint {
