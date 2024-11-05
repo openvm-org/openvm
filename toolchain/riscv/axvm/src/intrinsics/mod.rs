545c879c3f6d615be402e5267bf1bbea40d0138f
Arayi <arayik@intrinsictech.xyz>
2024-11-05 13:59:36 -0500

    feat: u256 not zkvm version

--- toolchain/riscv/axvm/src/intrinsics/mod.rs
+++ toolchain/riscv/axvm/src/intrinsics/mod.rs
@@ -1,10 +1,12 @@
 //! Functions that call custom instructions that use axVM intrinsic instructions.
 
 mod hash;
+mod int256;
 /// Library functions for user input/output.
 #[cfg(target_os = "zkvm")]
 mod io;
 
 pub use hash::*;
+pub use int256::*;
 #[cfg(target_os = "zkvm")]
 pub use io::*;
