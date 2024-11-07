314f90906fabdc684c621f4480217c6be432f3e7
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-11-06 22:00:46 -0800

    bench: switch revm_contract_deployment to use native_keccak256

--- benchmarks/programs/revm_contract_deployment/src/main.rs
+++ benchmarks/programs/revm_contract_deployment/src/main.rs
@@ -47,16 +47,4 @@ fn main() {
         .build();
 
     let _result = evm.transact().unwrap();
-    // let Some(storage0) = result
-    //     .state
-    //     .get(&address)
-    //     .expect("Contract not found")
-    //     .storage
-    //     .get::<U256>(&Default::default())
-    // else {
-    //     panic!("Failed to write storage in the init code: {result:#?}");
-    // };
-
-    // tracing::info!("storage U256(0) at {address}:  {storage0:#?}");
-    // assert_eq!(storage0.present_value(), U256::from(0x42), "{result:#?}");
 }
