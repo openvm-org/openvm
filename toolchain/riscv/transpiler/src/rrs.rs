9bd37e0752dfc3ba16fe663bceb068c0e61a907f
Arayi <arayik@intrinsictech.xyz>
2024-11-07 16:18:01 -0500

    feat: transpile pairing opcodes

--- toolchain/riscv/transpiler/src/rrs.rs
+++ toolchain/riscv/transpiler/src/rrs.rs
@@ -2,17 +2,17 @@ use std::marker::PhantomData;
 
 use axvm_instructions::{
     instruction::Instruction, riscv::RV32_REGISTER_NUM_LIMBS, utils::isize_to_field, BaseAluOpcode,
-    BranchEqualOpcode, BranchLessThanOpcode, DivRemOpcode, EccOpcode, LessThanOpcode, MulHOpcode,
-    MulOpcode, PhantomInstruction, Rv32AuipcOpcode, Rv32BaseAlu256Opcode, Rv32BranchEqual256Opcode,
-    Rv32HintStoreOpcode, Rv32JalLuiOpcode, Rv32JalrOpcode, Rv32KeccakOpcode, Rv32LessThan256Opcode,
-    Rv32LoadStoreOpcode, Rv32ModularArithmeticOpcode, Rv32Mul256Opcode, Rv32Shift256Opcode,
-    ShiftOpcode, UsizeOpcode,
+    BranchEqualOpcode, BranchLessThanOpcode, DivRemOpcode, EccOpcode, Fp12Opcode, LessThanOpcode,
+    MulHOpcode, MulOpcode, PairingOpcode, PhantomInstruction, Rv32AuipcOpcode,
+    Rv32BaseAlu256Opcode, Rv32BranchEqual256Opcode, Rv32HintStoreOpcode, Rv32JalLuiOpcode,
+    Rv32JalrOpcode, Rv32KeccakOpcode, Rv32LessThan256Opcode, Rv32LoadStoreOpcode,
+    Rv32ModularArithmeticOpcode, Rv32Mul256Opcode, Rv32Shift256Opcode, ShiftOpcode, UsizeOpcode,
 };
 use axvm_platform::constants::{
     Custom0Funct3::{self, *},
     Custom1Funct3::{self, *},
-    Int256Funct7, ModArithBaseFunct7, PhantomImm, SwBaseFunct7, CUSTOM_0, CUSTOM_1,
-    MODULAR_ARITHMETIC_MAX_KINDS, SHORT_WEIERSTRASS_MAX_KINDS,
+    Int256Funct7, ModArithBaseFunct7, PairingBaseFunct7, PhantomImm, SwBaseFunct7, CUSTOM_0,
+    CUSTOM_1, MODULAR_ARITHMETIC_MAX_KINDS, PAIRING_MAX_KINDS, SHORT_WEIERSTRASS_MAX_KINDS,
 };
 use p3_field::PrimeField32;
 use rrs_lib::{
@@ -392,6 +392,48 @@ fn process_custom_instruction<F: PrimeField32>(instruction_u32: u32) -> Instruct
                     let curve_idx_shift = ((dec_insn.funct7 as u8) / SHORT_WEIERSTRASS_MAX_KINDS)
                         * SHORT_WEIERSTRASS_MAX_KINDS;
                     let global_opcode = global_opcode + curve_idx_shift as usize;
+                    Some(from_r_type(global_opcode, 2, &dec_insn))
+                }
+                Some(Pairing) => {
+                    // pairing
+                    let dec_insn = RType::new(instruction_u32);
+                    let base_funct7 = (dec_insn.funct7 as u8) % PAIRING_MAX_KINDS;
+                    let global_opcode = match PairingBaseFunct7::from_repr(base_funct7) {
+                        Some(PairingBaseFunct7::MillerDoubleStep) => {
+                            PairingOpcode::MILLER_DOUBLE_STEP as usize + PairingOpcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::MillerDoubleAndAddStep) => {
+                            PairingOpcode::MILLER_DOUBLE_AND_ADD_STEP as usize + PairingOpcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::Fp12Mul) => {
+                            Fp12Opcode::MUL as usize + Fp12Opcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::EvaluateLine) => {
+                            PairingOpcode::EVALUATE_LINE as usize + PairingOpcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::Mul013By013) => {
+                            PairingOpcode::MUL_013_BY_013 as usize + PairingOpcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::MulBy013) => {
+                            PairingOpcode::MUL_BY_013 as usize + PairingOpcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::MulBy01234) => {
+                            PairingOpcode::MUL_BY_01234 as usize + PairingOpcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::Mul023By023) => {
+                            PairingOpcode::MUL_023_BY_023 as usize + PairingOpcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::MulBy023) => {
+                            PairingOpcode::MUL_BY_023 as usize + PairingOpcode::default_offset()
+                        }
+                        Some(PairingBaseFunct7::MulBy02345) => {
+                            PairingOpcode::MUL_BY_02345 as usize + PairingOpcode::default_offset()
+                        }
+                        _ => unimplemented!(),
+                    };
+                    let pairing_idx_shift = ((dec_insn.funct7 as u8) / PAIRING_MAX_KINDS)
+                        * PAIRING_MAX_KINDS;
+                    let global_opcode = global_opcode + pairing_idx_shift as usize;
                     Some(from_r_type(global_opcode, 2, &dec_insn))
                 }
                 _ => unimplemented!(),
