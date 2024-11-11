454f32e86aa71c7dae9ef46c25c44473751ffc77
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-11-11 12:08:06 +0700

    fmt

--- cargo-axiom/build.rs
+++ cargo-axiom/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    vergen::EmitBuilder::builder().build_timestamp().git_sha(true).emit().unwrap();
+    vergen::EmitBuilder::builder()
+        .build_timestamp()
+        .git_sha(true)
+        .emit()
+        .unwrap();
 }
