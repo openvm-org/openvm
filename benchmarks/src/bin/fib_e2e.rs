31622b66eec8e2e404fb73f7a6bc8f89019b1be5
Xinding Wei <weixinding@gmail.com>
2024-11-11 14:16:36 -0800

    Change fib_e2e FRI parameters & unify benchmark metric format

--- benchmarks/src/bin/fib_e2e.rs
+++ benchmarks/src/bin/fib_e2e.rs
@@ -1,7 +1,10 @@
 use std::sync::Arc;
 
 use ax_stark_sdk::{
-    ax_stark_backend::config::StarkGenericConfig,
+    ax_stark_backend::{
+        config::{Com, Domain, PcsProof, PcsProverData, StarkGenericConfig, Val},
+        prover::types::Proof,
+    },
     bench::run_with_metric_collection,
     config::{
         baby_bear_poseidon2::{BabyBearPoseidon2Config, BabyBearPoseidon2Engine},
@@ -23,7 +26,7 @@ use axvm_benchmarks::utils::build_bench_program;
 use axvm_circuit::{
     arch::{
         instructions::{exe::AxVmExe, program::DEFAULT_MAX_NUM_PUBLIC_VALUES},
-        VmConfig, VmExecutor,
+        SingleSegmentVmExecutor, VmConfig, VmExecutor,
     },
     prover::{local::VmLocalProver, ContinuationVmProver, SingleSegmentVmProver},
     system::program::trace::AxVmCommittedExe,
@@ -32,7 +35,8 @@ use axvm_native_compiler::conversion::CompilerOptions;
 use axvm_recursion::hints::Hintable;
 use axvm_transpiler::axvm_platform::bincode;
 use eyre::Result;
-use p3_field::AbstractField;
+use metrics::counter;
+use p3_field::{AbstractField, PrimeField32};
 use tracing::info_span;
 
 type OuterSC = BabyBearPoseidon2OuterConfig;
@@ -50,10 +54,10 @@ async fn main() -> Result<()> {
     let axiom_vm_pk = {
         let axiom_vm_config = AxiomVmConfig {
             max_num_user_public_values: NUM_PUBLIC_VALUES,
-            app_fri_params: standard_fri_params_with_100_bits_conjectured_security(1),
+            app_fri_params: standard_fri_params_with_100_bits_conjectured_security(2),
             leaf_fri_params: standard_fri_params_with_100_bits_conjectured_security(2),
-            internal_fri_params: standard_fri_params_with_100_bits_conjectured_security(3),
-            root_fri_params: standard_fri_params_with_100_bits_conjectured_security(3),
+            internal_fri_params: standard_fri_params_with_100_bits_conjectured_security(2),
+            root_fri_params: standard_fri_params_with_100_bits_conjectured_security(2),
             app_vm_config: VmConfig::rv32im()
                 .with_num_public_values(NUM_PUBLIC_VALUES)
                 .with_max_segment_len(segment_len),
@@ -74,7 +78,9 @@ async fn main() -> Result<()> {
         .collect();
     run_with_metric_collection("OUTPUT_PATH", || {
         let app_proofs = info_span!("App VM", group = "app_vm").in_scope(|| {
-            let vm = VmExecutor::new(axiom_vm_pk.app_vm_config.clone());
+            let mut vm_config = axiom_vm_pk.app_vm_config.clone();
+            vm_config.collect_metrics = true;
+            let vm = VmExecutor::new(vm_config);
             let execution_results = vm
                 .execute_segments(app_committed_exe.exe.clone(), vec![app_input.clone()])
                 .unwrap();
@@ -85,6 +91,7 @@ async fn main() -> Result<()> {
                 axiom_vm_pk.app_vm_pk.clone(),
                 app_committed_exe.clone(),
             );
+            counter!("fri.log_blowup").absolute(axiom_vm_pk.app_fri_params.log_blowup as u64);
             ContinuationVmProver::prove(&app_prover, vec![app_input])
         });
 
@@ -102,34 +109,35 @@ async fn main() -> Result<()> {
                 .enumerate()
                 .map(|(leaf_idx, input)| {
                     info_span!("leaf verifier proof", index = leaf_idx).in_scope(|| {
-                        SingleSegmentVmProver::prove(&leaf_prover, input.write_to_stream())
+                        single_segment_execute_and_prove(&leaf_prover, input.write_to_stream())
                     })
                 })
                 .collect::<Vec<_>>()
         });
-        let final_internal_proof = info_span!("internal verifier", group = "internal_verifier")
-            .in_scope(|| {
-                let internal_prover = VmLocalProver::<SC, BabyBearPoseidon2Engine>::new(
-                    axiom_vm_pk.internal_fri_params,
-                    axiom_vm_pk.internal_vm_config.clone(),
-                    axiom_vm_pk.internal_vm_pk.clone(),
-                    axiom_vm_pk.internal_committed_exe.clone(),
+        let final_internal_proof = {
+            let internal_prover = VmLocalProver::<SC, BabyBearPoseidon2Engine>::new(
+                axiom_vm_pk.internal_fri_params,
+                axiom_vm_pk.internal_vm_config.clone(),
+                axiom_vm_pk.internal_vm_pk.clone(),
+                axiom_vm_pk.internal_committed_exe.clone(),
+            );
+            let mut internal_node_idx = 0;
+            let mut internal_node_height = 0;
+            let mut proofs = leaf_proofs;
+            while proofs.len() > 1 {
+                let internal_inputs = InternalVmVerifierInput::chunk_leaf_or_internal_proofs(
+                    axiom_vm_pk
+                        .internal_committed_exe
+                        .committed_program
+                        .prover_data
+                        .commit
+                        .into(),
+                    &proofs,
+                    NUM_CHILDREN_INTERNAL,
                 );
-                let mut internal_node_idx = 0;
-                let mut internal_node_height = 0;
-                let mut proofs = leaf_proofs;
-                while proofs.len() > 1 {
-                    let internal_inputs = InternalVmVerifierInput::chunk_leaf_or_internal_proofs(
-                        axiom_vm_pk
-                            .internal_committed_exe
-                            .committed_program
-                            .prover_data
-                            .commit
-                            .into(),
-                        &proofs,
-                        NUM_CHILDREN_INTERNAL,
-                    );
-                    proofs = internal_inputs
+                let group = format!("internal_verifier_height_{}", internal_node_height);
+                proofs = info_span!("internal verifier", group = group).in_scope(|| {
+                    internal_inputs
                         .into_iter()
                         .map(|input| {
                             let ret = info_span!(
@@ -138,16 +146,17 @@ async fn main() -> Result<()> {
                                 height = internal_node_height
                             )
                             .in_scope(|| {
-                                SingleSegmentVmProver::prove(&internal_prover, input.write())
+                                single_segment_execute_and_prove(&internal_prover, input.write())
                             });
                             internal_node_idx += 1;
                             ret
                         })
-                        .collect();
-                    internal_node_height += 1;
-                }
-                proofs.pop().unwrap()
-            });
+                        .collect()
+                });
+                internal_node_height += 1;
+            }
+            proofs.pop().unwrap()
+        };
         #[allow(unused_variables)]
         let root_proof = info_span!("root verifier", group = "root_verifier").in_scope(move || {
             let root_prover = VmLocalProver::<OuterSC, BabyBearPoseidon2OuterEngine>::new(
@@ -160,7 +169,7 @@ async fn main() -> Result<()> {
                 proofs: vec![final_internal_proof],
                 public_values: app_proofs.user_public_values.public_values,
             };
-            SingleSegmentVmProver::prove(&root_prover, root_input.write())
+            single_segment_execute_and_prove(&root_prover, root_input.write())
         });
     });
 
@@ -179,4 +188,25 @@ fn generate_fib_exe(app_fri_params: FriParameters) -> Arc<AxVmCommittedExe<SC>> 
 
     let app_engine = BabyBearPoseidon2Engine::new(app_fri_params);
     Arc::new(AxVmCommittedExe::<SC>::commit(exe, app_engine.config.pcs()))
+}
+
+fn single_segment_execute_and_prove<SC: StarkGenericConfig, E: StarkFriEngine<SC>>(
+    prover: &VmLocalProver<SC, E>,
+    input: Vec<Vec<Val<SC>>>,
+) -> Proof<SC>
+where
+    Val<SC>: PrimeField32,
+    Domain<SC>: Send + Sync,
+    PcsProverData<SC>: Send + Sync,
+    Com<SC>: Send + Sync,
+    SC::Challenge: Send + Sync,
+    PcsProof<SC>: Send + Sync,
+{
+    counter!("fri.log_blowup").absolute(prover.fri_parameters.log_blowup as u64);
+    let mut vm_config = prover.vm_config.clone();
+    vm_config.collect_metrics = true;
+    let vm = SingleSegmentVmExecutor::new(vm_config);
+    vm.execute(prover.committed_exe.exe.clone(), input.clone())
+        .unwrap();
+    SingleSegmentVmProver::prove(prover, input)
 }
