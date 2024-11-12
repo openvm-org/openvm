7dfa032c4c3ade95ee21f6c185d639bd18747fc1
Stephen Hwang <stephenh@intrinsictech.xyz>
2024-11-12 09:52:42 -0500

    fix: run summarize on dev image

--- .github/workflows/stephen.bash
+++
@@ -1,5 +0,0 @@
-names=$(echo '[{"name":"verify_fibair","app_log_blowup":1,"agg_log_blowup":1},{"name":"verify_fibair","app_log_blowup":2,"agg_log_blowup":2},{"name":"fibonacci","app_log_blowup":2,"agg_log_blowup":2},{"name":"revm_transfer","app_log_blowup":2,"agg_log_blowup":2},{"name":"regex","app_log_blowup":2,"agg_log_blowup":2},{"name":"base64_json","app_log_blowup":2,"agg_log_blowup":2}]' | jq -r '.[].name' | sort -u)
-echo $names
-while read name; do
-    echo "Processing results for benchmark: $name"
-done <<< "$names"
