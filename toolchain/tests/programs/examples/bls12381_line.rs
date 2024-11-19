95befc4699a67c4ef58f4bfc24346170eaa23226
Yu Jiang Tham <yoojbruin@gmail.com>
2024-11-19 01:02:29 -0500

    Update bls test

--- toolchain/tests/programs/examples/bls12381_line.rs
+++ toolchain/tests/programs/examples/bls12381_line.rs
@@ -2,13 +2,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use axvm::io::read_vec;
-use axvm_algebra::IntMod;
+use axvm_algebra::{field::FieldExtension, IntMod};
 use axvm_ecc::{
-    field::FieldExtension,
-    pairing::{
-        bls12381::{Bls12381Fp, Bls12381Fp12, Bls12381Fp2, Bls12381Intrinsic},
-        EvaluatedLine, LineMulMType,
-    },
+    bls12_381::{Bls12_381, Fp12, Fp2},
+    pairing::{EvaluatedLine, LineMulMType},
 };
 
 axvm::entry!(main);
@@ -18,10 +15,10 @@ fn test_mul_023_by_023(io: &[u8]) {
     let l1 = &io[(3 * 16) * 4..(3 * 16) * 8];
     let expected = &io[(3 * 16) * 8..(3 * 16) * 18];
 
-    let l0_cast = unsafe { &*(l0.as_ptr() as *const EvaluatedLine<Bls12381Fp, Bls12381Fp2>) };
-    let l1_cast = unsafe { &*(l1.as_ptr() as *const EvaluatedLine<Bls12381Fp, Bls12381Fp2>) };
+    let l0_cast = unsafe { &*(l0.as_ptr() as *const EvaluatedLine<Fp2>) };
+    let l1_cast = unsafe { &*(l1.as_ptr() as *const EvaluatedLine<Fp2>) };
 
-    let r = Bls12381Intrinsic::mul_023_by_023(l0_cast.clone(), l1_cast.clone());
+    let r = Bls12_381::mul_023_by_023(&l0_cast, &l1_cast);
     let mut r_bytes = [0u8; (3 * 16) * 10];
     let mut i = 0;
     for x in r {
@@ -37,10 +34,10 @@ fn test_mul_by_02345(io: &[u8]) {
     let x = &io[(3 * 16) * 30..(3 * 16) * 40];
     let expected = &io[(3 * 16) * 40..(3 * 16) * 52];
 
-    let f_cast = unsafe { &*(f.as_ptr() as *const Bls12381Fp12) };
-    let x_cast = unsafe { &*(x.as_ptr() as *const [Bls12381Fp2; 5]) };
+    let f_cast = unsafe { &*(f.as_ptr() as *const Fp12) };
+    let x_cast = unsafe { &*(x.as_ptr() as *const [Fp2; 5]) };
 
-    let r = Bls12381Intrinsic::mul_by_02345(f_cast.clone(), x_cast.clone());
+    let r = Bls12_381::mul_by_02345(&f_cast, &x_cast);
     let mut r_bytes = [0u8; (3 * 16) * 12];
     let mut i = 0;
     for x in r.to_coeffs() {
