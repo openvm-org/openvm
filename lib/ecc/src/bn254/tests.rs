dfe124048ec21002f2e8791e0ade9624b527d9a9
Yu Jiang Tham <yoojbruin@gmail.com>
2024-11-19 20:25:40 -0500

    Additional tests (all failing)

--- lib/ecc/src/bn254/tests.rs
+++ lib/ecc/src/bn254/tests.rs
@@ -1,10 +1,10 @@
 use axvm_algebra::{field::FieldExtension, IntMod};
 use group::ff::Field;
-use halo2curves_axiom::bn256::{Fq, Fq12, Fq2};
+use halo2curves_axiom::bn256::{Fq, Fq12, Fq2, Fq6};
 use rand::{rngs::StdRng, SeedableRng};
 
 use super::{Fp, Fp12, Fp2};
-use crate::bn254::fp2_invert_assign;
+use crate::bn254::{fp2_invert_assign, fp6_square_assign, fp_invert_assign};
 
 fn convert_fq_to_bn254fp(x: Fq) -> Fp {
     let bytes = x.0.map(|y| y.to_le_bytes()).concat();
@@ -42,11 +42,40 @@ fn test_fp12_invert() {
 #[test]
 fn test_fp2_invert() {
     let mut rng = StdRng::seed_from_u64(25);
-    // let fq2 = Fq2::random(&mut rng);
-    let fq2 = Fq2::one();
+    let fq2 = Fq2::random(&mut rng);
     let fq2_inv = fq2.invert().unwrap();
 
     let mut fp2 = convert_fq2_to_bn254fp2(fq2);
     fp2_invert_assign(&mut fp2);
     assert_eq!(fp2, convert_fq2_to_bn254fp2(fq2_inv));
+}
+
+#[test]
+fn test_fp_invert() {
+    let mut rng = StdRng::seed_from_u64(35);
+    let fq = Fq::random(&mut rng);
+    let fq_inv = fq.invert().unwrap();
+
+    let mut fp = convert_fq_to_bn254fp(fq);
+    fp_invert_assign(&mut fp);
+    assert_eq!(fp, convert_fq_to_bn254fp(fq_inv));
+}
+
+#[test]
+fn test_fp6_square() {
+    let mut rng = StdRng::seed_from_u64(45);
+    let fq6 = Fq6::random(&mut rng);
+    let fq6_sq = fq6.square();
+
+    let fp6c0 = convert_fq2_to_bn254fp2(fq6.c0);
+    let fp6c1 = convert_fq2_to_bn254fp2(fq6.c1);
+    let fp6c2 = convert_fq2_to_bn254fp2(fq6.c2);
+    let mut fp6 = [fp6c0, fp6c1, fp6c2];
+    fp6_square_assign(&mut fp6);
+
+    let fp6_sqc0 = convert_fq2_to_bn254fp2(fq6_sq.c0);
+    let fp6_sqc1 = convert_fq2_to_bn254fp2(fq6_sq.c1);
+    let fp6_sqc2 = convert_fq2_to_bn254fp2(fq6_sq.c2);
+    let fp6_sq = [fp6_sqc0, fp6_sqc1, fp6_sqc2];
+    assert_eq!(fp6, fp6_sq);
 }
