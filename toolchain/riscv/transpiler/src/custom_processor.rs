66fc2b763b807c959f64e3d94955884452cde301
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-11-19 18:08:59 -0800

    Update toolchain/riscv/transpiler/src/custom_processor.rs

--- toolchain/riscv/transpiler/src/custom_processor.rs
+++ toolchain/riscv/transpiler/src/custom_processor.rs
@@ -1,5 +1,5 @@
 use axvm_instructions::instruction::Instruction;
 
 pub trait CustomInstructionProcessor<F> {
-    fn process_custom(&self, instruction_stream: &[u32]) -> Option<(Instruction<F>, usize)>; // (Instruction, how many u32's it consists of)
+    fn process_custom(&self, instruction_stream: &[u32]) -> Option<(Instruction<F>, usize)>; // (`Instruction`, how many u32's to advance the RISC-V instruction stream by)
 }
