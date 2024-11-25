54d2f7df5f254d425a07d7f39ca4ef0d389a8ad1
Yu Jiang Tham <yoojbruin@gmail.com>
2024-11-25 15:31:16 -0500

    Consolidate example programs

--- toolchain/tests/src/pairing_tests.rs
+++ toolchain/tests/src/pairing_tests.rs
@@ -9,7 +9,7 @@ use ax_ecc_execution::axvm_ecc::{
     AffinePoint,
 };
 use ax_stark_sdk::ax_stark_backend::p3_field::AbstractField;
-use axvm_circuit::arch::{VmConfig, VmExecutor};
+use axvm_circuit::arch::{EcCurve, PairingCurve, VmConfig, VmExecutor};
 use eyre::Result;
 use p3_baby_bear::BabyBear;
 use rand::SeedableRng;
@@ -32,14 +32,13 @@ mod bn254 {
         },
         curves::bn254::Bn254,
     };
-    use axvm_circuit::arch::{EcCurve, PairingCurve};
     use axvm_ecc_constants::BN254;
 
     use super::*;
 
     #[test]
     fn test_bn254_fp12_mul() -> Result<()> {
-        let elf = build_example_program("bn254_fp12_mul")?;
+        let elf = build_example_program("fp12_mul")?;
         let executor = VmExecutor::<F>::new(
             VmConfig::rv32im()
                 .add_pairing_support(vec![PairingCurve::Bn254])
@@ -66,7 +65,7 @@ mod bn254 {
 
     #[test]
     fn test_bn254_line_functions() -> Result<()> {
-        let elf = build_example_program("bn254_line")?;
+        let elf = build_example_program("pairing_line")?;
         let executor = VmExecutor::<F>::new(VmConfig::rv32im().add_canonical_pairing_curves());
 
         let mut rng = rand::rngs::StdRng::seed_from_u64(2);
@@ -109,7 +108,7 @@ mod bn254 {
 
     #[test]
     fn test_bn254_miller_step() -> Result<()> {
-        let elf = build_example_program("bn254_miller_step")?;
+        let elf = build_example_program("pairing_miller_step")?;
         let executor = VmExecutor::<F>::new(VmConfig::rv32im().add_canonical_pairing_curves());
 
         let mut rng = rand::rngs::StdRng::seed_from_u64(20);
@@ -143,7 +142,7 @@ mod bn254 {
 
     #[test]
     fn test_bn254_miller_loop() -> Result<()> {
-        let elf = build_example_program("bn254_miller_loop")?;
+        let elf = build_example_program("pairing_miller_loop")?;
         let exe = axvm_circuit::arch::instructions::exe::AxVmExe::<F>::from(elf.clone());
         let enabled_moduli = exe
             .custom_op_config
@@ -199,18 +198,45 @@ mod bls12_381 {
     use ax_ecc_execution::{
         axvm_ecc::{
             halo2curves::bls12_381::{Fq12, Fq2, G1Affine, G2Affine},
-            pairing::LineMulMType,
+            pairing::{LineMulMType, MillerStep},
             AffinePoint,
         },
         curves::bls12_381::Bls12_381,
     };
-    use axvm_circuit::arch::PairingCurve;
+    use axvm_ecc_constants::BLS12381;
 
     use super::*;
 
     #[test]
+    fn test_bls12_381_fp12_mul() -> Result<()> {
+        let elf = build_example_program("fp12_mul")?;
+        let executor = VmExecutor::<F>::new(
+            VmConfig::rv32im()
+                .add_pairing_support(vec![PairingCurve::Bls12_381])
+                .add_ecc_support(vec![EcCurve::Bls12_381])
+                .add_modular_support(vec![BLS12381.MODULUS.clone()])
+                .add_complex_ext_support(vec![BLS12381.MODULUS.clone()]),
+        );
+
+        let mut rng = rand::rngs::StdRng::seed_from_u64(50);
+        let f0 = Fq12::random(&mut rng);
+        let f1 = Fq12::random(&mut rng);
+        let r = f0 * f1;
+
+        let io = [f0, f1, r]
+            .into_iter()
+            .flat_map(|fp12| fp12.to_coeffs())
+            .flat_map(|fp2| fp2.to_bytes())
+            .map(AbstractField::from_canonical_u8)
+            .collect::<Vec<_>>();
+
+        executor.execute(elf, vec![io])?;
+        Ok(())
+    }
+
+    #[test]
     fn test_bls12_381_line_functions() -> Result<()> {
-        let elf = build_example_program("bls12381_line")?;
+        let elf = build_example_program("pairing_line")?;
         let executor = VmExecutor::<F>::new(
             VmConfig::rv32im().add_pairing_support(vec![PairingCurve::Bls12_381]),
         );
@@ -244,6 +270,42 @@ mod bls12_381 {
             .chain(x)
             .chain(r1.to_coeffs())
             .flat_map(|fp2| fp2.to_coeffs())
+            .flat_map(|fp| fp.to_bytes())
+            .map(AbstractField::from_canonical_u8)
+            .collect::<Vec<_>>();
+
+        let io_all = io0.into_iter().chain(io1).collect::<Vec<_>>();
+
+        executor.execute(elf, vec![io_all])?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_bls12_381_miller_step() -> Result<()> {
+        let elf = build_example_program("pairing_miller_step")?;
+        let executor = VmExecutor::<F>::new(
+            VmConfig::rv32im().add_pairing_support(vec![PairingCurve::Bls12_381]),
+        );
+
+        let mut rng = rand::rngs::StdRng::seed_from_u64(88);
+        let S = G2Affine::random(&mut rng);
+        let Q = G2Affine::random(&mut rng);
+
+        let s = AffinePoint::new(S.x, S.y);
+        let q = AffinePoint::new(Q.x, Q.y);
+
+        // Test miller_double_step
+        let (pt, l) = Bls12_381::miller_double_step(&s);
+        let io0 = [s.x, s.y, pt.x, pt.y, l.b, l.c]
+            .into_iter()
+            .flat_map(|fp| fp.to_bytes())
+            .map(AbstractField::from_canonical_u8)
+            .collect::<Vec<_>>();
+
+        // Test miller_double_and_add_step
+        let (pt, l0, l1) = Bls12_381::miller_double_and_add_step(&s, &q);
+        let io1 = [s.x, s.y, q.x, q.y, pt.x, pt.y, l0.b, l0.c, l1.b, l1.c]
+            .into_iter()
             .flat_map(|fp| fp.to_bytes())
             .map(AbstractField::from_canonical_u8)
             .collect::<Vec<_>>();
