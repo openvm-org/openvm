96585362cf781d21327444a59b7cbe1188f3bb54
Yi Sun <yi-sun@users.noreply.github.com>
2024-12-01 01:04:04 -0500

    fix: faulty rebase

--- extensions/pairing/circuit/src/pairing_chip/line/d_type/mul_013_by_013.rs
+++ extensions/pairing/circuit/src/pairing_chip/line/d_type/mul_013_by_013.rs
@@ -5,10 +5,7 @@ use ax_circuit_primitives::var_range::VariableRangeCheckerBus;
 use ax_mod_circuit_builder::{ExprBuilder, ExprBuilderConfig, FieldExpr, FieldExpressionCoreChip};
 use ax_stark_backend::p3_field::PrimeField32;
 use axvm_algebra_circuit::Fp2;
-use axvm_circuit::{
-    arch::VmChipWrapper, rv32im::adapters::Rv32VecHeapAdapterChip,
-    system::memory::MemoryControllerRef,
-};
+use axvm_circuit::{arch::VmChipWrapper, system::memory::MemoryControllerRef};
 use axvm_circuit_derive::InstructionExecutor;
 use axvm_instructions::PairingOpcode;
 use axvm_rv32_adapters::Rv32VecHeapAdapterChip;
