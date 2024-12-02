74a7a35a6880e419dcca2c10d6e0cb119aad8917
Stephen Hwang <stephenh@intrinsictech.xyz>
2024-12-02 18:59:42 -0500

    feat: axvm-sdk global struct API

--- crates/axvm-sdk/src/lib.rs
+++ crates/axvm-sdk/src/lib.rs
@@ -1,11 +1,28 @@
 extern crate core;
 
-use ax_stark_sdk::config::{
-    baby_bear_poseidon2::BabyBearPoseidon2Config,
-    baby_bear_poseidon2_outer::BabyBearPoseidon2OuterConfig,
+use std::{marker::PhantomData, sync::Arc};
+
+use ax_stark_sdk::{
+    ax_stark_backend::{prover::types::Proof, verifier::VerificationError, Chip},
+    config::{
+        baby_bear_poseidon2::BabyBearPoseidon2Config,
+        baby_bear_poseidon2_outer::BabyBearPoseidon2OuterConfig,
+    },
+};
+use axvm_circuit::{
+    arch::{instructions::exe::AxVmExe, ExecutionError, VmConfig},
+    system::program::trace::AxVmCommittedExe,
 };
 use axvm_native_recursion::types::InnerConfig;
+use axvm_transpiler::{elf::Elf, transpiler::Transpiler};
+use config::AppConfig;
+use keygen::AppProvingKey;
 use p3_baby_bear::BabyBear;
+#[cfg(feature = "static-verifier")]
+use {
+    axvm_native_recursion::halo2::verifier::Halo2VerifierCircuit, config::AggConfig,
+    keygen::AggProvingKey, snark_verifier_sdk::Snark,
+};
 
 pub mod commit;
 pub mod config;
@@ -21,3 +38,99 @@ pub(crate) type SC = BabyBearPoseidon2Config;
 pub(crate) type C = InnerConfig;
 pub(crate) type F = BabyBear;
 pub(crate) type OuterSC = BabyBearPoseidon2OuterConfig;
+
+pub struct Sdk<VC: VmConfig<F>> {
+    _marker: PhantomData<VC>,
+}
+
+impl<VC: VmConfig<F>> Sdk<VC>
+where
+    VC::Executor: Chip<SC>,
+    VC::Periphery: Chip<SC>,
+{
+    pub fn build(_pkg_dir: &str) -> Elf {
+        todo!()
+    }
+
+    pub fn transpile(_elf: Elf, _transpiler: Transpiler<F>) -> AxVmExe<F> {
+        todo!()
+    }
+
+    pub fn execute(_inputs: Vec<Vec<F>>, _exe: AxVmExe<F>) -> Result<(), ExecutionError> {
+        todo!()
+    }
+
+    pub fn app_keygen_and_commit_exe(
+        _config: AppConfig<VC>,
+        _exe: AxVmExe<F>,
+        _output_dir: Option<&str>,
+    ) -> (AppProvingKey<VC>, Arc<AxVmCommittedExe<SC>>) {
+        todo!()
+    }
+
+    pub fn load_app_pk_from_cached_dir(
+        _app_cache_dir: &str,
+    ) -> (AppProvingKey<VC>, Arc<AxVmCommittedExe<SC>>) {
+        todo!()
+    }
+
+    pub fn generate_app_proof(
+        _inputs: Vec<Vec<F>>,
+        _app_pk: AppProvingKey<VC>,
+        _app_exe: Arc<AxVmCommittedExe<SC>>,
+    ) -> Vec<Proof<SC>> {
+        todo!()
+    }
+
+    pub fn verify_app_proof(
+        _proof: Vec<Proof<SC>>,
+        _app_pk: &AppProvingKey<VC>,
+    ) -> Result<(), VerificationError> {
+        todo!()
+    }
+
+    #[cfg(feature = "static-verifier")]
+    pub fn agg_keygen_and_commit_leaf_exe(
+        _config: AggConfig,
+        _output_dir: Option<&str>,
+    ) -> (
+        AggProvingKey,
+        Arc<AxVmCommittedExe<SC>>,
+        Halo2VerifierCircuit,
+    ) {
+        todo!()
+    }
+
+    #[cfg(feature = "static-verifier")]
+    pub fn load_agg_pk_from_cached_dir(
+        _agg_cache_dir: &str,
+    ) -> (
+        AggProvingKey,
+        Arc<AxVmCommittedExe<SC>>,
+        Halo2VerifierCircuit,
+    ) {
+        todo!()
+    }
+
+    #[cfg(feature = "static-verifier")]
+    pub fn generate_e2e_proof(
+        _inputs: Vec<Vec<F>>,
+        _app_pk: AppProvingKey<VC>,
+        _app_exe: Arc<AxVmCommittedExe<SC>>,
+        _agg_pk: AggProvingKey,
+        _leaf_exe: Arc<AxVmCommittedExe<SC>>,
+        _static_verifier: Halo2VerifierCircuit,
+    ) -> Snark {
+        todo!()
+    }
+
+    #[cfg(feature = "static-verifier")]
+    pub fn generate_snark_verifier_contract(snark: Snark) {
+        todo!()
+    }
+
+    #[cfg(feature = "static-verifier")]
+    pub fn evm_verify_snark(snark: Snark) {
+        todo!()
+    }
+}
