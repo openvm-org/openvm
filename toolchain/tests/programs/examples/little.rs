e91d8e4ca4653461781558b31a9b5c98af3f63b2
Alexander Golovanov <Golovanov12345@gmail.com>
2024-12-02 23:23:28 +0100

    Small refactor of little.rs (insignificant)

--- toolchain/tests/programs/examples/little.rs
+++ toolchain/tests/programs/examples/little.rs
@@ -20,6 +20,8 @@ pub fn main() {
     let mut res = Secp256k1Coord::from_u32(1);
     let inv = res.clone().div_unsafe(&a);
 
+    // assert_ne!(res, Secp256k1Coord::from_u32(0));
+
     for i in 0..32 {
         for j in 0..8 {
             if pow[i] & (1 << j) != 0 {
@@ -30,16 +32,12 @@ pub fn main() {
     }
 
     // https://en.wikipedia.org/wiki/Fermat%27s_little_theorem
-    if res != inv {
-        axvm::process::panic();
-    }
+    assert_eq!(res, inv);
 
     let two = Secp256k1Coord::from_u32(2);
     let minus_two = Secp256k1Coord::from_le_bytes(&pow);
 
-    if (res - &minus_two) != (inv + &two) {
-        axvm::process::panic();
-    }
+    assert_eq!(res - &minus_two, inv + &two);
 
     if two == minus_two {
         axvm::process::panic();
