6156f9ff10fe4efeb40b149d948ef834ec3fee4f
Stephen Hwang <stephenh@intrinsictech.xyz>
2024-12-04 10:54:09 -0500

    chore: check something

--- crates/axvm-sdk/src/lib.rs
+++ crates/axvm-sdk/src/lib.rs
@@ -131,7 +131,7 @@ impl Sdk {
             let json = serde_json::to_string(&agg_pk.leaf_vm_pk.vm_config)?;
             println!("leaf_vm_pk.vm_config: {}", json);
             for (i, air) in agg_pk.leaf_vm_pk.vm_pk.per_air.iter().enumerate() {
-                if i < 13 {
+                if i <= 14 {
                     continue;
                 }
                 let json = serde_json::to_string(&air.preprocessed_data)?;
