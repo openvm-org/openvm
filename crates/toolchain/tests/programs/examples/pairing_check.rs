3568c91cdaec88ab1f6f8a803841ff5e06bff60b
Alexander Golovanov <Golovanov12345@gmail.com>
2024-12-04 16:37:40 +0100

    Fix (some) examples

--- crates/toolchain/tests/programs/examples/pairing_check.rs
+++ crates/toolchain/tests/programs/examples/pairing_check.rs
@@ -23,6 +23,10 @@ mod bn254 {
         "21888242871839275222246405745257275088696311157297823662689037894645226208583"
     );
 
+    axvm_algebra_complex_macros::complex_init! {
+        Fp2,
+    }
+
     pub fn test_pairing_check(io: &[u8]) {
         // axvm::io::print(format!("mod_idx = {}", <Fp as IntMod>::MOD_IDX));
         setup_all_moduli();
@@ -56,6 +60,10 @@ mod bls12_381 {
     use super::*;
 
     axvm_algebra_moduli_setup::moduli_init!("0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab");
+
+    axvm_algebra_complex_macros::complex_init! {
+        Fp2,
+    }
 
     pub fn test_pairing_check(io: &[u8]) {
         setup_all_moduli();
