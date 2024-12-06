23911909addf5e07f26d5fb62a3109565d42524a
Stephen Hwang <stephenh@intrinsictech.xyz>
2024-12-06 13:10:25 -0500

    feat: template structs for SDK CLI commands

--- crates/cargo-axiom/src/commands/mod.rs
+++ crates/cargo-axiom/src/commands/mod.rs
@@ -1,3 +1,23 @@
-pub mod build;
-// pub mod new;
-pub mod bench;
+mod bench;
+pub use bench::*;
+
+mod build;
+pub use build::*;
+
+mod contract;
+pub use contract::*;
+
+mod keygen;
+pub use keygen::*;
+
+// mod new;
+// pub use new::*;
+
+mod prove;
+pub use prove::*;
+
+mod run;
+pub use run::*;
+
+mod verify;
+pub use verify::*;
