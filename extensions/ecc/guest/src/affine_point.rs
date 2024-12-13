fae411f2acdec856bd613916f005292395dbb68b
Yu Jiang Tham <yoojbruin@gmail.com>
2024-12-13 15:29:34 -0800

    impl Group for AffinePoint

--- extensions/ecc/guest/src/affine_point.rs
+++ extensions/ecc/guest/src/affine_point.rs
@@ -1,6 +1,8 @@
-use core::ops::Neg;
+use core::ops::{Add, AddAssign, Neg, Sub, SubAssign};
 
 use openvm_algebra_guest::Field;
+
+use crate::Group;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq)]
 #[repr(C)]
@@ -40,5 +42,116 @@ where
             x: self.x,
             y: self.y.neg(),
         }
+    }
+}
+
+impl<F: Field> Add for AffinePoint<F> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        let x = self.x + rhs.x;
+        let y = self.y + rhs.y;
+        Self { x, y }
+    }
+}
+
+impl<'a, F: Field> Add<&'a Self> for AffinePoint<F> {
+    type Output = Self;
+
+    fn add(self, rhs: &'a Self) -> Self {
+        let x = self.x + rhs.x.clone();
+        let y = self.y + rhs.y.clone();
+        Self { x, y }
+    }
+}
+
+impl<F: Field> Add<&AffinePoint<F>> for &AffinePoint<F> {
+    type Output = AffinePoint<F>;
+
+    fn add(self, rhs: &AffinePoint<F>) -> Self::Output {
+        self.clone() + rhs.clone()
+    }
+}
+
+impl<F: Field> AddAssign for AffinePoint<F> {
+    fn add_assign(&mut self, rhs: Self) {
+        self.add_assign(&rhs);
+    }
+}
+
+impl<F: Field> AddAssign<&AffinePoint<F>> for AffinePoint<F> {
+    fn add_assign(&mut self, rhs: &AffinePoint<F>) {
+        *self = self.clone() + rhs;
+    }
+}
+
+impl<F: Field> Sub for AffinePoint<F> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        let x = self.x - rhs.x;
+        let y = self.y - rhs.y;
+        Self { x, y }
+    }
+}
+
+impl<'a, F: Field> Sub<&'a Self> for AffinePoint<F> {
+    type Output = Self;
+
+    fn sub(self, rhs: &'a Self) -> Self {
+        let x = self.x - rhs.x.clone();
+        let y = self.y - rhs.y.clone();
+        Self { x, y }
+    }
+}
+
+impl<F: Field> Sub<&AffinePoint<F>> for &AffinePoint<F> {
+    type Output = AffinePoint<F>;
+
+    fn sub(self, rhs: &AffinePoint<F>) -> Self::Output {
+        self.clone() - rhs.clone()
+    }
+}
+
+impl<F: Field> SubAssign for AffinePoint<F> {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.sub_assign(&rhs);
+    }
+}
+
+impl<F: Field> SubAssign<&AffinePoint<F>> for AffinePoint<F> {
+    fn sub_assign(&mut self, rhs: &AffinePoint<F>) {
+        *self = self.clone() - rhs;
+    }
+}
+
+impl<F: Field> Group for AffinePoint<F> {
+    type SelfRef<'a>
+        = &'a Self
+    where
+        Self: 'a;
+
+    const IDENTITY: Self = Self {
+        x: F::ZERO,
+        y: F::ZERO,
+    };
+
+    fn is_identity(&self) -> bool {
+        self.x == F::ZERO && self.y == F::ZERO
+    }
+
+    fn double(&self) -> Self {
+        if self.is_identity() {
+            self.clone()
+        } else {
+            self.clone() + self
+        }
+    }
+
+    fn double_assign(&mut self) {
+        if self.is_identity() {
+            return;
+        }
+        *self = self.double();
     }
 }
