7c13f266e481622f68d189e9ecb7a3f5d413f066
Stephen Hwang <stephenh@intrinsictech.xyz>
2024-12-13 18:44:50 +0000

    feat: CLI setup downloads kzg files

--- crates/cli/src/default.rs
+++ crates/cli/src/default.rs
@@ -1,7 +1,8 @@
 pub const DEFAULT_MANIFEST_DIR: &str = ".";
 
-pub const DEFAULT_AGG_PK_PATH: &str = "~/.openvm/agg.pk";
-pub const DEFAULT_VERIFIER_PATH: &str = "~/.openvm/verifier.sol";
+pub const DEFAULT_AGG_PK_PATH: &str = concat!(env!("HOME"), "/.openvm/agg.pk");
+pub const DEFAULT_VERIFIER_PATH: &str = concat!(env!("HOME"), "/.openvm/verifier.sol");
+pub const DEFAULT_PARAMS_DIR: &str = concat!(env!("HOME"), "/.openvm/params/");
 
 pub const DEFAULT_APP_PK_PATH: &str = "./openvm/app.pk";
 pub const DEFAULT_APP_VK_PATH: &str = "./openvm/app.vk";
