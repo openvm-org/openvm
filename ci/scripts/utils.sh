00ba22fade895b9fdbb1ef44c0d2005b0a951ebb
Arayi <arayik@intrinsictech.xyz>
2024-12-18 17:29:16 -0500

    fix: revert breaks

--- ci/scripts/utils.sh
+++ ci/scripts/utils.sh
@@ -86,7 +86,21 @@ commit_and_push_benchmark_results() {
 }
 
 install_s5cmd() {
-    rustup component add rust-src --toolchain nightly-2024-10-30
+    arch=$(uname -m)
+    case $arch in
+    arm64|aarch64)
+        rustup component add rust-src --toolchain nightly-2024-10-30-aarch64-unknown-linux-gnu
+        S5CMD_BIN="s5cmd_2.2.2_linux_arm64.deb"
+        ;;
+    x86_64|amd64)
+        rustup component add rust-src --toolchain nightly-2024-10-30-x86_64-unknown-linux-gnu
+        S5CMD_BIN="s5cmd_2.2.2_linux_amd64.deb"
+        ;;
+    *)
+        echo "Unsupported architecture: $arch"
+        exit 1
+        ;;
+    esac
 
     echo "Checking s5cmd"
     if type s5cmd &>/dev/null; then
