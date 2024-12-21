af9104f42efa39551d2676b94ee56041a130ec51
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-12-21 16:19:26 -0500

    chore: add comments clarifying its a wrapper

--- crates/circuits/poseidon2-air/src/lib.rs
+++ crates/circuits/poseidon2-air/src/lib.rs
@@ -1,7 +1,12 @@
-//! Standalone AIR implementation of the Poseidon2 permutation
+//! This is a wrapper around the Plonky3 [p3_poseidon2_air] used only for integration convenience to
+//! get around some complications with field-specific generics associated with Poseidon2.
+//! Currently it is only intended for use in OpenVM with BabyBear.
+//!
+//! We do not recommend external use of this crate, and suggest using the [p3_poseidon2_air] crate directly.
 
 pub use openvm_stark_sdk::p3_baby_bear;
 pub use p3_poseidon2;
+pub use p3_poseidon2_air;
 pub use p3_symmetric;
 
 mod poseidon2;
