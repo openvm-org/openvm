efddca4446635ba282d17004e121643d692c3306
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-01-05 01:17:29 -0500

    chore: rename app_log_blowup to leaf_log_blowup

--- ci/scripts/bench.py
+++ ci/scripts/bench.py
@@ -7,7 +7,7 @@ def run_cargo_command(
     bin_name,
     feature_flags,
     app_log_blowup,
-    agg_log_blowup,
+    leaf_log_blowup,
     root_log_blowup,
     internal_log_blowup,
     max_segment_length,
@@ -21,8 +21,8 @@ def run_cargo_command(
 
     if app_log_blowup is not None:
         command.extend(["--app_log_blowup", app_log_blowup])
-    if agg_log_blowup is not None:
-        command.extend(["--agg_log_blowup", agg_log_blowup])
+    if leaf_log_blowup is not None:
+        command.extend(["--leaf_log_blowup", leaf_log_blowup])
     if root_log_blowup is not None:
         command.extend(["--root_log_blowup", root_log_blowup])
     if internal_log_blowup is not None:
@@ -56,9 +56,9 @@ def bench():
     parser = argparse.ArgumentParser()
     parser.add_argument('bench_name', type=str, help="Name of the benchmark to run")
     parser.add_argument('--app_log_blowup', type=str, help="Application level log blowup")
-    parser.add_argument('--agg_log_blowup', type=str, help="Aggregation level log blowup")
-    parser.add_argument('--root_log_blowup', type=str, help="Application level log blowup")
-    parser.add_argument('--internal_log_blowup', type=str, help="Aggregation level log blowup")
+    parser.add_argument('--leaf_log_blowup', type=str, help="Leaf level log blowup")
+    parser.add_argument('--root_log_blowup', type=str, help="Root level log blowup")
+    parser.add_argument('--internal_log_blowup', type=str, help="Internal level log blowup")
     parser.add_argument('--max_segment_length', type=str, help="Max segment length for continuations")
     parser.add_argument('--features', type=str, help="Additional features")
     parser.add_argument('--output_path', type=str, required=True, help="The path to write the metrics to")
@@ -71,7 +71,7 @@ def bench():
         args.bench_name,
         feature_flags,
         args.app_log_blowup,
-        args.agg_log_blowup,
+        args.leaf_log_blowup,
         args.root_log_blowup,
         args.internal_log_blowup,
         args.max_segment_length,
