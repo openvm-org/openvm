02ebf104b69ada86811c9c930e78496b134f6b5f
TlatoaniHJ <TlatoaniHJ@users.noreply.github.com>
2025-01-06 13:30:27 -0500

    Update transpiler extension comment

--- crates/toolchain/transpiler/src/extension.rs
+++ crates/toolchain/transpiler/src/extension.rs
@@ -8,8 +8,8 @@ pub trait TranspilerExtension<F> {
     /// The `instruction_stream` provides a view of the remaining RISC-V instructions to be processed,
     /// presented as 32-bit chunks. The [`CustomInstructionProcessor`] should determine if it knows how to transpile
     /// the next contiguous section of RISC-V instructions into an [`Instruction`].
-    /// It returns `None` if it cannot transpile. Otherwise it returns `(instruction, how_many_u32s)` to indicate that
-    /// `instruction_stream[..how_many_u32s]` should be transpiled into `instruction`.
+    /// It returns `None` if it cannot transpile. Otherwise it returns `TranspilerOutput { instructions, used_u32s }` to indicate that
+    /// `instruction_stream[..used_u32s]` should be transpiled into `instructions`.
     fn process_custom(&self, instruction_stream: &[u32]) -> Option<TranspilerOutput<F>>;
 }
 
