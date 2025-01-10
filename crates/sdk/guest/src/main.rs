6aa8dfb7ac51f3a1f61e45727f9bf1ee8c82d274
alv-around <alfmail@pm.me>
2025-01-10 10:55:31 +0100

    update guest code

--- crates/sdk/guest/src/main.rs
+++ crates/sdk/guest/src/main.rs
@@ -1,20 +1,18 @@
 #![cfg_attr(target_os = "zkvm", no_main)]
 #![cfg_attr(target_os = "zkvm", no_std)]
 
-// src/main.rs
-use openvm::io::{read, reveal};
-
 openvm::entry!(main);
 
-fn main() {
-    let n: u64 = read();
-    let mut a: u64 = 0;
-    let mut b: u64 = 1;
-    for _ in 0..n {
-        let c: u64 = a.wrapping_add(b);
+pub fn main() {
+    let n = core::hint::black_box(1 << 10);
+    let mut a: u32 = 0;
+    let mut b: u32 = 1;
+    for _ in 1..n {
+        let sum = a + b;
         a = b;
-        b = c;
+        b = sum;
     }
-    reveal(a as u32, 0);
-    reveal((a >> 32) as u32, 1);
+    if a == 0 {
+        panic!();
+    }
 }
