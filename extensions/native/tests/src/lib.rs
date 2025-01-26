bc1b5a70e8459fe3656a95dfba82086bb3fef036
Alexander Golovanov <Golovanov12345@gmail.com>
2025-01-26 19:20:43 +0100

    Remove native test

--- extensions/native/tests/src/lib.rs
+++ extensions/native/tests/src/lib.rs
@@ -37,29 +37,4 @@ mod tests {
         air_test(Rv32WithKernelsConfig::default(), exe);
         Ok(())
     }
-
-    #[test]
-    fn test_hashmap() -> Result<()> {
-        let sdk = Sdk;
-
-        let elf = sdk.build(
-            GuestOptions::default().with_features(["std"]),
-            get_programs_dir!(),
-            &Some(TargetFilter {
-                kind: "example".to_string(),
-                name: "hashmap".to_string(),
-            }),
-        )?;
-        let exe = VmExe::from_elf(
-            elf.clone(),
-            Transpiler::<BabyBear>::default()
-                .with_extension(Rv32ITranspilerExtension)
-                .with_extension(Rv32MTranspilerExtension)
-                .with_extension(Rv32IoTranspilerExtension)
-                .with_extension(LongFormTranspilerExtension),
-        )?;
-
-        air_test(Rv32WithKernelsConfig::default(), exe);
-        Ok(())
-    }
 }
