2f9a52d087e7b12da57c823dc4216bd946bce973
Avaneesh Kulkarni <avaneeshk@intrinsictech.xyz>
2025-01-29 17:36:58 -0500

    fix linter errors

--- extensions/pairing/tests/programs/examples/bls_ec.rs
+++ extensions/pairing/tests/programs/examples/bls_ec.rs
@@ -1,9 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_main)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use hex_literal::hex;
-use openvm_algebra_guest::IntMod;
-use openvm_ecc_guest::{msm, weierstrass::WeierstrassPoint, Group};
 use openvm_pairing_guest::bls12_381::Bls12_381G1Affine;
 
 openvm_algebra_moduli_macros::moduli_init! {
@@ -19,5 +16,5 @@ openvm::entry!(main);
 
 pub fn main() {
     setup_all_moduli();
-    setup_all_curves();
+    setup_all_sw_curves();
 }
