398689ed43e932abcae2837240ea8502605a46c7
Avaneesh Kulkarni <avaneeshk@intrinsictech.xyz>
2025-01-30 16:04:16 -0500

    Add CachedMulTable for preprocessed msm for twisted edwards curves

--- extensions/ecc/guest/src/edwards.rs
+++ extensions/ecc/guest/src/edwards.rs
@@ -1,6 +1,9 @@
-use core::ops::Mul;
+use alloc::vec::Vec;
+use core::ops::{AddAssign, Mul};
 
-use openvm_algebra_guest::Field;
+use openvm_algebra_guest::{Field, IntMod};
+
+use crate::{Group, IntrinsicCurve};
 
 pub trait TwistedEdwardsPoint: Sized {
     /// The `a` coefficient in the twisted Edwards curve equation `ax^2 + y^2 = 1 + d x^2 y^2`.
@@ -266,4 +269,117 @@ macro_rules! impl_te_group_ops {
             }
         }
     };
+}
+
+// This is the same as the Weierstrass version, but for Edwards curves we use
+// TwistedEdwardsPoint::add_impl instead of WeierstrassPoint::add_ne_nonidentity, etc.
+// Unlike the Weierstrass version, we do not require the bases to have prime order, since our addition
+// formulas are complete.
+
+// MSM using preprocessed table (windowed method)
+// Reference: modified from https://github.com/arkworks-rs/algebra/blob/master/ec/src/scalar_mul/mod.rs
+
+/// Cached precomputations of scalar multiples of several base points.
+/// - `window_bits` is the window size used for the precomputation
+/// - `max_scalar_bits` is the maximum size of the scalars that will be multiplied
+/// - `table` is the precomputed table
+pub struct CachedMulTable<'a, C: IntrinsicCurve> {
+    /// Window bits. Must be > 0.
+    /// For alignment, we currently require this to divide 8 (bits in a byte).
+    pub window_bits: usize,
+    pub bases: &'a [C::Point],
+    /// `table[i][j] = (j + 2) * bases[i]` for `j + 2 < 2 ** window_bits`
+    table: Vec<Vec<C::Point>>,
+    /// Needed to return reference to the identity point.
+    identity: C::Point,
+}
+
+impl<'a, C: IntrinsicCurve> CachedMulTable<'a, C>
+where
+    C::Point: TwistedEdwardsPoint + Group,
+    C::Scalar: IntMod,
+{
+    pub fn new(bases: &'a [C::Point], window_bits: usize) -> Self {
+        assert!(window_bits > 0);
+        let window_size = 1 << window_bits;
+        let table = bases
+            .iter()
+            .map(|base| {
+                if base.is_identity() {
+                    vec![<C::Point as Group>::IDENTITY; window_size - 2]
+                } else {
+                    let mut multiples = Vec::with_capacity(window_size - 2);
+                    for _ in 0..window_size - 2 {
+                        let multiple = multiples
+                            .last()
+                            .map(|last| TwistedEdwardsPoint::add_impl(last, base))
+                            .unwrap_or_else(|| base.double());
+                        multiples.push(multiple);
+                    }
+                    multiples
+                }
+            })
+            .collect();
+
+        Self {
+            window_bits,
+            bases,
+            table,
+            identity: <C::Point as Group>::IDENTITY,
+        }
+    }
+
+    fn get_multiple(&self, base_idx: usize, scalar: usize) -> &C::Point {
+        if scalar == 0 {
+            &self.identity
+        } else if scalar == 1 {
+            unsafe { self.bases.get_unchecked(base_idx) }
+        } else {
+            unsafe { self.table.get_unchecked(base_idx).get_unchecked(scalar - 2) }
+        }
+    }
+
+    /// Computes `sum scalars[i] * bases[i]`.
+    ///
+    /// For implementation simplicity, currently only implemented when
+    /// `window_bits` divides 8 (number of bits in a byte).
+    pub fn windowed_mul(&self, scalars: &[C::Scalar]) -> C::Point {
+        assert_eq!(8 % self.window_bits, 0);
+        assert_eq!(scalars.len(), self.bases.len());
+        let windows_per_byte = 8 / self.window_bits;
+
+        let num_windows = C::Scalar::NUM_LIMBS * windows_per_byte;
+        let mask = (1u8 << self.window_bits) - 1;
+
+        // The current byte index (little endian) at the current step of the
+        // windowed method, across all scalars.
+        let mut limb_idx = C::Scalar::NUM_LIMBS;
+        // The current bit (little endian) within the current byte of the windowed
+        // method. The window will look at bits `bit_idx..bit_idx + window_bits`.
+        // bit_idx will always be in range [0, 8)
+        let mut bit_idx = 0;
+
+        let mut res = <C::Point as Group>::IDENTITY;
+        for outer in 0..num_windows {
+            if bit_idx == 0 {
+                limb_idx -= 1;
+                bit_idx = 8 - self.window_bits;
+            } else {
+                bit_idx -= self.window_bits;
+            }
+
+            if outer != 0 {
+                for _ in 0..self.window_bits {
+                    res.double_assign();
+                }
+            }
+            for (base_idx, scalar) in scalars.iter().enumerate() {
+                let scalar = (scalar.as_le_bytes()[limb_idx] >> bit_idx) & mask;
+                let summand = self.get_multiple(base_idx, scalar as usize);
+                // handles identity
+                res.add_assign(summand);
+            }
+        }
+        res
+    }
 }
