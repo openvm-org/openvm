27eb1494e7cda849a55cfbbdb9e41350f0e266cc
Alexander Golovanov <Golovanov12345@gmail.com>
2025-02-04 14:00:49 +0100

    Revert "Calculate the capacity first, then reserve exactly this much"
    This reverts commit 3ee49b31b098167768c246c709ac99bb77bdc4d1.

--- crates/vm/src/system/memory/tree/mod.rs
+++ crates/vm/src/system/memory/tree/mod.rs
@@ -120,22 +120,7 @@ impl<const CHUNK: usize, F: PrimeField32> MemoryNode<CHUNK, F> {
     ) -> MemoryNode<CHUNK, F> {
         // Construct a Vec that includes the address space in the label calculation,
         // representing the entire memory tree.
-        let capacity = {
-            let mut capacity = 0;
-            let mut last = (0, 0);
-            for ((address_space, pointer), _) in memory.items() {
-                if pointer as usize / CHUNK >= (1 << memory_dimensions.address_height) {
-                    continue;
-                }
-                let label = (address_space, pointer / CHUNK as u32);
-                if label != last {
-                    capacity += 1;
-                    last = label;
-                }
-            }
-            capacity
-        };
-        let mut memory_partition: Vec<(u64, [F; CHUNK])> = Vec::with_capacity(capacity);
+        let mut memory_partition: Vec<(u64, [F; CHUNK])> = Vec::new();
         for ((address_space, pointer), value) in memory.items() {
             if pointer as usize / CHUNK >= (1 << memory_dimensions.address_height) {
                 continue;
