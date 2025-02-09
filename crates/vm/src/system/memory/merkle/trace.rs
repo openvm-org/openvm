113683d993af5dca89db2be1b8b333a23add9acd
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-02-08 23:55:25 -0800

    Revert "par_sort"
    This reverts commit 018bb167ecee4e84c54e321872d7134f5c16fb89.

--- crates/vm/src/system/memory/merkle/trace.rs
+++ crates/vm/src/system/memory/merkle/trace.rs
@@ -121,7 +121,7 @@ where
         } = self.final_state.unwrap();
         // important that this sort be stable,
         // because we need the initial root to be first and the final root to be second
-        rows.par_sort_by_key(|row| Reverse(row.parent_height));
+        rows.sort_by_key(|row| Reverse(row.parent_height));
 
         let width = MemoryMerkleCols::<Val<SC>, CHUNK>::width();
         let mut height = rows.len().next_power_of_two();
