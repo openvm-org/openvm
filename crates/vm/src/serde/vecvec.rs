c39be9ddb4fe2b36d759d58f1678ea0191cc118f
Lun-Kai <lunkai@intrinsictech.xyz>
2025-02-18 16:02:49 -0800

    not work: pointer being freed was not allocated

--- crates/vm/src/serde/vecvec.rs
+++ crates/vm/src/serde/vecvec.rs
@@ -1,10 +1,10 @@
 use super::SerdeError;
 
-// TODO: allocate once and split
 // First 8 bytes store the length of the vec
+// Next 8 bytes, store the sum of the lengths of all inner vecs
 // For each inner vec, we store the length as u64, then the vec
 pub fn size(v: &Vec<Vec<u8>>) -> usize {
-    let mut total_len = 8;
+    let mut total_len = 16;
     for inner_vec in v {
         total_len += inner_vec.len() + 8;
     }
@@ -14,6 +14,9 @@ pub fn size(v: &Vec<Vec<u8>>) -> usize {
 pub fn dump(v: &Vec<Vec<u8>>, data: &mut [u8]) -> usize {
     let mut current_pos = 0;
     data[current_pos..current_pos + 8].copy_from_slice(&(v.len() as u64).to_le_bytes());
+    current_pos += 8;
+    let sum_len = v.iter().map(|v| v.len()).sum::<usize>();
+    data[current_pos..current_pos + 8].copy_from_slice(&(sum_len as u64).to_le_bytes());
     current_pos += 8;
     for inner_vec in v {
         data[current_pos..current_pos + 8].copy_from_slice(&(inner_vec.len() as u64).to_le_bytes());
@@ -25,27 +28,52 @@ pub fn dump(v: &Vec<Vec<u8>>, data: &mut [u8]) -> usize {
 }
 
 pub fn load(data: &[u8]) -> Result<Vec<Vec<u8>>, SerdeError> {
-    let num_vecs = u64::from_le_bytes(data[0..8].try_into().unwrap());
-    let mut result = Vec::with_capacity(num_vecs as usize);
-    let mut offset = 8;
-
-    for _ in 0..num_vecs {
-        if offset + 8 > data.len() {
-            return Err(SerdeError::InvalidData(
-                "Insufficient data for Read variant".to_string(),
-            ));
-        }
-        let len = u64::from_le_bytes(data[offset..offset + 8].try_into().unwrap()) as usize;
-        offset += 8;
-        if offset + len > data.len() {
-            return Err(SerdeError::InvalidData(
-                "Insufficient data for Read variant".to_string(),
-            ));
-        }
-        let inner_vec = data[offset..offset + len].to_vec();
-        offset += len;
-        result.push(inner_vec);
+    if data.len() < 16 {
+        return Err(SerdeError::InvalidData(
+            "Insufficient data for VecVec".to_string(),
+        ));
     }
+    let num_vecs = u64::from_le_bytes(data[0..8].try_into().unwrap()) as usize;
+    let sum_len = u64::from_le_bytes(data[8..16].try_into().unwrap()) as usize;
+    let mut result = Vec::with_capacity(num_vecs);
+    let mut internal_vecs: Vec<u8> = vec![0; sum_len];
+
+    if data.len() < 16 + sum_len + 8 * num_vecs {
+        return Err(SerdeError::InvalidData(
+            "Insufficient data for VecVec".to_string(),
+        ));
+    }
+
+    let mut offset = 16;
+    let mut internal_vec_count = 0;
+
+    println!("start unsafe");
+    unsafe {
+        for i in 0..num_vecs {
+            let len = u64::from_le_bytes(data[offset..offset + 8].try_into().unwrap()) as usize;
+            offset += 8;
+            println!(
+                "in loop i = {}, internal_vec_count = {}, len = {}",
+                i, internal_vec_count, len
+            );
+
+            // let inner_vec = data[offset..offset + len].to_vec();
+            println!(
+                "vec at ptr: {:p}",
+                internal_vecs.as_mut_ptr().add(internal_vec_count)
+            );
+            let mut inner_vec =
+                Vec::from_raw_parts(internal_vecs.as_mut_ptr().add(internal_vec_count), len, len);
+            inner_vec.copy_from_slice(&data[offset..offset + len]);
+
+            offset += len;
+            internal_vec_count += len;
+            result.push(inner_vec);
+        }
+        assert_eq!(internal_vec_count, sum_len);
+        std::mem::forget(internal_vecs);
+    }
+    println!("end unsafe");
 
     Ok(result)
 }
