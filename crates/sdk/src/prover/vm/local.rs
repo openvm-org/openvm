3e76fc5d483c6bcfd33ef5315483e41d56522571
Yi Sun <yi-sun@users.noreply.github.com>
2025-03-22 15:27:15 -0400

    test: try explicit ram drop

--- crates/sdk/src/prover/vm/local.rs
+++ crates/sdk/src/prover/vm/local.rs
@@ -94,11 +94,13 @@ where
                     let proof_input = info_span!("trace_gen", segment = seg_idx)
                         .in_scope(|| seg.generate_proof_input(Some(committed_program.clone())));
 
-                    info_span!("prove_segment", segment = seg_idx)
-                        .in_scope(|| vm.engine.prove(&self.pk.vm_pk, proof_input))
+                    let ret = info_span!("prove_segment", segment = seg_idx)
+                        .in_scope(|| vm.engine.prove(&self.pk.vm_pk, proof_input));
+
+                    drop(proof_input);
+                    ret
                 };
 
-                drop(proof_input);
                 result
             })
             .unwrap();
