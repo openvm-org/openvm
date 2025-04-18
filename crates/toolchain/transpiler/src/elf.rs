8292a89c6424490383e7491ad7e3feee83d053c0
Manh Dinh <manh9203@gmail.com>
2025-04-18 17:28:14 -0400

    Fix lint

--- crates/toolchain/transpiler/src/elf.rs
+++ crates/toolchain/transpiler/src/elf.rs
@@ -1,9 +1,9 @@
 // Initial version taken from https://github.com/succinctlabs/sp1/blob/v2.0.0/crates/core/executor/src/disassembler/elf.rs under MIT License
 // and https://github.com/risc0/risc0/blob/f61379bf69b24d56e49d6af96a3b284961dcc498/risc0/binfmt/src/elf.rs#L34 under Apache License
+use std::{cmp::min, collections::BTreeMap, fmt::Debug};
+#[cfg(feature = "function-span")]
 use std::{
-    cmp::min,
-    collections::{hash_map::Entry, BTreeMap, HashMap},
-    fmt::Debug,
+    collections::{hash_map::Entry, HashMap},
     io::Write,
 };
 
