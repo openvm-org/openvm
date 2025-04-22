d6a1ef1092ba6703114768d6471612223438a0aa
Avaneesh Kulkarni <avaneeshk@intrinsictech.xyz>
2025-04-22 15:53:56 -0400

    Add hint-based square root implementation

---
+++ extensions/algebra/tests/programs/examples/sqrt.rs
@@ -0,0 +1,38 @@
+#![cfg_attr(not(feature = "std"), no_main)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use openvm_algebra_guest::{Field, IntMod, Sqrt};
+
+extern crate alloc;
+
+openvm::entry!(main);
+
+openvm_algebra_moduli_macros::moduli_declare! {
+    Secp256k1Coord {
+        modulus = "0xFFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE FFFFFC2F",
+        prime = true
+    }
+}
+
+openvm_algebra_moduli_macros::moduli_init!(
+    "0xFFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE FFFFFC2F"
+);
+
+pub fn main() {
+    setup_all_moduli();
+
+    let a = Secp256k1Coord::from_u32(4);
+    let sqrt = a.sqrt();
+    assert_eq!(sqrt, Some(Secp256k1Coord::from_u32(2)));
+
+    let b = <Secp256k1Coord as IntMod>::ZERO - <Secp256k1Coord as IntMod>::ONE;
+    let sqrt = b.sqrt();
+    // -1 is not a quadratic residue modulo p when p = 3 mod 4
+    // See https://math.stackexchange.com/questions/735400/if-p-equiv-3-mod-4-with-p-prime-prove-1-is-a-non-quadratic-residue-modulo
+    assert_eq!(sqrt, None);
+
+    let expected = b * Secp256k1Coord::from_u32(2).invert();
+    let c = expected.square();
+    let result = c.sqrt();
+    assert!(result == Some(expected.clone()) || result == Some(-expected));
+}
