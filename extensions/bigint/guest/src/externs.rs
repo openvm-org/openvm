7e239c9315bcd6457ff71fd3a0c2afde00263ac6
Avaneesh Kulkarni <avaneeshk@intrinsictech.xyz>
2025-04-30 16:56:16 -0400

    Remove U256 and I256 from bigint guest library

--- extensions/bigint/guest/src/externs.rs
+++ extensions/bigint/guest/src/externs.rs
@@ -5,7 +5,7 @@ use openvm_platform::custom_insn_r;
 use super::{Int256Funct7, BEQ256_FUNCT3, INT256_FUNCT3, OPCODE};
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_wrapping_add_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_wrapping_add_impl(result: *mut u8, a: *const u8, b: *const u8) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -17,7 +17,7 @@ unsafe extern "C" fn zkvm_u256_wrapping_add_impl(result: *mut u8, a: *const u8, 
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_wrapping_sub_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_wrapping_sub_impl(result: *mut u8, a: *const u8, b: *const u8) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -29,7 +29,7 @@ unsafe extern "C" fn zkvm_u256_wrapping_sub_impl(result: *mut u8, a: *const u8, 
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_wrapping_mul_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_wrapping_mul_impl(result: *mut u8, a: *const u8, b: *const u8) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -41,7 +41,7 @@ unsafe extern "C" fn zkvm_u256_wrapping_mul_impl(result: *mut u8, a: *const u8, 
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_bitxor_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_bitxor_impl(result: *mut u8, a: *const u8, b: *const u8) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -53,7 +53,7 @@ unsafe extern "C" fn zkvm_u256_bitxor_impl(result: *mut u8, a: *const u8, b: *co
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_bitand_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_bitand_impl(result: *mut u8, a: *const u8, b: *const u8) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -65,7 +65,7 @@ unsafe extern "C" fn zkvm_u256_bitand_impl(result: *mut u8, a: *const u8, b: *co
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_bitor_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_bitor_impl(result: *mut u8, a: *const u8, b: *const u8) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -77,7 +77,7 @@ unsafe extern "C" fn zkvm_u256_bitor_impl(result: *mut u8, a: *const u8, b: *con
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_wrapping_shl_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_wrapping_shl_impl(result: *mut u8, a: *const u8, b: *const u8) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -89,7 +89,7 @@ unsafe extern "C" fn zkvm_u256_wrapping_shl_impl(result: *mut u8, a: *const u8, 
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_wrapping_shr_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_wrapping_shr_impl(result: *mut u8, a: *const u8, b: *const u8) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -101,7 +101,11 @@ unsafe extern "C" fn zkvm_u256_wrapping_shr_impl(result: *mut u8, a: *const u8, 
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_arithmetic_shr_impl(result: *mut u8, a: *const u8, b: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_arithmetic_shr_impl(
+    result: *mut u8,
+    a: *const u8,
+    b: *const u8,
+) {
     custom_insn_r!(
         opcode = OPCODE,
         funct3 = INT256_FUNCT3,
@@ -113,7 +117,7 @@ unsafe extern "C" fn zkvm_u256_arithmetic_shr_impl(result: *mut u8, a: *const u8
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_eq_impl(a: *const u8, b: *const u8) -> bool {
+pub unsafe extern "C" fn zkvm_u256_eq_impl(a: *const u8, b: *const u8) -> bool {
     let mut is_equal: u32;
     asm!("li {res}, 1",
         ".insn b {opcode}, {func3}, {rs1}, {rs2}, 8",
@@ -128,7 +132,7 @@ unsafe extern "C" fn zkvm_u256_eq_impl(a: *const u8, b: *const u8) -> bool {
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_cmp_impl(a: *const u8, b: *const u8) -> Ordering {
+pub unsafe extern "C" fn zkvm_u256_cmp_impl(a: *const u8, b: *const u8) -> Ordering {
     let mut cmp_result = MaybeUninit::<crate::U256>::uninit();
     custom_insn_r!(
         opcode = OPCODE,
@@ -157,7 +161,7 @@ unsafe extern "C" fn zkvm_u256_cmp_impl(a: *const u8, b: *const u8) -> Ordering 
 }
 
 #[no_mangle]
-unsafe extern "C" fn zkvm_u256_clone_impl(result: *mut u8, a: *const u8) {
+pub unsafe extern "C" fn zkvm_u256_clone_impl(result: *mut u8, a: *const u8) {
     let zero = &crate::U256::ZERO as *const _ as *const u8;
     custom_insn_r!(
         opcode = OPCODE,
