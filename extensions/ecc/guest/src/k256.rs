438a3573449489fe2d69239fe8bd4a91b75c431e
Avaneesh Kulkarni <avaneeshk@intrinsictech.xyz>
2025-04-30 22:22:52 -0400

    Fix bugs

--- extensions/ecc/guest/src/k256.rs
+++ extensions/ecc/guest/src/k256.rs
@@ -7,7 +7,7 @@ use hex_literal::hex;
 use lazy_static::lazy_static;
 #[cfg(not(target_os = "zkvm"))]
 use num_bigint::BigUint;
-use openvm_algebra_guest::{Field, IntMod};
+use openvm_algebra_guest::IntMod;
 use openvm_algebra_moduli_macros::moduli_declare;
 use openvm_ecc_sw_macros::sw_declare;
 
