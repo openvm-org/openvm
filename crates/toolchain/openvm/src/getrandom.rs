16a27ddd06fcc9f8df20e11cfbe6247f1ca8ad17
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-05-08 13:07:49 -0400

    chore: fix typo

--- crates/toolchain/openvm/src/getrandom.rs
+++ crates/toolchain/openvm/src/getrandom.rs
@@ -1,7 +1,7 @@
 //! [getrandom] custom backend implementations. The implementations are feature-gated. The default
 //! feature enables "getrandom-unsupported", which is a backend that always errors. This should be
-//! used when `getrandom` is never called by pulled in as a dependency unavoidably. If no feature is
-//! enabled, then no custom implementation is registered, and the user must supply their own as
+//! used when `getrandom` is never called but pulled in as a dependency unavoidably. If no feature
+//! is enabled, then no custom implementation is registered, and the user must supply their own as
 //! described in the [getrandom] documentation.
 
 #[cfg(feature = "getrandom-unsupported")]
