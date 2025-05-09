5a87301dc43035b0146e4e518a44c89a8e94a938
Avaneesh Kulkarni <avaneeshk@intrinsictech.xyz>
2025-05-09 13:19:43 -0400

    Update kitchen sink benchmark to use guest-libs

--- benchmarks/guest/kitchen-sink/src/main.rs
+++ benchmarks/guest/kitchen-sink/src/main.rs
@@ -1,19 +1,25 @@
 use std::hint::black_box;
 
 use openvm_algebra_guest::IntMod;
-use openvm_bigint_guest::I256;
-use openvm_keccak256_guest::keccak256;
-use openvm_sha256_guest::sha256;
+use openvm_keccak256::keccak256;
+use openvm_ruint::aliases::U256;
+use openvm_sha2::sha256;
 #[allow(unused_imports)]
 use {
     openvm_ecc_guest::{
-        k256::Secp256k1Point, p256::P256Point, weierstrass::WeierstrassPoint, AffinePoint,
+        k256::{Secp256k1Coord, Secp256k1Point, Secp256k1Scalar},
+        p256::{P256Coord, P256Point, P256Scalar},
+        weierstrass::WeierstrassPoint,
+        CyclicGroup,
     },
-    openvm_pairing_guest::{
-        bls12_381::{Bls12_381G1Affine, G2Affine as Bls12_381G2Affine},
-        bn254::{Bn254, Bn254G1Affine, Fp, Fp2, G2Affine as Bn254G2Affine},
-        pairing::PairingCheck,
+    openvm_pairing::{
+        bls12_381::{
+            Bls12_381Fp, Bls12_381Fp2, Bls12_381G1Affine, Bls12_381Scalar,
+            G2Affine as Bls12_381G2Affine,
+        },
+        bn254::{Bn254, Bn254Fp, Bn254Fp2, Bn254G1Affine, Bn254Scalar, G2Affine as Bn254G2Affine},
     },
+    openvm_pairing_guest::pairing::PairingCheck,
 };
 
 // Note: these will all currently be represented as bytes32 even though they could be smaller
@@ -47,9 +53,62 @@ openvm_algebra_guest::complex_macros::complex_init! {
     Bls12_381Fp2 { mod_idx = 7 },
 }
 
+fn materialize_modular_chip<T: IntMod>() {
+    // hopefully the compiler doesn't optimize out the operations
+    // add/sub chip
+    black_box(T::ZERO + T::ZERO);
+    // mul/div chip
+    black_box(T::ZERO * T::ZERO);
+    // is_equal chip
+    black_box(T::ZERO.assert_reduced());
+}
+
+// making this a macro since there's no complex extension trait
+macro_rules! materialize_complex_chip {
+    ($complex_type:ty, $modular_type:ty) => {
+        // hopefully the compiler doesn't optimize out the operations
+        let zero = <$complex_type>::new(
+            <$modular_type as IntMod>::ZERO,
+            <$modular_type as IntMod>::ZERO,
+        );
+        // add/sub chip
+        black_box(&zero + &zero);
+        // mul/div chip
+        black_box(&zero * &zero);
+    };
+}
+
+fn materialize_ecc_chip<T: WeierstrassPoint + CyclicGroup>() {
+    // add chip
+    // it is important that neither operand is identity, otherwise the chip will not be materialized
+    black_box(T::GENERATOR + T::GENERATOR);
+    // double chip
+    // it is important that the operand is not identity, otherwise the chip will not be materialized
+    black_box(T::GENERATOR.double());
+}
+
 pub fn main() {
-    // TODO: Since we don't explicitly call setup functions anymore, we should rewrite this test
-    // to use every declared modulus and curve to ensure that every chip is materialized.
+    // Since we don't explicitly call setup functions anymore, we must ensure every declared modulus
+    // and curve chip is materialized.
+    materialize_modular_chip::<Mod1e18>();
+    materialize_modular_chip::<Secp256k1Coord>();
+    materialize_modular_chip::<Secp256k1Scalar>();
+    materialize_modular_chip::<P256Coord>();
+    materialize_modular_chip::<P256Scalar>();
+    materialize_modular_chip::<Bn254Fp>();
+    materialize_modular_chip::<Bn254Scalar>();
+    materialize_modular_chip::<Bls12_381Fp>();
+    materialize_modular_chip::<Bls12_381Scalar>();
+    materialize_modular_chip::<Mersenne61>();
+    materialize_modular_chip::<Seven>();
+
+    materialize_complex_chip!(Bn254Fp2, Bn254Fp);
+    materialize_complex_chip!(Bls12_381Fp2, Bls12_381Fp);
+
+    materialize_ecc_chip::<Secp256k1Point>();
+    materialize_ecc_chip::<P256Point>();
+    materialize_ecc_chip::<Bn254G1Affine>();
+    materialize_ecc_chip::<Bls12_381G1Affine>();
 
     let [one, six] = [1, 6].map(Seven::from_u32);
     assert_eq!(one + six, Seven::ZERO);
@@ -79,9 +138,9 @@ pub fn main() {
         let digest2 = sha256(&hash);
         hash.extend_from_slice(&digest2);
 
-        // SAFETY: internally I256 is represented as [u8; 32]
-        let i1 = I256::from_le_bytes(digest1);
-        let i2 = I256::from_le_bytes(digest2);
+        // SAFETY: internally U256 is represented as [u8; 32]
+        let i1 = U256::from_le_bytes(digest1);
+        let i2 = U256::from_le_bytes(digest2);
 
         black_box(&i1 + &i2);
         black_box(&i1 - &i2);
@@ -91,7 +150,7 @@ pub fn main() {
         black_box(i1 <= i2);
         black_box(&i1 & &i2);
         black_box(&i1 ^ &i2);
-        black_box(&i1 << &i2);
-        black_box(&i1 >> &i2);
+        black_box(i1 << &i2);
+        black_box(i1 >> &i2);
     }
 }
