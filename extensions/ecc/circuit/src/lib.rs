1d51c6ae4ba8962bced8653eca4f554b736b450c
Arayi <arayik@intrinsictech.xyz>
2025-06-03 19:13:49 -0400

    make ecc tests compile

--- extensions/ecc/circuit/src/lib.rs
+++ extensions/ecc/circuit/src/lib.rs
@@ -1,8 +1,8 @@
 mod weierstrass_chip;
 pub use weierstrass_chip::*;
 
-mod weierstrass_extension;
-pub use weierstrass_extension::*;
+// mod weierstrass_extension;
+// pub use weierstrass_extension::*;
 
-mod config;
-pub use config::*;
+// mod config;
+// pub use config::*;
