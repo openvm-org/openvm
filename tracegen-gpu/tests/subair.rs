65f4c13d188480b67a9a554f7ed1bee7c397f5d9
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2025-06-10 08:56:35 -0400

    feat: encoder SubAir CUDA tracegen + tests (#69)

--- tracegen-gpu/tests/subair.rs
+++ tracegen-gpu/tests/subair.rs
@@ -1,13 +1,14 @@
 use std::sync::Arc;
 
 use cuda_kernels::dummy::{
+    encoder,
     fibair::fibair_tracegen,
     is_zero,
     less_than::{assert_less_than_tracegen, less_than_array_tracegen, less_than_tracegen},
 };
 use cuda_utils::copy::MemCopyD2H;
 use cuda_utils::{copy::MemCopyH2D, d_buffer::DeviceBuffer};
-use openvm_circuit_primitives::{is_zero::IsZeroSubAir, TraceSubRowGenerator};
+use openvm_circuit_primitives::{encoder::Encoder, is_zero::IsZeroSubAir, TraceSubRowGenerator};
 use openvm_stark_backend::p3_matrix::dense::RowMajorMatrix;
 use openvm_stark_sdk::{dummy_airs::fib_air::chip::FibonacciChip, utils::create_seeded_rng};
 use p3_baby_bear::BabyBear;
@@ -219,4 +220,75 @@ fn test_is_zero_against_cpu_full() {
             assert_eq!(results[n + i], cur_out);
         }
     }
+}
+
+#[test]
+fn test_encoder_with_invalid_row() {
+    // Max number of flags for k = 6
+    let num_flags = 461;
+    let max_degree = 5;
+    let reserve_invalid = true;
+
+    let encoder = Encoder::new(num_flags, max_degree, reserve_invalid);
+    let expected_k = encoder.width();
+
+    let values = (0..num_flags)
+        .map(|i| encoder.get_flag_pt(i))
+        .collect::<Vec<_>>();
+    let cpu_matrix = Arc::new(RowMajorMatrix::<F>::new(
+        values
+            .into_iter()
+            .flat_map(|v| v.into_iter().map(F::from_canonical_u32))
+            .collect(),
+        expected_k,
+    ));
+
+    let gpu_matrix = DeviceMatrix::<F>::with_capacity(num_flags, expected_k);
+    unsafe {
+        encoder::tracegen(
+            gpu_matrix.buffer(),
+            num_flags as u32,
+            max_degree,
+            reserve_invalid,
+            expected_k as u32,
+        )
+        .unwrap();
+    };
+
+    assert_eq_cpu_and_gpu_matrix(cpu_matrix, &gpu_matrix);
+}
+
+#[test]
+fn test_encoder_without_invalid_row() {
+    let num_flags = 18;
+    let max_degree = 2;
+    let reserve_invalid = false;
+
+    let encoder = Encoder::new(num_flags, max_degree, reserve_invalid);
+    let expected_k = encoder.width();
+
+    let values = (0..num_flags)
+        .map(|i| encoder.get_flag_pt(i))
+        .collect::<Vec<_>>();
+    let cpu_matrix = Arc::new(RowMajorMatrix::<F>::new(
+        values
+            .into_iter()
+            .flat_map(|v| v.into_iter().map(F::from_canonical_u32))
+            .collect(),
+        expected_k,
+    ));
+
+    let gpu_matrix = DeviceMatrix::<F>::with_capacity(num_flags, expected_k);
+    unsafe {
+        encoder::tracegen(
+            gpu_matrix.buffer(),
+            num_flags as u32,
+            max_degree,
+            reserve_invalid,
+            expected_k as u32,
+        )
+        .unwrap();
+    };
+
+    assert_eq_cpu_and_gpu_matrix(cpu_matrix, &gpu_matrix);
 }
