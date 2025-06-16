9eb373c6231b63db85266c312d7f9e783ff0109f
Avaneesh Kulkarni <avaneeshk@intrinsictech.xyz>
2025-06-16 17:45:07 -0400

    Fix lint

--- extensions/ecc/circuit/src/edwards_chip/mod.rs
+++ extensions/ecc/circuit/src/edwards_chip/mod.rs
@@ -21,8 +21,8 @@ use utils::jacobi;
 
 /// BLOCK_SIZE: how many cells do we read at a time, must be a power of 2.
 /// BLOCKS: how many blocks do we need to represent one input or output
-/// For example, for bls12_381, BLOCK_SIZE = 16, each element has 3 blocks and with two elements per input AffinePoint, BLOCKS = 6.
-/// For secp256k1, BLOCK_SIZE = 32, BLOCKS = 2.
+/// For example, for bls12_381, BLOCK_SIZE = 16, each element has 3 blocks and with two elements per
+/// input AffinePoint, BLOCKS = 6. For secp256k1, BLOCK_SIZE = 32, BLOCKS = 2.
 #[derive(Chip, ChipUsageGetter, InstructionExecutor)]
 pub struct TeAddChip<F: PrimeField32, const BLOCKS: usize, const BLOCK_SIZE: usize>(
     pub  VmChipWrapper<
