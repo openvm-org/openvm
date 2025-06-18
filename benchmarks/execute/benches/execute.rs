2d5df00859015799dd4279692dded7ab71dfb5a1
Ayush Shukla <ayush@axiom.xyz>
2025-06-18 02:18:10 +0200

    fix: store widths/interactions as static

--- benchmarks/execute/benches/execute.rs
+++ benchmarks/execute/benches/execute.rs
@@ -27,7 +27,7 @@ use openvm_stark_sdk::{
     config::baby_bear_poseidon2::{
         default_engine, BabyBearPoseidon2Config, BabyBearPoseidon2Engine,
     },
-    openvm_stark_backend::{self, keygen::types::MultiStarkVerifyingKey, p3_field::PrimeField32},
+    openvm_stark_backend::{self, p3_field::PrimeField32},
     p3_baby_bear::BabyBear,
 };
 use openvm_transpiler::{transpiler::Transpiler, FromElf};
@@ -48,7 +48,7 @@ static AVAILABLE_PROGRAMS: &[&str] = &[
     // "pairing",
 ];
 
-static SHARED_VK: OnceLock<MultiStarkVerifyingKey<BabyBearPoseidon2Config>> = OnceLock::new();
+static SHARED_WIDTHS_AND_INTERACTIONS: OnceLock<(Vec<usize>, Vec<usize>)> = OnceLock::new();
 
 // TODO(ayush): remove from here
 #[derive(Clone, Debug, VmConfig, Serialize, Deserialize)]
@@ -121,12 +121,12 @@ fn load_program_executable(program: &str) -> Result<VmExe<BabyBear>> {
     Ok(VmExe::from_elf(elf, transpiler)?)
 }
 
-fn shared_vk(
-    vm: &VirtualMachine<BabyBearPoseidon2Config, BabyBearPoseidon2Engine, ExecuteConfig>,
-) -> &'static MultiStarkVerifyingKey<BabyBearPoseidon2Config> {
-    SHARED_VK.get_or_init(|| {
+fn shared_widths_and_interactions() -> &'static (Vec<usize>, Vec<usize>) {
+    SHARED_WIDTHS_AND_INTERACTIONS.get_or_init(|| {
+        let vm = create_default_vm();
         let pk = vm.keygen();
-        pk.get_vk()
+        let vk = pk.get_vk();
+        (vk.total_widths(), vk.num_interactions())
     })
 }
 
@@ -154,42 +154,35 @@ fn benchmark_execute_metered(bencher: Bencher, program: &str) {
             let exe = load_program_executable(program).expect("Failed to load program executable");
             let state = create_initial_state(&vm.config().system.memory_config, &exe, vec![]);
 
-            let vk = shared_vk(&vm);
-
-            (vm.executor, exe, state, vk)
+            let (widths, interactions) = shared_widths_and_interactions();
+            (vm.executor, exe, state, widths, interactions)
         })
-        .bench_values(|(executor, exe, state, vk)| {
+        .bench_values(|(executor, exe, state, widths, interactions)| {
             executor
-                .execute_metered_from_state(exe, state, vk.total_widths(), vk.num_interactions())
+                .execute_metered_from_state(exe, state, widths, interactions)
                 .expect("Failed to execute program");
         });
 }
 
-// #[divan::bench(args = AVAILABLE_PROGRAMS, sample_count=3)]
-// fn benchmark_execute_e3(bencher: Bencher, program: &str) {
-//     bencher
-//         .with_inputs(|| {
-//             let vm = create_default_vm();
-//             let vk = shared_vk(&vm);
+#[divan::bench(args = AVAILABLE_PROGRAMS, sample_count=3)]
+fn benchmark_execute_e3(bencher: Bencher, program: &str) {
+    bencher
+        .with_inputs(|| {
+            let vm = create_default_vm();
+            let exe = load_program_executable(program).expect("Failed to load program executable");
+            let state = create_initial_state(&vm.config().system.memory_config, &exe, vec![]);
 
-//             let exe = load_program_executable(program).expect("Failed to load program executable");
-//             let state = create_initial_state(&vm.config().system.memory_config, &exe, vec![]);
+            let (widths, interactions) = shared_widths_and_interactions();
+            let segments = vm
+                .executor
+                .execute_metered(exe.clone(), vec![], widths, interactions)
+                .expect("Failed to execute program");
 
-//             let segments = vm
-//                 .executor
-//                 .execute_metered(
-//                     exe.clone(),
-//                     vec![],
-//                     vk.total_widths(),
-//                     vk.num_interactions(),
-//                 )
-//                 .expect("Failed to execute program");
-
-//             (vm.executor, exe, state, segments)
-//         })
-//         .bench_values(|(executor, exe, state, segments)| {
-//             executor
-//                 .execute_from_state(exe, state, &segments)
-//                 .expect("Failed to execute program");
-//         });
-// }
+            (vm.executor, exe, state, segments)
+        })
+        .bench_values(|(executor, exe, state, segments)| {
+            executor
+                .execute_from_state(exe, state, &segments)
+                .expect("Failed to execute program");
+        });
+}
