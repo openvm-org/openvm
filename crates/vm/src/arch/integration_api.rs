f84f4f5346e1ff3930c2dc8450c42e218afbb229
Arayi <arayik@intrinsictech.xyz>
2025-06-24 18:23:42 -0400

    remove duplication

--- crates/vm/src/arch/integration_api.rs
+++ crates/vm/src/arch/integration_api.rs
@@ -560,7 +560,6 @@ impl<'a, A, C, M> RecordArena<'a, AdapterCoreLayout<M>, (A, C)> for DenseRecordA
 where
     [u8]: CustomBorrow<'a, A, AdapterCoreLayout<M>> + CustomBorrow<'a, C, AdapterCoreLayout<M>>,
     M: Clone,
-    M: Clone,
     A: SizedRecord<AdapterCoreLayout<M>>,
     C: SizedRecord<AdapterCoreLayout<M>>,
 {
