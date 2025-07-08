459ddb1bb04152774f32c87ced40eae8fdfb0029
Stephen Hwang <stephenh@intrinsictech.xyz>
2025-07-08 14:57:07 -0400

    feat: kitchen sink print max trace heights

--- benchmarks/prove/src/bin/kitchen_sink.rs
+++ benchmarks/prove/src/bin/kitchen_sink.rs
@@ -5,7 +5,8 @@ use eyre::Result;
 use num_bigint::BigUint;
 use openvm_algebra_circuit::{Fp2Extension, ModularExtension};
 use openvm_benchmarks_prove::util::BenchmarkCli;
-use openvm_circuit::arch::{instructions::exe::VmExe, SystemConfig};
+use openvm_circuit::arch::{instructions::exe::VmExe, SingleSegmentVmExecutor, SystemConfig};
+use openvm_continuations::verifier::leaf::types::LeafVmVerifierInput;
 use openvm_ecc_circuit::{WeierstrassExtension, P256_CONFIG, SECP256K1_CONFIG};
 use openvm_native_recursion::halo2::utils::{CacheHalo2ParamsReader, DEFAULT_PARAMS_DIR};
 use openvm_pairing_circuit::{PairingCurve, PairingExtension};
@@ -20,6 +21,95 @@ use openvm_stark_sdk::{
     bench::run_with_metric_collection, config::baby_bear_poseidon2::BabyBearPoseidon2Engine,
 };
 use openvm_transpiler::FromElf;
+
+fn leaf_verifier_max_trace_heights() -> Result<Vec<u32>> {
+    let args = BenchmarkCli::parse();
+
+    let bn_config = PairingCurve::Bn254.curve_config();
+    let bls_config = PairingCurve::Bls12_381.curve_config();
+    let vm_config = SdkVmConfig::builder()
+        .system(SystemConfig::default().with_continuations().into())
+        .rv32i(Default::default())
+        .rv32m(Default::default())
+        .io(Default::default())
+        .keccak(Default::default())
+        .sha256(Default::default())
+        .bigint(Default::default())
+        .modular(ModularExtension::new(vec![
+            BigUint::from_str("1000000000000000003").unwrap(),
+            SECP256K1_CONFIG.modulus.clone(),
+            SECP256K1_CONFIG.scalar.clone(),
+            P256_CONFIG.modulus.clone(),
+            P256_CONFIG.scalar.clone(),
+            bn_config.modulus.clone(),
+            bn_config.scalar.clone(),
+            bls_config.modulus.clone(),
+            bls_config.scalar.clone(),
+            BigUint::from(2u32).pow(61) - BigUint::from(1u32),
+            BigUint::from(7u32),
+        ]))
+        .fp2(Fp2Extension::new(vec![
+            (
+                BN254_COMPLEX_STRUCT_NAME.to_string(),
+                bn_config.modulus.clone(),
+            ),
+            (
+                BLS12_381_COMPLEX_STRUCT_NAME.to_string(),
+                bls_config.modulus.clone(),
+            ),
+        ]))
+        .ecc(WeierstrassExtension::new(vec![
+            SECP256K1_CONFIG.clone(),
+            P256_CONFIG.clone(),
+            bn_config.clone(),
+            bls_config.clone(),
+        ]))
+        .pairing(PairingExtension::new(vec![
+            PairingCurve::Bn254,
+            PairingCurve::Bls12_381,
+        ]))
+        .build();
+
+    let elf = args.build_bench_program("kitchen-sink", &vm_config, None)?;
+    let exe = VmExe::from_elf(elf, vm_config.transpiler())?;
+
+    let sdk = Sdk::new();
+    let app_config = args.app_config(vm_config.clone());
+    let app_pk = Arc::new(sdk.app_keygen(app_config)?);
+    let app_committed_exe = commit_app_exe(app_pk.app_fri_params(), exe);
+
+    let agg_config = args.agg_config();
+    let agg_stark_pk = sdk.agg_stark_keygen(agg_config.agg_stark_config)?;
+
+    let app_proof = sdk.generate_app_proof(app_pk.clone(), app_committed_exe, StdIn::default())?;
+    let leaf_input = LeafVmVerifierInput::chunk_continuation_vm_proof(
+        &app_proof,
+        args.agg_tree_config.num_children_leaf,
+    )
+    .pop()
+    .unwrap();
+
+    let executor = {
+        let mut executor = SingleSegmentVmExecutor::new(agg_stark_pk.leaf_vm_pk.vm_config.clone());
+        executor.set_trace_height_constraints(
+            agg_stark_pk
+                .leaf_vm_pk
+                .vm_pk
+                .trace_height_constraints
+                .clone(),
+        );
+        executor
+    };
+
+    let vm_vk = agg_stark_pk.leaf_vm_pk.vm_pk.get_vk();
+    let max_trace_heights = executor.execute_metered(
+        app_pk.leaf_committed_exe.exe.clone(),
+        leaf_input.write_to_stream(),
+        &vm_vk.total_widths(),
+        &vm_vk.num_interactions(),
+    )?;
+    Ok(max_trace_heights)
+}
 
 fn main() -> Result<()> {
     let args = BenchmarkCli::parse();
@@ -87,6 +177,40 @@ fn main() -> Result<()> {
         &halo2_params_reader,
         &DefaultStaticVerifierPvHandler,
     )?;
+
+    {
+        let app_proof =
+            sdk.generate_app_proof(app_pk.clone(), app_committed_exe.clone(), StdIn::default())?;
+        let leaf_input = LeafVmVerifierInput::chunk_continuation_vm_proof(
+            &app_proof,
+            args.agg_tree_config.num_children_leaf,
+        )
+        .pop()
+        .unwrap();
+
+        let executor = {
+            let mut executor =
+                SingleSegmentVmExecutor::new(full_agg_pk.agg_stark_pk.leaf_vm_pk.vm_config.clone());
+            executor.set_trace_height_constraints(
+                full_agg_pk
+                    .agg_stark_pk
+                    .leaf_vm_pk
+                    .vm_pk
+                    .trace_height_constraints
+                    .clone(),
+            );
+            executor
+        };
+
+        let vm_vk = full_agg_pk.agg_stark_pk.leaf_vm_pk.vm_pk.get_vk();
+        let max_trace_heights = executor.execute_metered(
+            app_pk.leaf_committed_exe.exe.clone(),
+            leaf_input.write_to_stream(),
+            &vm_vk.total_widths(),
+            &vm_vk.num_interactions(),
+        )?;
+        println!("max_trace_heights: {:?}", max_trace_heights);
+    }
 
     run_with_metric_collection("OUTPUT_PATH", || -> Result<()> {
         let mut prover = EvmHalo2Prover::<_, BabyBearPoseidon2Engine>::new(
