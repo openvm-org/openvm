0abeb61475300f6d8ae6d6b2786f8511a16c057b
Xinding Wei <weixinding@gmail.com>
2025-07-14 16:28:13 -0700

    Add assertions back & Remove commented codes

--- extensions/rv32im/tests/programs/examples/hashmap.rs
+++ extensions/rv32im/tests/programs/examples/hashmap.rs
@@ -11,8 +11,8 @@ fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
     map.insert(2, 8);
-    // assert!(map.get(&1) == Some(&2));
-    // assert!(map.get(&2) == Some(&8));
-    // assert!(!map.contains_key(&3));
-    // println!("{:?}", map.get(&1).unwrap());
+    assert!(map.get(&1) == Some(&2));
+    assert!(map.get(&2) == Some(&8));
+    assert!(!map.contains_key(&3));
+    println!("{:?}", map.get(&1).unwrap());
 }
