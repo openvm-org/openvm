5be8f2037bced9acc7255d4c90e9660224257d55
Ayush Shukla <shuklaayush247@gmail.com>
2025-07-18 17:20:10 +0100

    fix(new-execution): optimize ecc e1/e2 execution (#1872)
    - avoid mod-builder for e1/e2 execution in ecc
    - use hardcoded implementation for k256, p256, bn254 and bls12-381

--- extensions/ecc/circuit/src/weierstrass_chip/mod.rs
+++ extensions/ecc/circuit/src/weierstrass_chip/mod.rs
@@ -1,39 +1,27 @@
 mod add_ne;
+mod curves;
 mod double;
 
 pub use add_ne::*;
 pub use double::*;
+
 #[cfg(test)]
 mod tests;
 
-use openvm_algebra_circuit::FieldExprVecHeapStep;
 use openvm_circuit::arch::{VmAirWrapper, VmChipWrapper};
 use openvm_mod_circuit_builder::{FieldExpressionCoreAir, FieldExpressionFiller};
 use openvm_rv32_adapters::{Rv32VecHeapAdapterAir, Rv32VecHeapAdapterFiller};
 
-pub type WeierstrassAir<
-    const NUM_READS: usize,
-    const BLOCKS: usize,
-    const BLOCK_SIZE: usize,
-> = VmAirWrapper<
-    Rv32VecHeapAdapterAir<NUM_READS, BLOCKS, BLOCKS, BLOCK_SIZE, BLOCK_SIZE>,
-    FieldExpressionCoreAir,
->;
+pub type WeierstrassAir<const NUM_READS: usize, const BLOCKS: usize, const BLOCK_SIZE: usize> =
+    VmAirWrapper<
+        Rv32VecHeapAdapterAir<NUM_READS, BLOCKS, BLOCKS, BLOCK_SIZE, BLOCK_SIZE>,
+        FieldExpressionCoreAir,
+    >;
 
-pub type WeierstrassStep<
-    const NUM_READS: usize,
-    const BLOCKS: usize,
-    const BLOCK_SIZE: usize,
-> = FieldExprVecHeapStep<NUM_READS, BLOCKS, BLOCK_SIZE>;
-
-pub type WeierstrassChip<
-    F,
-    const NUM_READS: usize,
-    const BLOCKS: usize,
-    const BLOCK_SIZE: usize,
-> = VmChipWrapper<
-    F,
-    FieldExpressionFiller<
-        Rv32VecHeapAdapterFiller<NUM_READS, BLOCKS, BLOCKS, BLOCK_SIZE, BLOCK_SIZE>,
-    >,
->;
+pub type WeierstrassChip<F, const NUM_READS: usize, const BLOCKS: usize, const BLOCK_SIZE: usize> =
+    VmChipWrapper<
+        F,
+        FieldExpressionFiller<
+            Rv32VecHeapAdapterFiller<NUM_READS, BLOCKS, BLOCKS, BLOCK_SIZE, BLOCK_SIZE>,
+        >,
+    >;
