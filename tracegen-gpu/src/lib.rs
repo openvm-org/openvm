509a8a2fc383c9412ef1469ecfcc9497a4cfe050
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-07-22 15:30:07 +0000

    chore: update backend to use new OpenVM traits + remove duplicate Chip

--- tracegen-gpu/src/lib.rs
+++ tracegen-gpu/src/lib.rs
@@ -18,27 +18,3 @@ pub mod testing;
 mod utils;
 
 pub use utils::*;
-
-pub trait DeviceChip<SC: StarkGenericConfig, PB: ProverBackend>: ChipUsageGetter {
-    fn air(&self) -> AirRef<SC>;
-    fn generate_trace(&self) -> PB::Matrix;
-    fn generate_device_air_proof_input(&self) -> DeviceAirProofRawInput<PB> {
-        DeviceAirProofRawInput {
-            cached_mains: vec![],
-            common_main: Some(self.generate_trace()),
-            public_values: vec![],
-        }
-    }
-}
-
-impl<SC: StarkGenericConfig, PB: ProverBackend, G: DeviceChip<SC, PB>> DeviceChip<SC, PB>
-    for Arc<G>
-{
-    fn air(&self) -> AirRef<SC> {
-        self.as_ref().air()
-    }
-
-    fn generate_trace(&self) -> PB::Matrix {
-        self.as_ref().generate_trace()
-    }
-}
