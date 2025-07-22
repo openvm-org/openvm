1ab9514810de4c3eb9864a317d8cbe493f88491c
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-07-22 15:31:52 +0000

    chore: temporarily comment out tracegen modules

--- tracegen-gpu/src/lib.rs
+++ tracegen-gpu/src/lib.rs
@@ -1,20 +1,10 @@
-use std::sync::Arc;
-
-use openvm_stark_backend::{
-    config::StarkGenericConfig, prover::hal::ProverBackend, AirRef, ChipUsageGetter,
-};
-use stark_backend_gpu::types::DeviceAirProofRawInput;
-
-#[macro_use]
-extern crate derive_new;
-
-pub mod dummy;
+// pub mod dummy;
 pub mod extensions;
-pub mod mod_builder;
-pub mod primitives;
-pub mod system;
-#[cfg(any(feature = "test-utils", test))]
-pub mod testing;
+// pub mod mod_builder;
+// pub mod primitives;
+// pub mod system;
+// #[cfg(any(feature = "test-utils", test))]
+// pub mod testing;
 mod utils;
 
 pub use utils::*;
