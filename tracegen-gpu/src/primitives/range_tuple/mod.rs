7e1f0c12e6780bfb6c9a507dd84d9c71d46af5b7
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-07-26 09:36:22 -0700

    fix: reset stateful chips after tracegen (#170)
    * fix: reset stateful chips after tracegen
    
    * codespell

--- tracegen-gpu/src/primitives/range_tuple/mod.rs
+++ tracegen-gpu/src/primitives/range_tuple/mod.rs
@@ -50,16 +50,19 @@ impl<RA, const N: usize> Chip<RA, GpuBackend> for RangeTupleCheckerChipGPU<N> {
             cpu_chip
                 .count
                 .iter()
-                .map(|c| c.load(Ordering::Relaxed))
+                .map(|c| c.swap(0, Ordering::Relaxed))
                 .collect::<Vec<_>>()
                 .to_device()
                 .unwrap()
         });
-        let trace =
-            DeviceMatrix::<F>::new(self.count.clone(), self.count.len(), NUM_RANGE_TUPLE_COLS);
+        // ATTENTION: we create a new buffer to copy `count` into because this chip is stateful and
+        // `count` will be reused.
+        let trace = DeviceMatrix::<F>::with_capacity(self.count.len(), NUM_RANGE_TUPLE_COLS);
         unsafe {
             tracegen(&self.count, &cpu_count, trace.buffer()).unwrap();
         }
+        // Zero the internal count buffer because this chip is stateful and may be used again.
+        self.count.fill_zero().unwrap();
         AirProvingContext::simple_no_pis(trace)
     }
 }
