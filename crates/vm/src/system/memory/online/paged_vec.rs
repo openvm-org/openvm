1cc957c27828bf3c9792ea0c996dbab23e277fa5
Arayi <arayik@intrinsictech.xyz>
2025-08-05 14:55:08 -0400

    page size = total size

--- crates/vm/src/system/memory/online/paged_vec.rs
+++ crates/vm/src/system/memory/online/paged_vec.rs
@@ -16,9 +16,8 @@ impl<T: Copy + Default> PagedVec<T> {
     #[inline]
     /// `total_size` is the capacity of elements of type `T`.
     pub fn new(total_size: usize) -> Self {
-        let num_pages = total_size.div_ceil(PAGE_SIZE);
         Self {
-            pages: vec![Some(vec![T::default(); PAGE_SIZE].into_boxed_slice()); num_pages],
+            pages: vec![Some(vec![T::default(); total_size].into_boxed_slice()); 1],
         }
     }
 
@@ -26,46 +25,44 @@ impl<T: Copy + Default> PagedVec<T> {
     /// exists.
     #[inline]
     pub fn get(&mut self, index: usize) -> &T {
-        let page_idx = index >> PAGE_SIZE_LOG2;
-        let offset = index & (PAGE_SIZE - 1);
+        // let page_idx = index >> PAGE_SIZE_LOG2;
+        // let offset = index & (PAGE_SIZE - 1);
 
-        assert!(
-            page_idx < self.pages.len(),
-            "PagedVec::get index out of bounds: {} >= {}",
-            index,
-            self.pages.len() << PAGE_SIZE_LOG2
-        );
+        // assert!(
+        //     page_idx < self.pages.len(),
+        //     "PagedVec::get index out of bounds: {} >= {}",
+        //     index,
+        //     self.pages.len() << PAGE_SIZE_LOG2
+        // );
 
         unsafe {
             // SAFETY:
             // - We just ensured the page exists and has size `page_size`
             // - offset < page_size by construction
             self.pages
-                .get_unchecked(page_idx)
+                .get_unchecked(0)
                 .as_ref()
                 .unwrap()
-                .get_unchecked(offset)
+                .get_unchecked(index)
         }
     }
 
     /// Panics if the index is out of bounds. Creates new page before write when necessary.
     #[inline]
     pub fn set(&mut self, index: usize, value: T) {
-        let page_idx = index >> PAGE_SIZE_LOG2;
-        let offset = index & (PAGE_SIZE - 1);
+        // let page_idx = index >> PAGE_SIZE_LOG2;
+        // let offset = index & (PAGE_SIZE - 1);
 
-        assert!(
-            page_idx < self.pages.len(),
-            "PagedVec::set index out of bounds: {} >= {}",
-            index,
-            self.pages.len() << PAGE_SIZE_LOG2
-        );
+        // assert!(
+        //     page_idx < self.pages.len(),
+        //     "PagedVec::set index out of bounds: {} >= {}",
+        //     index,
+        //     self.pages.len() << PAGE_SIZE_LOG2
+        // );
 
-        let page = self.pages[page_idx].as_mut().unwrap();
-        // SAFETY:
-        // - If page exists, then it has size `page_size`
         unsafe {
-            *page.get_unchecked_mut(offset) = value;
+            let page = self.pages.get_unchecked_mut(0).as_mut().unwrap();
+            *page.get_unchecked_mut(index) = value;
         }
     }
 
@@ -73,33 +70,13 @@ impl<T: Copy + Default> PagedVec<T> {
     where
         T: Send + Sync,
     {
-        self.pages
-            .par_iter()
-            .enumerate()
-            .filter_map(move |(page_idx, page)| {
-                page.as_ref().map(move |p| {
-                    p.par_iter()
-                        .enumerate()
-                        .map(move |(offset, &value)| (page_idx * PAGE_SIZE + offset, value))
-                })
-            })
-            .flatten()
+        self.pages[0].as_ref().unwrap().par_iter().enumerate().map(move |(offset, &value)| (offset, value))
     }
 
     pub fn iter(&self) -> impl Iterator<Item = (usize, T)> + '_
     where
         T: Send + Sync,
     {
-        self.pages
-            .iter()
-            .enumerate()
-            .filter_map(move |(page_idx, page)| {
-                page.as_ref().map(move |p| {
-                    p.iter()
-                        .enumerate()
-                        .map(move |(offset, &value)| (page_idx * PAGE_SIZE + offset, value))
-                })
-            })
-            .flatten()
+        self.pages[0].as_ref().unwrap().iter().enumerate().map(move |(offset, &value)| (offset, value))
     }
 }
