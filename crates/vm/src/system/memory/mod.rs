2c0b5ace23d44196a97c5a89871d0238f92e80ba
Alexander Golovanov <Golovanov12345@gmail.com>
2025-08-08 22:49:10 +0200

    Move the online tests to memory `tests.rs`

--- crates/vm/src/system/memory/mod.rs
+++ crates/vm/src/system/memory/mod.rs
@@ -16,8 +16,8 @@ pub mod merkle;
 pub mod offline_checker;
 pub mod online;
 pub mod persistent;
-// #[cfg(test)]
-// mod tests;
+#[cfg(test)]
+mod tests;
 pub mod volatile;
 
 pub use controller::*;
