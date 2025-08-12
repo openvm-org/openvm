6791dfb2eec349accd262df7f50dba6027cb68dc
Ayush Shukla <ayush@axiom.xyz>
2025-08-12 19:29:08 +0200

    fix: ignore metered cost benches

--- benchmarks/execute/benches/execute.rs
+++ benchmarks/execute/benches/execute.rs
@@ -298,7 +298,7 @@ fn benchmark_execute_metered(bencher: Bencher, program: &str) {
         });
 }
 
-#[divan::bench(args = AVAILABLE_PROGRAMS, sample_count=5)]
+#[divan::bench(ignore, args = AVAILABLE_PROGRAMS, sample_count=5)]
 fn benchmark_execute_metered_cost(bencher: Bencher, program: &str) {
     bencher
         .with_inputs(|| {
