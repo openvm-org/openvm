openvm-org/stark-backend

chore: Reorganize axiom-gpu repository (#71)
- Moves cuda/kernels/backend into crates/backend by a) moving CUDA files into crates/backend/cuda and b) moving wrapper .rs files to their appropriate locations in crates/backend/src
- Moves cuda/kernels/tracegen into crates/tracegen analogously to above
- Add .clang-format file and a script to generate .clangd, which allows devs to use Intellisense properly
- Move cuda/fields and cuda/utils into crates/backend/cuda/include and crates/backend/src respectively
- Add tracegen documentation (i.e. README and comments)

src/lib.rs
@@ -1,5 +1,6 @@
 pub mod base;
 mod committer;
+pub mod cuda;
 pub mod fri_log_up;
 mod lde;
 mod merkle_tree;

openvm-org/openvm

tracegen-gpu/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod dummy;
+pub mod extensions;
 pub mod primitives;
+pub mod system;
 pub mod utils;