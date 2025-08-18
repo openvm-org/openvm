197b444ccbaa91abeee8840bf38126fa413e972a
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-08-18 09:13:07 -0700

    chore: remove args_conflict_with_subcommands

--- crates/cli/src/bin/cargo-openvm.rs
+++ crates/cli/src/bin/cargo-openvm.rs
@@ -12,7 +12,7 @@ pub enum Cargo {
 }
 
 #[derive(clap::Args)]
-#[command(author, about, long_about = None, args_conflicts_with_subcommands = true, version = OPENVM_VERSION_MESSAGE)]
+#[command(author, about, long_about = None, version = OPENVM_VERSION_MESSAGE)]
 pub struct VmCli {
     #[command(subcommand)]
     pub command: VmCliCommands,
