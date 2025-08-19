d36dbaae798de6858a4cec917b69ee945f7d7092
Peyman Jabbarzade <peyman.jabarzade@gmail.com>
2025-08-19 11:41:59 -0400

    feat: initial code for memcpy chip

--- crates/vm/src/system/memory/offline_checker/mod.rs
+++ crates/vm/src/system/memory/offline_checker/mod.rs
@@ -43,6 +43,36 @@ pub type MemoryReadAuxRecord = MemoryBaseAuxRecord;
 
 #[repr(C)]
 #[derive(Debug, Clone)]
+pub struct MemoryExtendedAuxRecord {
+    pub prev_timestamp: u32,
+    pub timestamp_lt_aux: [u32; AUX_LEN],
+}
+
+impl MemoryExtendedAuxRecord {
+    pub fn from_aux_cols<F: PrimeField32>(aux_cols: MemoryBaseAuxCols<F>) -> Self {
+        Self {
+            prev_timestamp: aux_cols.prev_timestamp.as_canonical_u32(),
+            timestamp_lt_aux: aux_cols
+                .timestamp_lt_aux
+                .lower_decomp
+                .map(|x| x.as_canonical_u32()),
+        }
+    }
+
+    pub fn to_aux_cols<F: PrimeField32>(&self) -> MemoryBaseAuxCols<F> {
+        MemoryBaseAuxCols {
+            prev_timestamp: F::from_canonical_u32(self.prev_timestamp),
+            timestamp_lt_aux: LessThanAuxCols {
+                lower_decomp: self.timestamp_lt_aux.map(|x| F::from_canonical_u32(x)),
+            },
+        }
+    }
+}
+
+pub type MemoryReadAuxRecord = MemoryBaseAuxRecord;
+
+#[repr(C)]
+#[derive(Debug, Clone)]
 pub struct MemoryWriteAuxRecord<T, const NUM_LIMBS: usize> {
     pub prev_timestamp: u32,
     pub prev_data: [T; NUM_LIMBS],
--- extensions/memcpy/circuit/src/lib.rs
+++ extensions/memcpy/circuit/src/lib.rs
@@ -7,7 +7,10 @@ pub use bus::*;
 pub use extension::*;
 pub use iteration::*;
 pub use loops::*;
-use openvm_circuit::system::memory::{merkle::public_values::PUBLIC_VALUES_AS, online::{GuestMemory, TracingMemory}};
+use openvm_circuit::system::memory::{
+    merkle::public_values::PUBLIC_VALUES_AS,
+    online::{GuestMemory, TracingMemory},
+};
 use openvm_instructions::riscv::{RV32_MEMORY_AS, RV32_REGISTER_AS};
 
 // ==== Do not change these constants! ====
@@ -18,7 +21,6 @@ pub const A1_REGISTER_PTR: usize = 11 * 4;
 pub const A2_REGISTER_PTR: usize = 12 * 4;
 pub const A3_REGISTER_PTR: usize = 13 * 4;
 pub const A4_REGISTER_PTR: usize = 14 * 4;
-
 
 // TODO: These are duplicated from extensions/rv32im/circuit/src/adapters/mod.rs
 // to prevent cyclic dependencies. Fix this.
