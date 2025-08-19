openvm-org/stark-backend

feat: RangeChecker tracegen (#61)
* histogram ready and tested

* var_range tracegen

* half of the test with dummy chip

* new tracegen with warp primitives

* Buffer -> Matrix

* DeviceProofInput

* test passed

* -1 allocation

src/lib.rs
@@ -1,12 +1,12 @@
 pub mod base;
 mod committer;
-mod fri_log_up;
+pub mod fri_log_up;
 mod lde;
 mod merkle_tree;
 mod opener;
 mod quotient;
 mod transpiler;
-mod types;
+pub mod types;
 mod view; // temporary until we have GPU perm trace
 
 pub mod prelude {

openvm-org/openvm

tracegen-gpu/src/lib.rs
@@ -1,0 +1,2 @@
+pub mod dummy;
+pub mod primitives;