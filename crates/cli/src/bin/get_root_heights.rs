109f144e8a327487fa8156201b19890b75f67f4e
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-08-18 22:54:33 -0700

    chore: get root verifier vk

--- crates/cli/src/bin/get_root_heights.rs
+++ crates/cli/src/bin/get_root_heights.rs
@@ -1,7 +1,10 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use openvm_sdk::{fs::read_object_from_file, keygen::AggProvingKey};
+use openvm_sdk::{
+    fs::{read_object_from_file, write_to_file_json},
+    keygen::AggProvingKey,
+};
 
 #[derive(Parser, Debug)]
 pub struct Cli {
@@ -12,6 +15,10 @@ pub struct Cli {
 fn main() -> eyre::Result<()> {
     let args = Cli::parse();
     let pk: AggProvingKey = read_object_from_file(&args.path)?;
+    write_to_file_json(
+        args.path.with_extension("root.vk.json"),
+        pk.root_verifier_pk.vm_pk.vm_pk.get_vk(),
+    )?;
     println!(
         "root verifier circuit fixed air heights: {:?}",
         pk.root_verifier_pk.air_heights
