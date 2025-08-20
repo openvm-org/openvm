6c973ea57a661569f592b0c5970b2862e98689bf
Grigorii Alekseev <grigorii@intrinsictech.xyz>
2025-08-20 23:41:53 +0000

    chore: keccak migrate

--- crates/vm/build.rs
+++ crates/vm/build.rs
@@ -12,6 +12,7 @@ fn main() {
             .include_from_dep("DEP_CUDA_COMMON_INCLUDE")
             .include("../circuits/primitives/cuda/include")
             .include("../circuits/poseidon2-air/cuda/include")
+            .include("cuda/include")
             .watch("cuda/src/system")
             .library_name("tracegen_gpu_system")
             .files_from_glob("cuda/src/system/**/*.cu");
