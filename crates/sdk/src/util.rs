e82da100145cc126e12a74e8a004dd646c2e2bd1
Yi Sun <yi-sun@users.noreply.github.com>
2025-08-20 21:35:17 -0400

    chore: reduce warn to info for degree inequality

--- crates/sdk/src/util.rs
+++ crates/sdk/src/util.rs
@@ -2,9 +2,15 @@ use openvm_circuit::arch::SystemConfig;
 use openvm_stark_sdk::config::FriParameters;
 
 pub fn check_max_constraint_degrees(config: &SystemConfig, fri_params: &FriParameters) {
-    if config.max_constraint_degree != fri_params.max_constraint_degree() {
+    if config.max_constraint_degree > fri_params.max_constraint_degree() {
         tracing::warn!(
-            "config.max_constraint_degree ({}) != fri_params.max_constraint_degree() ({})",
+            "config.max_constraint_degree ({}) > fri_params.max_constraint_degree() ({})",
+            config.max_constraint_degree,
+            fri_params.max_constraint_degree()
+        );
+    } else if config.max_constraint_degree < fri_params.max_constraint_degree() {
+        tracing::info!(
+            "config.max_constraint_degree ({}) < fri_params.max_constraint_degree() ({})",
             config.max_constraint_degree,
             fri_params.max_constraint_degree()
         );
