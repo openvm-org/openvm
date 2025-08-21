fadc0a1d3b7452f99169be037be50ae77db56489
Alexander Golovanov <Sample text>
2025-08-21 16:12:51 +0000

    Fix

--- extensions/native/circuit/build.rs
+++ extensions/native/circuit/build.rs
@@ -12,7 +12,7 @@ fn main() {
             .include_from_dep("DEP_CUDA_COMMON_INCLUDE")
             .include("../../../crates/circuits/primitives/cuda/include")
             .include("../../../crates/circuits/poseidon2-air/cuda/include")
-            .include("../../../crates/vm/cuda/src")
+            .include("../../../crates/vm/cuda/include")
             .watch("cuda/src")
             .library_name("tracegen_gpu_native")
             .files_from_glob("cuda/src/**/*.cu");
