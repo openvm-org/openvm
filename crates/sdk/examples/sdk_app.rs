e71e92123cde64bfaf06f7482f7dbf5d5015d2c9
Stephen Hwang <stephenh@intrinsictech.xyz>
2025-08-27 10:56:13 -0400

    docs: update SDK docs with standard section + new example

--- crates/sdk/examples/sdk_app.rs
+++ crates/sdk/examples/sdk_app.rs
@@ -1,27 +1,38 @@
+// [!region dependencies]
 use openvm_build::GuestOptions;
 use openvm_sdk::{prover::verify_app_proof, Sdk, StdIn};
+// [!endregion dependencies]
 
 #[allow(unused_variables, unused_doc_comments)]
 fn main() -> eyre::Result<()> {
-    // 1. Build the VmConfig with the extensions needed.
+    // [!region init]
+    // 1. Initialize Sdk with the standard configuration.
     let sdk = Sdk::standard();
+    // [!endregion init]
 
+    // [!region build]
     // 2 Build the ELF with default guest options and target filter.
     let guest_opts = GuestOptions::default();
     let target_path = "your_path_project_root";
     let elf = sdk.build(guest_opts, target_path, &None, None)?;
+    // [!endregion build]
 
+    // [!region execution]
     // 3. Run the program with default inputs.
     let output = sdk.execute(elf.clone(), StdIn::default())?;
     println!("public values output: {:?}", output);
+    // [!endregion execution]
 
+    // [!region proof_generation]
     // 4. Generate an app proof.
     let mut prover = sdk.app_prover(elf)?.with_program_name("test_program");
-    let app_commit = prover.app_commit();
     let proof = prover.prove(StdIn::default())?;
+    // [!endregion proof_generation]
 
+    // [!region verification]
     // 5. Verify your program at the app level.
     verify_app_proof(&sdk.app_pk().get_app_vk(), &proof)?;
+    // [!endregion verification]
 
     Ok(())
 }
