c5edd6c0d901b1895ae1b111097fe76649df8a8a
Yi Sun <yi-sun@users.noreply.github.com>
2025-08-26 21:14:09 -0400

    fix: change rustdocs link

--- docs/vocs/vocs.config.ts
+++ docs/vocs/vocs.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     { text: 'Specs', link: '/specs/openvm/overview' },
     {
       text: 'Rustdocs',
-      link: '/docs'
+      link: `${process.env.SITE_URL || 'http://localhost:3000'}/docs/openvm`
     },
     { text: 'GitHub', link: 'https://github.com/openvm-org/openvm' },
     {
