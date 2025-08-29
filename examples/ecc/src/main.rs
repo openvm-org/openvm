38929c75b34d9145973a807917c9799a42443d89
Alexander Golovanov <Golovanov12345@gmail.com>
2025-08-30 00:25:18 +0200

    Get rid of `openvm::entry` but add `use openvm` so stuff is not optimized away

--- examples/ecc/src/main.rs
+++ examples/ecc/src/main.rs
@@ -1,5 +1,6 @@
 // [!region imports]
 use hex_literal::hex;
+use openvm;
 use openvm_algebra_guest::IntMod;
 use openvm_ecc_guest::weierstrass::WeierstrassPoint;
 use openvm_k256::{Secp256k1Coord, Secp256k1Point};
