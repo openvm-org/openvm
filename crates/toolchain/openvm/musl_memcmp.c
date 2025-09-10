8f4cb3d345b5ef8d03900c47dc2e8093b1d07b72
gunadigani <gunadigan@gmail.com>
2025-09-10 11:37:47 -0400

    feat: add musl's memcmp C implementation

---
+++ crates/toolchain/openvm/musl_memcmp.c
@@ -0,0 +1,8 @@
+typedef unsigned int size_t;
+
+int memcmp(const void *vl, const void *vr, size_t n)
+{
+	const unsigned char *l=vl, *r=vr;
+	for (; n && *l == *r; n--, l++, r++);
+	return n ? *l-*r : 0;
+}
