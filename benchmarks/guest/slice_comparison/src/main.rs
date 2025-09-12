da7db6fe5c3a9a14a40d78e152bd5b4fc8d47695
gunadigani <gunadigan@gmail.com>
2025-09-12 09:08:21 -0400

    chore: rename random_slice_comparison to slice_comparison

---
+++ benchmarks/guest/slice_comparison/src/main.rs
@@ -0,0 +1,45 @@
+use openvm::io::reveal_u32;
+
+extern "C" {
+    fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32;
+}
+
+const N: usize = 32_768;
+
+fn main() {
+    let mut a: [u128; N] = [u128::MAX - 1; N];
+    let mut b: [u128; N] = [u128::MAX - 1; N];
+
+    let mut total_res: i32 = 0;
+
+    // equal case
+    {
+        let res = unsafe {
+            memcmp(
+                a.as_mut_ptr() as *const u8,
+                b.as_mut_ptr() as *const u8,
+                N * size_of::<u128>(),
+            )
+        };
+        total_res += res.signum();
+    }
+
+    let indices = [N - 1, N - 10, N - 100, N - 10000];
+
+    // not equal case
+    for i in 0..indices.len() {
+        let idx = indices[i];
+        a[idx] = u128::MAX;
+        let res = unsafe {
+            memcmp(
+                a.as_mut_ptr() as *const u8,
+                b.as_mut_ptr() as *const u8,
+                N * size_of::<u128>(),
+            )
+        };
+        a[idx] = u128::MAX - 1;
+        total_res += res.signum();
+    }
+
+    reveal_u32(total_res as u32, 0);
+}
