32bda466964bbd4be5a0236dde403c61f7e98761
Zach Langley <zblangley@gmail.com>
2025-10-02 12:51:35 -0400

    fix: sample mu after observing claims (#27)
    The batching randomness for the claims must be independent of the claims

--- crates/recursion/src/tests.rs
+++ crates/recursion/src/tests.rs
@@ -80,6 +80,7 @@ fn test_circuit() {
         .collect();
 
     let proof = Proof {
+        public_values: vec![pis.clone()],
         common_main_commit: [21, 22, 23, 24, 25, 26, 27, 28].map(F::from_canonical_u32),
         trace_shapes: vec![Some(TraceShape {
             hypercube_dim: log_trace_degree - params.l_skip,
