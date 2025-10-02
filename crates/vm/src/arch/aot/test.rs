98cdee6c0a739014c525f2cdfec1cb60991b9c17
Ubuntu <ubuntu@ip-172-31-18-208.ec2.internal>
2025-10-02 20:35:19 +0000

    feat: tests for loadb and add

--- crates/vm/src/arch/aot/test.rs
+++ crates/vm/src/arch/aot/test.rs
@@ -6,23 +6,43 @@ use openvm_rv32im_transpiler::BaseAluOpcode;
 use openvm_stark_backend::p3_field::FieldAlgebra;
 use openvm_stark_sdk::p3_baby_bear::BabyBear;
 use p3_baby_bear::BabyBearParameters;
+use openvm_rv32im_transpiler::Rv32LoadStoreOpcode;
 
 #[test]
 fn test_aot() {
     type F = BabyBear;
 
-    let instruction = Instruction {
-        opcode: BaseAluOpcode::ADD.global_opcode(),
+    let program = Program::from_instructions(&[
+    Instruction {
+        opcode: Rv32LoadStoreOpcode::LOADB.global_opcode(),
         a: F::from_canonical_u32(0),
-        b: F::from_canonical_u32(0),
+        b: F::from_canonical_u32(1),
         c: F::from_canonical_u32(0),
         d: F::from_canonical_u32(0),
         e: F::from_canonical_u32(0),
         f: F::from_canonical_u32(0),
         g: F::from_canonical_u32(0),
-    };
-
-    let program = Program::from_instructions(&[instruction]);
+    }, 
+    Instruction {
+        opcode: Rv32LoadStoreOpcode::LOADB.global_opcode(),
+        a: F::from_canonical_u32(1),
+        b: F::from_canonical_u32(2),
+        c: F::from_canonical_u32(0),
+        d: F::from_canonical_u32(0),
+        e: F::from_canonical_u32(0),
+        f: F::from_canonical_u32(0),
+        g: F::from_canonical_u32(0),
+    }, 
+    Instruction {
+        opcode: BaseAluOpcode::ADD.global_opcode(),
+        a: F::from_canonical_u32(2),
+        b: F::from_canonical_u32(0),
+        c: F::from_canonical_u32(1),
+        d: F::from_canonical_u32(0),
+        e: F::from_canonical_u32(0),
+        f: F::from_canonical_u32(0),
+        g: F::from_canonical_u32(0),
+    }]);
     let exe = VmExe {
         program,
         pc_start: 0,
