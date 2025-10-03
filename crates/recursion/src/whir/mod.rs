352083731de323ed6309ad452fd27cefa04890da
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-10-03 13:23:37 -0700

    feat: add grinding (#40)
    Co-authored-by: Zach Langley <zach@axiom.xyz>

--- crates/recursion/src/whir/mod.rs
+++ crates/recursion/src/whir/mod.rs
@@ -1,8 +1,10 @@
 use core::iter::zip;
 use std::sync::Arc;
 
+use itertools::izip;
 use openvm_stark_backend::{AirRef, prover::types::AirProofRawInput};
 use openvm_stark_sdk::config::baby_bear_poseidon2::BabyBearPoseidon2Config;
+use p3_field::Powers;
 use stark_backend_v2::{
     F,
     keygen::types::{MultiStarkVerifyingKeyV2, SystemParams},
@@ -56,13 +58,17 @@ impl AirModule for WhirModule {
             initial_round_merkle_proofs,
             codeword_opened_rows,
             codeword_merkle_proofs,
+            whir_pow_witnesses,
         } = &proof.whir_proof;
 
         let _mu = ts.sample_ext();
 
         let mut sumcheck_poly_iter = whir_sumcheck_polys.iter();
         debug_assert_eq!(ood_values.len(), codeword_commits.len());
-        for (i, (ood, commit)) in zip(ood_values, codeword_commits).enumerate() {
+        debug_assert_eq!(ood_values.len(), whir_pow_witnesses.len());
+        for (i, (ood, commit, pow_witness)) in
+            izip!(ood_values, codeword_commits, whir_pow_witnesses).enumerate()
+        {
             for _ in 0..vk.inner.params.k_whir {
                 if let Some(evals) = sumcheck_poly_iter.next() {
                     for eval in evals {
@@ -76,23 +82,11 @@ impl AirModule for WhirModule {
             let _z0 = ts.sample_ext();
             ts.observe_ext(*ood);
 
+            ts.observe(*pow_witness);
+            let _pow_bits = ts.sample();
+
             for j in 0..vk.inner.params.num_whir_queries {
                 let _bits = ts.sample();
-                if i == 0 {
-                    for l in 0..initial_round_opened_rows.len() {
-                        ts.observe_slice(&initial_round_opened_rows[l][j]);
-                        for commit in &initial_round_merkle_proofs[l][j] {
-                            ts.observe_slice(commit);
-                        }
-                    }
-                } else {
-                    for val in &codeword_opened_rows[i - 1][j] {
-                        ts.observe_ext(*val);
-                    }
-                    for commit in &codeword_merkle_proofs[i - 1][j] {
-                        ts.observe_slice(commit);
-                    }
-                }
             }
             let _gamma = ts.sample_ext();
         }
