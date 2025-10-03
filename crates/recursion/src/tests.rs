138c1f8b4e2a292756692352cbb8c6f71279c7e3
Zach Langley <zblangley@gmail.com>
2025-10-03 13:33:24 -0400

    fix: preflight k_whir (#41)
    Also improves testing:
    - Adds test to ensure prover and preflight sponge state agree to prevent
    regressions
    - Adds test-utils module in `backend` to provide test fixture; use
    `test-utils` feature flag

--- crates/recursion/src/tests.rs
+++ crates/recursion/src/tests.rs
@@ -1,96 +1,44 @@
-use core::cmp::max;
-use std::sync::Arc;
-
-use openvm_stark_backend::{engine::StarkEngine, prover::types::AirProvingContext};
+use openvm_stark_backend::engine::StarkEngine;
 use openvm_stark_sdk::{
     config::{FriParameters, baby_bear_poseidon2::BabyBearPoseidon2Engine},
-    dummy_airs::fib_air::air::FibonacciAir,
     engine::StarkFriEngine,
 };
-use p3_field::{FieldAlgebra, PrimeField32};
-use p3_matrix::dense::RowMajorMatrix;
 use stark_backend_v2::{
-    F,
-    keygen::types::{MultiStarkProvingKeyV2, SystemParams},
     poseidon2::sponge::DuplexSponge,
-    prover::{AirProvingContextV2, ProvingContextV2, prove},
+    test_utils::{FibFixture, test_system_params_small},
 };
 
 use crate::system::VerifierCircuit;
 
-fn get_fib_number(n: usize) -> u32 {
-    let mut a = 0;
-    let mut b = 1;
-    for _ in 0..n - 1 {
-        let c = (a + b) % (15 * (1 << 27) + 1);
-        a = b;
-        b = c;
-    }
-    b
-}
-
-pub fn generate_trace_rows<F: PrimeField32>(a: u32, b: u32, n: usize) -> RowMajorMatrix<F> {
-    assert!(n.is_power_of_two());
-
-    let mut rows = vec![vec![F::from_canonical_u32(a), F::from_canonical_u32(b)]];
-
-    for i in 1..n {
-        rows.push(vec![rows[i - 1][1], rows[i - 1][0] + rows[i - 1][1]]);
-    }
-
-    RowMajorMatrix::new(rows.concat(), 2)
-}
-
 #[test]
-fn test_circuit() {
-    let params = SystemParams {
-        l_skip: 2,
-        n_stack: 5,
-        log_blowup: 1,
-        k_whir: 2,
-        num_whir_queries: 5, // TEST
-    };
+fn test_recursion_circuit_single_fib() {
     let log_trace_degree = 3;
 
-    // Public inputs:
-    let a = 0u32;
-    let b = 1u32;
-    let n = 1usize << log_trace_degree;
-
-    let pis = [a, b, get_fib_number(n)]
-        .map(F::from_canonical_u32)
-        .to_vec();
-    let air = FibonacciAir;
-    // FRI params will be ignored later
-    let engine = BabyBearPoseidon2Engine::new(FriParameters::standard_fast());
-    let mut keygen_builder = engine.keygen_builder();
-    let air_ids = engine.set_up_keygen_builder(&mut keygen_builder, &[Arc::new(air)]);
-    assert_eq!(air_ids[0], 0);
-    let fib_pk_v1 = keygen_builder.generate_pk();
-    let fib_pk = MultiStarkProvingKeyV2::from_v1(params, fib_pk_v1);
-    let fib_vk = fib_pk.get_vk();
-
-    let trace = generate_trace_rows::<F>(a, b, n);
-
-    let single_ctx = AirProvingContextV2::from_v1(
-        params,
-        AirProvingContext::simple(Arc::new(trace), pis.clone()),
-    );
-    let ctx = ProvingContextV2::new(vec![(air_ids[0], single_ctx)]);
-
-    let mut transcript = DuplexSponge::default();
-    let proof = prove(&mut transcript, &fib_pk, ctx);
+    let fib = FibFixture::build(test_system_params_small(), 0, 1, 1 << log_trace_degree);
+    let proof = fib.prove();
 
     let circuit = VerifierCircuit::new();
-
     let engine = BabyBearPoseidon2Engine::new(FriParameters::standard_fast());
     let mut keygen_builder = engine.keygen_builder();
-
     for air in circuit.airs() {
         keygen_builder.add_air(air);
     }
     let pk = keygen_builder.generate_pk();
-
-    let proof_inputs = circuit.generate_proof_inputs(&fib_vk, &proof);
+    let proof_inputs = circuit.generate_proof_inputs(&fib.vk, &proof);
     engine.debug(&circuit.airs(), &pk.per_air, &proof_inputs);
+}
+
+#[test]
+fn test_preflight_single_fib() {
+    let fib = FibFixture::build(test_system_params_small(), 0, 1, 1 << 5);
+
+    let mut prover_sponge = DuplexSponge::default();
+    let proof = fib.prove_with_sponge(&mut prover_sponge);
+
+    let circuit = VerifierCircuit::new();
+    let mut preflight = circuit.run_preflight(&fib.vk, &proof);
+
+    let pro = prover_sponge.sample();
+    let pre = preflight.transcript.sponge.sample();
+    assert_eq!(pro, pre);
 }
