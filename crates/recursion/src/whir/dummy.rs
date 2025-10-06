88e252eb638d5584864e770de2527b0a69db1a42
Zach Langley <zblangley@gmail.com>
2025-10-06 16:34:40 -0400

    refactor: make DuplexSponge a trait (#46)
    Allows for more fine-grained detection of preflight sponge divergence

--- crates/recursion/src/whir/dummy.rs
+++ crates/recursion/src/whir/dummy.rs
@@ -7,7 +7,10 @@ use openvm_stark_backend::{
 use p3_air::{Air, AirBuilder, BaseAir};
 use p3_field::FieldAlgebra;
 use p3_matrix::{Matrix, dense::RowMajorMatrix};
-use stark_backend_v2::{F, keygen::types::MultiStarkVerifyingKeyV2, proof::Proof};
+use stark_backend_v2::{
+    F, keygen::types::MultiStarkVerifyingKeyV2, poseidon2::sponge::FiatShamirTranscript,
+    proof::Proof,
+};
 use stark_recursion_circuit_derive::AlignedBorrow;
 
 use crate::{
@@ -99,10 +102,10 @@ impl<AB: AirBuilder + InteractionBuilder> Air<AB> for DummyWhirAir {
     }
 }
 
-pub(crate) fn generate_trace(
+pub(crate) fn generate_trace<TS: FiatShamirTranscript>(
     vk: &MultiStarkVerifyingKeyV2,
     proof: &Proof,
-    preflight: &Preflight,
+    preflight: &Preflight<TS>,
 ) -> RowMajorMatrix<F> {
     let mut stacking_commitments_msgs = preflight.stacking_commitments_msgs(vk, proof).into_iter();
     let mut stacking_widths_bus_msgs = preflight.stacking_widths_bus_msgs(vk).into_iter();
