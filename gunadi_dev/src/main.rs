e2de90b166559f5e3563d32a7d27908b21cb7b1f
Ubuntu <ubuntu@ip-172-31-18-208.ec2.internal>
2025-10-09 16:47:05 +0000

    feat(in-progress): trying to get pre_compute_insns to compile

---
+++ gunadi_dev/src/main.rs
@@ -0,0 +1,112 @@
+use openvm_instructions::exe::VmExe;
+use openvm_circuit::arch::VmExecState;
+use openvm_circuit::arch::VmState; 
+use openvm_instructions::program::Program;
+use openvm_instructions::instruction::Instruction;
+use openvm_rv32im_transpiler::BaseAluOpcode;
+use openvm_instructions::LocalOpcode;
+use openvm_circuit::arch::MemoryConfig;
+use p3_baby_bear::BabyBear;
+use openvm_circuit::arch::SystemConfig;
+use openvm_circuit::system::memory::online::GuestMemory;
+use openvm_instructions::exe::SparseMemoryImage;
+use openvm_circuit::arch::execution_mode::ExecutionCtx;
+use openvm_circuit::arch::interpreter::get_pre_compute_max_size;
+use openvm_circuit::arch::ExecutorInventory;
+use openvm_circuit::arch::VmExecutor;
+use openvm_rv32im_circuit::Rv32IExecutor;
+
+use openvm_circuit::derive::VmConfig;
+
+use openvm_rv32im_circuit::{
+    Rv32I, Rv32Io, Rv32M
+};
+
+#[derive(Clone, Debug, VmConfig)]
+pub struct ExecuteConfig {
+    pub system: SystemConfig,
+    pub rv32i: Rv32I,
+    pub rv32m: Rv32M,
+    pub io: Rv32Io,
+}
+
+impl Default for ExecuteConfig {
+    fn default() -> Self {
+        Self {
+            system: SystemConfig::default(),
+            rv32i: Rv32I,
+            rv32m: Rv32M::default(),
+            io: Rv32Io,
+        }
+    }
+}
+
+use openvm_stark_backend::config::Val;
+
+
+type F = BabyBear;
+type Executor = Rv32IExecutor;
+
+pub struct AotInstance {
+
+}
+
+impl AotInstance {
+    pub fn new(
+        inventory: &ExecutorInventory<Executor>,
+        exe: &VmExe<F>,
+    ) {
+        let memory_config : MemoryConfig = Default::default();
+        let system_config = SystemConfig::default_from_memory(memory_config);
+        let init_memory : SparseMemoryImage = Default::default();
+        let vm_state: VmState<F> = VmState::initial(&system_config, &init_memory, 0, vec![]);
+        let exec_ctx = ExecutionCtx::new(None); 
+        let mut vm_exec_state: VmExecState<F, GuestMemory, ExecutionCtx> = VmExecState::new(vm_state, exec_ctx);
+
+        let program = &exe.program; 
+        let pre_compute_max_size = get_pre_compute_max_size(program, &inventory);
+
+    }    
+}
+
+
+fn main() {
+    let program = Program::from_instructions(&[
+        Instruction::from_isize(
+            BaseAluOpcode::ADD.global_opcode(),
+            4, // a
+            4, // b
+            5, // c
+            0, // d
+            0  // e
+        ),
+        Instruction::from_isize(
+            BaseAluOpcode::SUB.global_opcode(),
+            4, //a 
+            4, //b
+            2, //c
+            0, //d
+            0  //e
+        )
+    ]);
+
+    let exe = VmExe {
+        program, 
+        pc_start: 0,
+        fn_bounds: Default::default(),
+        init_memory: Default::default(),
+    };
+
+    let config = ExecuteConfig::default();
+    let executor = VmExecutor::<Val<E::SC>, _>::new(config)?;
+    executor.instance(exe);    
+    
+    // let inventory : ExecutorInventory<Executor> = ExecutorInventory::new(system_config);
+
+    // let base_alu =
+    //     Rv32BaseAluExecutor::new(Rv32BaseAluAdapterExecutor, BaseAluOpcode::CLASS_OFFSET);
+
+    // inventory.add_executor(base_alu, BaseAluOpcode::iter().map(|x| x.global_opcode()));
+
+    // let aot_instance = AotInstance::new(&inventory, &exe);
+} 
