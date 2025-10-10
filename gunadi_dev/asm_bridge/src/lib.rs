fd2652c67cb1f8b6b0d8016eb83b5c9fdec37be8
Ubuntu <ubuntu@ip-172-31-18-208.ec2.internal>
2025-10-10 19:59:23 +0000

    fix: fixed memory leaks

--- gunadi_dev/asm_bridge/src/lib.rs
+++ gunadi_dev/asm_bridge/src/lib.rs
@@ -11,16 +11,6 @@ use openvm_circuit::arch::execution_mode::ExecutionCtx;
 use openvm_circuit::system::memory::online::GuestMemory;
 use openvm_circuit::arch::interpreter::PreComputeInstruction;
 
-type F = BabyBear;
-
-#[no_mangle]
-pub extern "C" fn TEST_FN(
-    base: *mut c_void,
-    vec_ptr: *const c_void,
-) {
-    println!("TEST_FN");
-}
-
 global_asm!(include_str!("asm_run.s"));
 
 extern "C" {
@@ -43,25 +33,24 @@ pub unsafe extern "C" fn asm_run(
 pub extern "C" fn extern_handler(
     base: *mut c_void,
     vec_ptr: *const c_void,
-) {
+    pc_val: u32,
+) -> u32 {
+    let mut instret: Box<u64> = Box::new(0);
+    let mut pc: Box<u32> = Box::new(pc_val);
+
     unsafe {
         type F = BabyBear;
         type Ctx = ExecutionCtx;
-        
         let typed_ptr = vec_ptr as *const Vec<PreComputeInstruction<'_, F, Ctx>>;
         let pre_compute_insns = &*typed_ptr;
-
-        let fi = &pre_compute_insns[1];
-        
-        let mut instret: Box<u64> = Box::new(1);
-        let mut pc: Box<u32> = Box::new(1);
+        let fi = &pre_compute_insns[(pc_val / 4) as usize];
         let arg : u64 = 10; // arg variable, change later
 
         let vm_exec_state_ref = unsafe {
             &mut *(base as *mut VmExecState<F, GuestMemory, ExecutionCtx>)
         };
         (fi.handler)(fi.pre_compute, &mut instret, &mut pc, arg, vm_exec_state_ref);
-
-        println!("handler called with args {:?}", fi.pre_compute);
     };
+
+    return *pc;
 }
