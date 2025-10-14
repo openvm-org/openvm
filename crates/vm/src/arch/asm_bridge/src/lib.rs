5c4c9a2ffd44c490e9dbd5b84fdbe9d680ae801e
Ubuntu <ubuntu@ip-172-31-18-208.ec2.internal>
2025-10-14 22:15:14 +0000

    feat: handle exit code

--- crates/vm/src/arch/asm_bridge/src/lib.rs
+++ crates/vm/src/arch/asm_bridge/src/lib.rs
@@ -31,29 +31,54 @@ pub unsafe extern "C" fn asm_run(
 }
 
 #[no_mangle]
+pub extern "C" fn set_instret_and_pc(
+    base: *mut c_void,
+    instret: u64,
+    pc_val: u32,
+) {
+    type F = BabyBear;
+    type Ctx = ExecutionCtx;
+    let vm_exec_state_ref = unsafe {
+        &mut *(base as *mut VmExecState<F, GuestMemory, Ctx>)
+    };
+    vm_exec_state_ref.vm_state.set_instret_and_pc(instret, pc_val);
+}
+
+#[no_mangle]
 pub extern "C" fn extern_handler(
     base: *mut c_void,
     vec_ptr: *const c_void,
     pc_val: u32,
 ) -> u32 {
-    let mut instret: Box<u64> = Box::new(0);
+    type F = BabyBear;
+    type Ctx = ExecutionCtx;
+
+    let mut instret: Box<u64> = Box::new(0); // placeholder to call the handler function
     let mut pc: Box<u32> = Box::new(pc_val);
 
+    let vm_exec_state_ref = unsafe {
+        &mut *(base as *mut VmExecState<F, GuestMemory, Ctx>)
+    };
+
     unsafe {
-        type F = BabyBear;
-        type Ctx = ExecutionCtx;
         let typed_ptr = vec_ptr as *const Vec<PreComputeInstruction<'_, F, Ctx>>;
         let pre_compute_insns = &*typed_ptr;
         let fi = &pre_compute_insns[(pc_val / 4) as usize];
         let arg : u64 = 0; // todo: change this later
-
-        let vm_exec_state_ref = unsafe {
-            &mut *(base as *mut VmExecState<F, GuestMemory, Ctx>)
-        };
         (fi.handler)(fi.pre_compute, &mut instret, &mut pc, arg, vm_exec_state_ref);
     };
 
-    return *pc;
+    match vm_exec_state_ref.exit_code {
+        Ok(None) => {
+            return *pc;
+            // execution continues 
+        }
+        _ => {
+            // special indicator that we must terminate
+            // this won't collide with actual pc value because pc values are always multiple of 4
+            return 1;
+        }
+    }
 }
 
 #[no_mangle]
