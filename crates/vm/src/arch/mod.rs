4ae23dc4fbf2aa509def2e61acf7309b46ea97ba
Maillew <lin.s.william0147@gmail.com>
2025-10-15 20:54:32 +0000

    fix: aot crate cfg for x86

--- crates/vm/src/arch/mod.rs
+++ crates/vm/src/arch/mod.rs
@@ -23,6 +23,9 @@ pub mod hasher;
 pub mod interpreter;
 /// Interpreter for preflight VM execution, for trace generation purposes.
 pub mod interpreter_preflight;
+/// AOT execution
+#[cfg(target_arch = "x86_64")]
+pub mod aot;
 /// Testing framework
 #[cfg(any(test, feature = "test-utils"))]
 pub mod testing;
@@ -35,6 +38,8 @@ pub use execution_mode::{ExecutionCtxTrait, MeteredExecutionCtxTrait};
 pub use extensions::*;
 pub use integration_api::*;
 pub use interpreter::InterpretedInstance;
+#[cfg(target_arch = "x86_64")]
+pub use aot::AotInstance;
 pub use openvm_circuit_derive::create_handler;
 pub use openvm_instructions as instructions;
 pub use record_arena::*;
