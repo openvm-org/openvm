9fb18e423b460129d60a807271469936d19475ae
Zach Langley <zblangley@gmail.com>
2025-10-16 12:59:05 -0400

    feat: smarter AlignedBorrow (#80)
    `AlignedBorrow` now exposes `as_slice()`, `as_slice_mut()`, and
    `to_vec()`
    
    Also export `define_typed_bus` macro for use in other crate modules

--- crates/recursion/src/bus.rs
+++ crates/recursion/src/bus.rs
@@ -1,48 +1,44 @@
 use core::iter;
 
-use openvm_stark_backend::interaction::{BusIndex, InteractionBuilder, PermutationCheckBus};
 use stark_backend_v2::{D_EF, DIGEST_SIZE};
 use stark_recursion_circuit_derive::AlignedBorrow;
 
-pub trait BusPayload<E> {
-    fn into_bus_vec(self) -> Vec<E>;
-}
-
+#[macro_export]
 macro_rules! define_typed_bus {
     ($Bus:ident, $Msg:ident) => {
         #[derive(Copy, Clone, Debug)]
-        pub struct $Bus(PermutationCheckBus);
+        pub struct $Bus(openvm_stark_backend::interaction::PermutationCheckBus);
 
         impl $Bus {
             #[inline]
-            pub fn new(bus_index: BusIndex) -> Self {
-                Self(PermutationCheckBus::new(bus_index))
+            pub fn new(bus_index: openvm_stark_backend::interaction::BusIndex) -> Self {
+                Self(openvm_stark_backend::interaction::PermutationCheckBus::new(
+                    bus_index,
+                ))
             }
 
             #[inline]
             pub fn send<AB>(
                 &self,
                 builder: &mut AB,
-                message: $Msg<impl Into<AB::Expr>>,
+                message: $Msg<impl Into<AB::Expr> + Clone>,
                 enabled: impl Into<AB::Expr>,
             ) where
-                AB: InteractionBuilder,
-                $Msg<AB::Expr>: BusPayload<AB::Expr>,
+                AB: openvm_stark_backend::interaction::InteractionBuilder,
             {
-                self.0.send(builder, message.into_bus_vec(), enabled);
+                self.0.send(builder, message.to_vec(), enabled);
             }
 
             #[inline]
             pub fn receive<AB>(
                 &self,
                 builder: &mut AB,
-                message: $Msg<impl Into<AB::Expr>>,
+                message: $Msg<impl Into<AB::Expr> + Clone>,
                 enabled: impl Into<AB::Expr>,
             ) where
-                AB: InteractionBuilder,
-                $Msg<AB::Expr>: BusPayload<AB::Expr>,
+                AB: openvm_stark_backend::interaction::InteractionBuilder,
             {
-                self.0.receive(builder, message.into_bus_vec(), enabled);
+                self.0.receive(builder, message.to_vec(), enabled);
             }
         }
     };
@@ -55,12 +51,6 @@ pub struct RangeCheckerBusMessage<T> {
     pub max_bits: T,
 }
 
-impl<E, T: Into<E>> BusPayload<E> for RangeCheckerBusMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.value.into(), self.max_bits.into()]
-    }
-}
-
 define_typed_bus!(RangeCheckerBus, RangeCheckerBusMessage);
 
 #[repr(C)]
@@ -68,12 +58,6 @@ define_typed_bus!(RangeCheckerBus, RangeCheckerBusMessage);
 pub struct PowerCheckerBusMessage<T> {
     pub log: T,
     pub exp: T,
-}
-
-impl<E, T: Into<E>> BusPayload<E> for PowerCheckerBusMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.log.into(), self.exp.into()]
-    }
 }
 
 define_typed_bus!(PowerCheckerBus, PowerCheckerBusMessage);
@@ -84,12 +68,6 @@ pub struct GkrModuleMessage<T> {
     pub tidx: T,
     pub n_logup: T,
     pub n_max: T,
-}
-
-impl<E, T: Into<E>> BusPayload<E> for GkrModuleMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.tidx.into(), self.n_logup.into(), self.n_max.into()]
-    }
 }
 
 define_typed_bus!(GkrModuleBus, GkrModuleMessage);
@@ -104,33 +82,12 @@ pub struct BatchConstraintModuleMessage<T> {
     pub gkr_input_layer_claim: [[T; D_EF]; 2],
 }
 
-impl<E, T: Into<E>> BusPayload<E> for BatchConstraintModuleMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        [self.tidx.into(), self.n_max.into()]
-            .into_iter()
-            .chain(self.alpha_logup.map(Into::into))
-            .chain(self.beta_logup.map(Into::into))
-            .chain(
-                self.gkr_input_layer_claim
-                    .into_iter()
-                    .flat_map(|claim| claim.into_iter().map(Into::into)),
-            )
-            .collect()
-    }
-}
-
 define_typed_bus!(BatchConstraintModuleBus, BatchConstraintModuleMessage);
 
 #[repr(C)]
 #[derive(AlignedBorrow, Debug, Clone)]
 pub struct StackingModuleMessage<T> {
     pub tidx: T,
-}
-
-impl<E, T: Into<E>> BusPayload<E> for StackingModuleMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.tidx.into()]
-    }
 }
 
 define_typed_bus!(StackingModuleBus, StackingModuleMessage);
@@ -141,12 +98,6 @@ pub struct WhirModuleMessage<T> {
     pub tidx: T,
 }
 
-impl<E, T: Into<E>> BusPayload<E> for WhirModuleMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.tidx.into()]
-    }
-}
-
 define_typed_bus!(WhirModuleBus, WhirModuleMessage);
 
 #[repr(C)]
@@ -155,12 +106,6 @@ pub struct TranscriptBusMessage<T> {
     pub tidx: T,
     pub value: T,
     pub is_sample: T,
-}
-
-impl<E, T: Into<E>> BusPayload<E> for TranscriptBusMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.tidx.into(), self.value.into(), self.is_sample.into()]
-    }
 }
 
 define_typed_bus!(TranscriptBus, TranscriptBusMessage);
@@ -176,19 +121,6 @@ pub struct AirShapeBusMessage<T> {
     pub num_interactions: T,
 }
 
-impl<E, T: Into<E>> BusPayload<E> for AirShapeBusMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![
-            self.sort_idx.into(),
-            self.air_id.into(),
-            self.hypercube_dim.into(),
-            self.has_preprocessed.into(),
-            self.num_main_parts.into(),
-            self.num_interactions.into(),
-        ]
-    }
-}
-
 define_typed_bus!(AirShapeBus, AirShapeBusMessage);
 
 #[repr(C)]
@@ -197,12 +129,6 @@ pub struct AirPartShapeBusMessage<T> {
     pub idx: T,
     pub part: T,
     pub width: T,
-}
-
-impl<E, T: Into<E>> BusPayload<E> for AirPartShapeBusMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.idx.into(), self.part.into(), self.width.into()]
-    }
 }
 
 define_typed_bus!(AirPartShapeBus, AirPartShapeBusMessage);
@@ -214,12 +140,6 @@ pub struct StackingWidthBusMessage<T> {
     pub width: T,
 }
 
-impl<E, T: Into<E>> BusPayload<E> for StackingWidthBusMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.commit_idx.into(), self.width.into()]
-    }
-}
-
 define_typed_bus!(StackingWidthsBus, StackingWidthBusMessage);
 
 #[repr(C)]
@@ -227,14 +147,6 @@ define_typed_bus!(StackingWidthsBus, StackingWidthBusMessage);
 pub struct StackingCommitmentsBusMessage<T> {
     pub commit_idx: T,
     pub commitment: [T; DIGEST_SIZE],
-}
-
-impl<E, T: Into<E>> BusPayload<E> for StackingCommitmentsBusMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        iter::once(self.commit_idx.into())
-            .chain(self.commitment.map(Into::into))
-            .collect()
-    }
 }
 
 define_typed_bus!(StackingCommitmentsBus, StackingCommitmentsBusMessage);
@@ -246,14 +158,6 @@ pub struct XiRandomnessMessage<T> {
     pub challenge: [T; D_EF],
 }
 
-impl<E, T: Into<E>> BusPayload<E> for XiRandomnessMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        iter::once(self.idx.into())
-            .chain(self.challenge.map(Into::into))
-            .collect()
-    }
-}
-
 define_typed_bus!(XiRandomnessBus, XiRandomnessMessage);
 
 #[repr(C)]
@@ -261,14 +165,6 @@ define_typed_bus!(XiRandomnessBus, XiRandomnessMessage);
 pub struct ConstraintSumcheckRandomness<T> {
     pub idx: T,
     pub challenge: [T; D_EF],
-}
-
-impl<E, T: Into<E>> BusPayload<E> for ConstraintSumcheckRandomness<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        iter::once(self.idx.into())
-            .chain(self.challenge.map(Into::into))
-            .collect()
-    }
 }
 
 define_typed_bus!(
@@ -287,18 +183,6 @@ pub struct ColumnClaimsMessage<T> {
     pub rot_claim: [T; D_EF],
 }
 
-impl<E, T: Into<E>> BusPayload<E> for ColumnClaimsMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        iter::once(self.idx.into())
-            .chain(iter::once(self.sort_idx.into()))
-            .chain(iter::once(self.part_idx.into()))
-            .chain(iter::once(self.col_idx.into()))
-            .chain(self.col_claim.map(Into::into))
-            .chain(self.rot_claim.map(Into::into))
-            .collect()
-    }
-}
-
 define_typed_bus!(ColumnClaimsBus, ColumnClaimsMessage);
 
 #[repr(C)]
@@ -308,14 +192,6 @@ pub struct StackingClaimsMessage<T> {
     pub claim: [T; D_EF],
 }
 
-impl<E, T: Into<E>> BusPayload<E> for StackingClaimsMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        iter::once(self.idx.into())
-            .chain(self.claim.map(Into::into))
-            .collect()
-    }
-}
-
 define_typed_bus!(StackingClaimsBus, StackingClaimsMessage);
 
 #[repr(C)]
@@ -323,14 +199,6 @@ define_typed_bus!(StackingClaimsBus, StackingClaimsMessage);
 pub struct StackingSumcheckRandomnessMessage<T> {
     pub idx: T,
     pub challenge: [T; D_EF],
-}
-
-impl<E, T: Into<E>> BusPayload<E> for StackingSumcheckRandomnessMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        iter::once(self.idx.into())
-            .chain(self.challenge.map(Into::into))
-            .collect()
-    }
 }
 
 define_typed_bus!(
@@ -344,12 +212,6 @@ pub struct PublicValuesBusMessage<T> {
     pub air_idx: T,
     pub pv_idx: T,
     pub value: T,
-}
-
-impl<E, T: Into<E>> BusPayload<E> for PublicValuesBusMessage<T> {
-    fn into_bus_vec(self) -> Vec<E> {
-        vec![self.air_idx.into(), self.pv_idx.into(), self.value.into()]
-    }
 }
 
 define_typed_bus!(PublicValuesBus, PublicValuesBusMessage);
