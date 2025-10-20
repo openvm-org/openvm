928ab9233146f6918057b2a6507dc43c2d69d115
Ayush Shukla <ayush@axiom.xyz>
2025-10-20 21:55:43 +0300

    feat: add util methods for extension field ops (#94)

--- crates/recursion/src/utils.rs
+++ crates/recursion/src/utils.rs
@@ -1,25 +1,58 @@
 use p3_air::AirBuilder;
-use p3_field::FieldAlgebra;
+use p3_field::{FieldAlgebra, extension::BinomiallyExtendable};
+use stark_backend_v2::D_EF;
 
 use crate::bus::TranscriptBusMessage;
 
-pub fn transcript_messages<F, const N: usize>(
-    tidx: impl Into<F>,
-    value: [impl Into<F>; N],
-    is_sample: impl Into<F>,
-) -> [TranscriptBusMessage<F>; N]
+pub fn ext_field_one_minus<FA>(x: [impl Into<FA>; D_EF]) -> [FA; D_EF]
 where
-    F: FieldAlgebra,
+    FA: FieldAlgebra,
 {
-    let tidx = tidx.into();
-    let value = value.map(Into::into);
-    let is_sample = is_sample.into();
+    let [x0, x1, x2, x3] = x.map(Into::into);
+    [FA::ONE - x0, -x1, -x2, -x3]
+}
 
-    core::array::from_fn(|j| TranscriptBusMessage {
-        tidx: tidx.clone() + F::from_canonical_usize(j),
-        value: value[j].clone(),
-        is_sample: is_sample.clone(),
-    })
+pub fn ext_field_add<FA>(x: [impl Into<FA>; D_EF], y: [impl Into<FA>; D_EF]) -> [FA; D_EF]
+where
+    FA: FieldAlgebra,
+{
+    let [x0, x1, x2, x3] = x.map(Into::into);
+    let [y0, y1, y2, y3] = y.map(Into::into);
+    [x0 + y0, x1 + y1, x2 + y2, x3 + y3]
+}
+
+pub fn ext_field_subtract<FA>(x: [impl Into<FA>; D_EF], y: [impl Into<FA>; D_EF]) -> [FA; D_EF]
+where
+    FA: FieldAlgebra,
+{
+    let [x0, x1, x2, x3] = x.map(Into::into);
+    let [y0, y1, y2, y3] = y.map(Into::into);
+    [x0 - y0, x1 - y1, x2 - y2, x3 - y3]
+}
+
+pub fn ext_field_multiply<FA>(x: [impl Into<FA>; D_EF], y: [impl Into<FA>; D_EF]) -> [FA; D_EF]
+where
+    FA: FieldAlgebra,
+    FA::F: BinomiallyExtendable<D_EF>,
+{
+    let [x0, x1, x2, x3] = x.map(Into::into);
+    let [y0, y1, y2, y3] = y.map(Into::into);
+
+    let w = FA::from_f(FA::F::W);
+
+    let z0_beta_terms = x1.clone() * y3.clone() + x2.clone() * y2.clone() + x3.clone() * y1.clone();
+    let z1_beta_terms = x2.clone() * y3.clone() + x3.clone() * y2.clone();
+    let z2_beta_terms = x3.clone() * y3.clone();
+
+    [
+        x0.clone() * y0.clone() + z0_beta_terms * w.clone(),
+        x0.clone() * y1.clone() + x1.clone() * y0.clone() + z1_beta_terms * w.clone(),
+        x0.clone() * y2.clone()
+            + x1.clone() * y1.clone()
+            + x2.clone() * y0.clone()
+            + z2_beta_terms * w,
+        x0 * y3 + x1 * y2 + x2 * y1 + x3 * y0,
+    ]
 }
 
 // TODO(ayush): move to a custom air builder
@@ -42,4 +75,23 @@ pub fn assert_eq_array<AB, const N: usize>(
     for (a, e) in actual.into_iter().zip(expected.into_iter()) {
         builder.assert_eq(a, e);
     }
+}
+
+pub fn transcript_messages<F, const N: usize>(
+    tidx: impl Into<F>,
+    value: [impl Into<F>; N],
+    is_sample: impl Into<F>,
+) -> [TranscriptBusMessage<F>; N]
+where
+    F: FieldAlgebra,
+{
+    let tidx = tidx.into();
+    let value = value.map(Into::into);
+    let is_sample = is_sample.into();
+
+    core::array::from_fn(|j| TranscriptBusMessage {
+        tidx: tidx.clone() + F::from_canonical_usize(j),
+        value: value[j].clone(),
+        is_sample: is_sample.clone(),
+    })
 }
