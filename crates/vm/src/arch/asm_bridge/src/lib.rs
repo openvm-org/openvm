604dc03a579621b9f60a2e7bc703b1c3ac94ffba
Ubuntu <ubuntu@ip-172-31-18-208.ec2.internal>
2025-10-21 18:39:58 +0000

    feat: add lib.rs

--- crates/vm/src/arch/asm_bridge/src/lib.rs
+++ crates/vm/src/arch/asm_bridge/src/lib.rs
@@ -31,6 +31,7 @@ pub unsafe extern "C" fn asm_run(
     from_state_pc: u32,
     from_state_instret: u64,
 ) {
+    println!("asm_run is called");
     asm_run_internal(
         vm_exec_state_ptr,
         pre_compute_insns_ptr,
@@ -71,6 +72,7 @@ pub extern "C" fn extern_handler(
     cur_pc: u32,
     cur_instret: u64,
 ) -> u32 {
+    println!("extern_handler vm_exec_state_ptr {:p}", vm_exec_state_ptr);
     // this is boxed for safety so that when `execute_e12_impl` runs when called by the handler
     // it would be able to dereference instret and pc correctly
     let mut instret: Box<u64> = Box::new(cur_instret);
@@ -102,12 +104,16 @@ pub extern "C" fn extern_handler(
         );
     };
 
+    
+
     match vm_exec_state_ref.exit_code {
         Ok(None) => {
+            println!("extern_handler returns {}", *pc);
             // execution continues
             *pc
         }
         _ => {
+            println!("extern_handler returns {}", 1);
             // special indicator that we must terminate
             // this won't collide with actual pc value because pc values are always multiple of 4
             1
@@ -117,6 +123,7 @@ pub extern "C" fn extern_handler(
 
 #[no_mangle]
 pub extern "C" fn should_suspend(instret: u64, _pc: u32, exec_state_ptr: *mut c_void) -> u32 {
+    println!("should_suspend exec_state_ptr {:p}", exec_state_ptr);
     // reference to vm_exec_state
     let vm_exec_state_ref =
         unsafe { &mut *(exec_state_ptr as *mut VmExecState<F, GuestMemory, Ctx>) };
@@ -124,8 +131,10 @@ pub extern "C" fn should_suspend(instret: u64, _pc: u32, exec_state_ptr: *mut c_
     let instret_end = vm_exec_state_ref.ctx.instret_end;
 
     if instret >= instret_end {
+        println!("should suspend returns true");
         1 // should suspend is `true`
     } else {
+        println!("should suspend returns false");
         0 // should suspend is `false`
     }
 }
