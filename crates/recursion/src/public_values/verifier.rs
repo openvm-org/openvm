3e43cfb861f41b7539153607dcba1b64e39c5f12
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2025-10-27 14:18:11 -0400

    feat: tracegen and AIRs for non-root continuation layers (#120)

---
+++ crates/recursion/src/public_values/verifier.rs
@@ -0,0 +1,346 @@
+use std::{
+    borrow::{Borrow, BorrowMut},
+    sync::Arc,
+};
+
+use openvm_circuit::system::{connector::VmConnectorPvs, memory::merkle::MemoryMerklePvs};
+use openvm_circuit_primitives::utils::{and, assert_array_eq, not};
+use openvm_stark_backend::{
+    interaction::InteractionBuilder,
+    prover::types::AirProofRawInput,
+    rap::{BaseAirWithPublicValues, PartitionedBaseAir},
+};
+use p3_air::{Air, AirBuilder, AirBuilderWithPublicValues, BaseAir};
+use p3_field::FieldAlgebra;
+use p3_matrix::{Matrix, dense::RowMajorMatrix};
+use stark_backend_v2::{DIGEST_SIZE, F, proof::Proof};
+use stark_recursion_circuit_derive::AlignedBorrow;
+
+use crate::{
+    bus::PublicValuesBus,
+    public_values::{NonRootVerifierPvs, app::*},
+    utils::assert_zeros,
+};
+
+pub const VERIFIER_PVS_AIR_ID: usize = 0;
+pub const CONSTRAINT_EVAL_AIR_ID: usize = 6;
+pub const CONSTRAINT_EVAL_CACHED_INDEX: usize = 0;
+
+#[repr(C)]
+#[derive(AlignedBorrow)]
+pub struct VerifierPvsCols<F> {
+    pub proof_idx: F,
+    pub is_valid: F,
+    pub is_last: F,
+    pub has_verifier_pvs: F,
+    pub child_pvs: NonRootVerifierPvs<F>,
+}
+
+pub enum VerifierChildLevel {
+    App,
+    Leaf,
+    InternalForLeaf,
+    InternalRecursive,
+}
+
+pub fn generate_proving_input(
+    proofs: &[Proof],
+    user_pv_commit: Option<[F; DIGEST_SIZE]>,
+) -> AirProofRawInput<F> {
+    let num_proofs = proofs.len();
+    let height = num_proofs.next_power_of_two();
+    let width = VerifierPvsCols::<u8>::width();
+
+    debug_assert!(num_proofs > 0);
+
+    let mut trace = vec![F::ZERO; height * width];
+    let mut child_level = VerifierChildLevel::App;
+
+    for (proof_idx, (proof, chunk)) in proofs.iter().zip(trace.chunks_exact_mut(width)).enumerate()
+    {
+        let cols: &mut VerifierPvsCols<F> = chunk.borrow_mut();
+
+        cols.proof_idx = F::from_canonical_usize(proof_idx);
+        cols.is_valid = F::ONE;
+        cols.is_last = F::from_bool(proof_idx + 1 == num_proofs);
+
+        if let Some(user_pv_commit) = user_pv_commit {
+            cols.has_verifier_pvs = F::ZERO;
+            cols.child_pvs.user_pv_commit = user_pv_commit;
+            cols.child_pvs.app_commit = proof.trace_vdata[PROGRAM_AIR_ID]
+                .as_ref()
+                .unwrap()
+                .cached_commitments[PROGRAM_CACHED_TRACE_INDEX];
+
+            let &VmConnectorPvs {
+                initial_pc,
+                final_pc,
+                exit_code,
+                is_terminate,
+            } = proof.public_values[CONNECTOR_AIR_ID].as_slice().borrow();
+            cols.child_pvs.is_terminate = is_terminate;
+            cols.child_pvs.initial_pc = initial_pc;
+            cols.child_pvs.final_pc = final_pc;
+            cols.child_pvs.exit_code = exit_code;
+
+            let &MemoryMerklePvs::<_, DIGEST_SIZE> {
+                initial_root,
+                final_root,
+            } = proof.public_values[MERKLE_AIR_ID].as_slice().borrow();
+            cols.child_pvs.initial_root = initial_root;
+            cols.child_pvs.final_root = final_root;
+        } else {
+            let child_pvs: &NonRootVerifierPvs<F> =
+                proof.public_values[VERIFIER_PVS_AIR_ID].as_slice().borrow();
+            cols.has_verifier_pvs = F::ONE;
+            cols.child_pvs = *child_pvs;
+
+            child_level = match child_pvs.internal_flag {
+                F::ZERO => VerifierChildLevel::Leaf,
+                F::ONE => VerifierChildLevel::InternalForLeaf,
+                F::TWO => VerifierChildLevel::InternalRecursive,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    let first_row: &VerifierPvsCols<F> = trace[..width].borrow();
+    let mut pvs = first_row.child_pvs;
+
+    match child_level {
+        VerifierChildLevel::App => {}
+        VerifierChildLevel::Leaf => {
+            pvs.leaf_commit = proofs[0].trace_vdata[CONSTRAINT_EVAL_AIR_ID]
+                .as_ref()
+                .unwrap()
+                .cached_commitments[CONSTRAINT_EVAL_CACHED_INDEX]
+        }
+        VerifierChildLevel::InternalForLeaf => {
+            pvs.internal_for_leaf_commit = proofs[0].trace_vdata[CONSTRAINT_EVAL_AIR_ID]
+                .as_ref()
+                .unwrap()
+                .cached_commitments[CONSTRAINT_EVAL_CACHED_INDEX]
+        }
+        VerifierChildLevel::InternalRecursive => {
+            pvs.internal_recursive_commit = proofs[0].trace_vdata[CONSTRAINT_EVAL_AIR_ID]
+                .as_ref()
+                .unwrap()
+                .cached_commitments[CONSTRAINT_EVAL_CACHED_INDEX]
+        }
+    }
+
+    AirProofRawInput {
+        cached_mains: vec![],
+        common_main: Some(Arc::new(RowMajorMatrix::new(trace, width))),
+        public_values: pvs.to_vec(),
+    }
+}
+
+pub struct VerifierPvsAir {
+    pub public_values_bus: PublicValuesBus,
+    // TODO[INT-5302]: add bus to receive constraint eval cached commit
+}
+
+impl<F> BaseAir<F> for VerifierPvsAir {
+    fn width(&self) -> usize {
+        VerifierPvsCols::<u8>::width()
+    }
+}
+impl<F> BaseAirWithPublicValues<F> for VerifierPvsAir {
+    fn num_public_values(&self) -> usize {
+        NonRootVerifierPvs::<u8>::width()
+    }
+}
+impl<F> PartitionedBaseAir<F> for VerifierPvsAir {}
+
+impl<AB: AirBuilder + InteractionBuilder + AirBuilderWithPublicValues> Air<AB> for VerifierPvsAir {
+    fn eval(&self, builder: &mut AB) {
+        let main = builder.main();
+        let (local, next) = (main.row_slice(0), main.row_slice(1));
+        let local: &VerifierPvsCols<AB::Var> = (*local).borrow();
+        let next: &VerifierPvsCols<AB::Var> = (*next).borrow();
+
+        /*
+         * We first constrain segment adjacency, i.e. that rows in the trace are such that the
+         * first row is the (chronologically) first segment, and adjacent rows correspond to
+         * adjacent segments.
+         */
+        // constrain increasing proof_idx
+        builder.assert_bool(local.is_valid);
+        builder.when_first_row().assert_zero(local.proof_idx);
+        builder
+            .when(and(local.is_valid, next.is_valid))
+            .assert_eq(local.proof_idx + AB::F::ONE, next.proof_idx);
+
+        // constrain is_last, note proof_idx on the first row is 0
+        builder.assert_bool(local.is_last);
+        builder.when(local.is_last).assert_one(local.is_valid);
+        builder
+            .when(local.is_last)
+            .assert_zero(next.is_valid * next.proof_idx);
+
+        // constrain that is_terminate is the last valid proof
+        builder.assert_bool(local.child_pvs.is_terminate);
+        builder
+            .when(local.child_pvs.is_terminate)
+            .assert_one(local.is_last);
+
+        // constrain that non-terminal segments exited successfully
+        builder
+            .when(not(local.child_pvs.is_terminate))
+            .assert_zero(local.child_pvs.exit_code);
+
+        // when local and next are valid, constrain increasing proof_idx and adjacency
+        let mut when_both = builder.when(and(local.is_valid, not(local.is_last)));
+        when_both.assert_eq(local.child_pvs.final_pc, next.child_pvs.initial_pc);
+        assert_array_eq(
+            &mut when_both,
+            local.child_pvs.final_root,
+            next.child_pvs.initial_root,
+        );
+
+        /*
+         * Next we constrain the consistency of verifier-specific public values. Note we can
+         * determine what layer a verifier is at using the has_verifier_pvs and internal_flag
+         * columns. If has_verifier_pvs is 0, then we have a leaf verifier that is verifying
+         * an app segment proof. If has_verifier_pvs is 1 and internal_flag is 0, then we have
+         * an internal verifier that has leaf children, and if internal_flag is 1 then we have
+         * internal_for_leaf children. Finally, if internal_flag is 2 then we are verifying
+         * internal_recursion children.
+         */
+        // constrain verifier pvs columns are the same across all rows
+        when_both.assert_eq(local.child_pvs.internal_flag, next.child_pvs.internal_flag);
+        assert_array_eq(
+            &mut when_both,
+            local.child_pvs.leaf_commit,
+            next.child_pvs.leaf_commit,
+        );
+        assert_array_eq(
+            &mut when_both,
+            local.child_pvs.internal_for_leaf_commit,
+            next.child_pvs.internal_for_leaf_commit,
+        );
+        assert_array_eq(
+            &mut when_both,
+            local.child_pvs.internal_recursive_commit,
+            next.child_pvs.internal_recursive_commit,
+        );
+
+        // constraints for basic validity
+        builder.assert_bool(local.has_verifier_pvs);
+        builder.assert_tern(local.child_pvs.internal_flag);
+        builder
+            .when(local.has_verifier_pvs)
+            .assert_one(local.is_valid);
+
+        // constrain that child commits are 0 when they shouldn't be defined
+        builder
+            .when(not(local.has_verifier_pvs))
+            .assert_zero(local.child_pvs.internal_flag);
+
+        assert_zeros(
+            &mut builder.when(not(local.has_verifier_pvs)),
+            local.child_pvs.leaf_commit,
+        );
+        assert_zeros(
+            &mut builder.when(
+                (local.child_pvs.internal_flag - AB::F::ONE)
+                    * (local.child_pvs.internal_flag - AB::F::TWO),
+            ),
+            local.child_pvs.internal_for_leaf_commit,
+        );
+        assert_zeros(
+            &mut builder
+                .when(local.child_pvs.internal_flag * (local.child_pvs.internal_flag - AB::F::ONE)),
+            local.child_pvs.internal_recursive_commit,
+        );
+
+        // TODO[INT-5302]: receive verifier public values from proof shape
+        // TODO[INT-5302]: receive constraint eval cached commit from proof shape
+
+        /*
+         * Finally, we need to constrain that the public values this AIR produces are consistent
+         * with the child's. Note that we only impose constraints for layers below the current
+         * one - it is impossible for the current layer to know its own commit, and future layers
+         * will catch if we pre-emptively define a current or future verifier commit.
+         */
+        let &NonRootVerifierPvs::<_> {
+            initial_pc,
+            user_pv_commit,
+            app_commit,
+            is_terminate,
+            final_pc,
+            exit_code,
+            initial_root,
+            final_root,
+            internal_flag,
+            leaf_commit,
+            internal_for_leaf_commit,
+            internal_recursive_commit,
+        } = builder.public_values().borrow();
+
+        // constrain first proof pvs
+        builder.when_first_row().assert_one(local.is_valid);
+        builder
+            .when_first_row()
+            .assert_eq(local.child_pvs.initial_pc, initial_pc);
+        assert_array_eq(
+            &mut builder.when_first_row(),
+            local.child_pvs.initial_root,
+            initial_root,
+        );
+
+        // constrain last proof pvs
+        builder
+            .when(local.is_last)
+            .assert_eq(local.child_pvs.is_terminate, is_terminate);
+        builder
+            .when(local.is_last)
+            .assert_eq(local.child_pvs.final_pc, final_pc);
+        assert_array_eq(
+            &mut builder.when(local.is_last),
+            local.child_pvs.final_root,
+            final_root,
+        );
+
+        // constrain other app pvs
+        builder
+            .when(local.is_last)
+            .assert_eq(local.child_pvs.exit_code, exit_code);
+        assert_array_eq(builder, local.child_pvs.user_pv_commit, user_pv_commit);
+        assert_array_eq(builder, local.child_pvs.app_commit, app_commit);
+
+        // constrain internal_flag is 0 at the leaf level
+        builder
+            .when(not(local.has_verifier_pvs))
+            .assert_zero(internal_flag);
+
+        // constrain internal_flag and leaf_commit are set at all internal levels
+        builder
+            .when(local.has_verifier_pvs)
+            .assert_bool(internal_flag.into() - AB::F::ONE);
+        assert_array_eq(
+            &mut builder.when(local.has_verifier_pvs),
+            local.child_pvs.leaf_commit,
+            leaf_commit,
+        );
+
+        // constrain verifier-specific pvs at all internal_recursive levels
+        builder
+            .when(local.child_pvs.internal_flag)
+            .assert_zero(internal_flag.into() - AB::F::TWO);
+        assert_array_eq(
+            &mut builder.when(local.child_pvs.internal_flag),
+            local.child_pvs.internal_for_leaf_commit,
+            internal_for_leaf_commit,
+        );
+
+        // constrain verifier-specific pvs at internal_recursive levels after the first
+        assert_array_eq(
+            &mut builder
+                .when(local.child_pvs.internal_flag * (local.child_pvs.internal_flag - AB::F::ONE)),
+            local.child_pvs.internal_recursive_commit,
+            internal_recursive_commit,
+        );
+    }
+}
