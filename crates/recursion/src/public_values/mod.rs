2ff9be09660cf66cb7c023b37cb5837cd4fd0f15
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2025-10-27 14:18:11 -0400

    feat: tracegen and AIRs for non-root continuation layers (#120)

---
+++ crates/recursion/src/public_values/mod.rs
@@ -0,0 +1,45 @@
+use stark_backend_v2::DIGEST_SIZE;
+use stark_recursion_circuit_derive::AlignedBorrow;
+
+pub mod app {
+    pub use openvm_circuit::arch::{
+        CONNECTOR_AIR_ID, MERKLE_AIR_ID, PROGRAM_AIR_ID, PROGRAM_CACHED_TRACE_INDEX,
+        PUBLIC_VALUES_AIR_ID,
+    };
+}
+
+pub mod receiver;
+pub mod verifier;
+
+#[repr(C)]
+#[derive(AlignedBorrow, Clone, Copy)]
+pub struct NonRootVerifierPvs<F> {
+    // app commit pvs
+    pub user_pv_commit: [F; DIGEST_SIZE],
+    pub app_commit: [F; DIGEST_SIZE],
+
+    // connector pvs
+    pub is_terminate: F,
+    pub initial_pc: F,
+    pub final_pc: F,
+    pub exit_code: F,
+
+    // memory merkle pvs
+    pub initial_root: [F; DIGEST_SIZE],
+    pub final_root: [F; DIGEST_SIZE],
+
+    // verifier-specific pvs
+    pub internal_flag: F,
+    pub leaf_commit: [F; DIGEST_SIZE],
+    pub internal_for_leaf_commit: [F; DIGEST_SIZE],
+    pub internal_recursive_commit: [F; DIGEST_SIZE],
+}
+
+#[repr(C)]
+#[derive(AlignedBorrow)]
+pub struct RootVerifierPvs<F> {
+    pub app_commit: [F; DIGEST_SIZE],
+    pub leaf_commit: [F; DIGEST_SIZE],
+    pub internal_for_leaf_commit: [F; DIGEST_SIZE],
+    pub internal_recursive_commit: [F; DIGEST_SIZE],
+}
