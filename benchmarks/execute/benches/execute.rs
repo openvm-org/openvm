e75610f6ae4a6ef4df7f1b048fe1b1ebfb4283ce
Gunadi Gani <87816385+GunaDD@users.noreply.github.com>
2025-10-28 21:47:27 -0400

    chore: codspeed benchmarks for pr to feat/aot (#2183)
    * Change workflow to enable codpseed bench with aot features
    * Run the extension tests with aot feature on
    * Aot skips `test_load_x0` since it is an untrusted program
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-18-208.ec2.internal>

--- benchmarks/execute/benches/execute.rs
+++ benchmarks/execute/benches/execute.rs
@@ -250,6 +250,7 @@ fn benchmark_execute(bencher: Bencher, program: &str) {
                 .expect("Failed to execute program in interpreted mode");
         });
 }
+        
 
 /*
 #[divan::bench(args = APP_PROGRAMS, sample_count=0)]
