5eff634132eb76aaa6866e9fe6035df52d307977
Maillew <lin.s.william0147@gmail.com>
2025-10-31 22:53:36 +0000

    chore: rebase cleanup

--- crates/vm/src/utils/stark_utils.rs
+++ crates/vm/src/utils/stark_utils.rs
@@ -108,7 +108,7 @@ where
     Assertions for Pure Execution AOT
     */
     let interp_state_pure = vm
-        .get_interp_instance(&exe)?
+        .naive_interpreter(&exe)?
         .execute(input.clone(), None)
         .expect("Failed to execute");
 
