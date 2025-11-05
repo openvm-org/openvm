032d88cc95b9b5a3b2eb3d2267c9cbe655a2274a
Ubuntu <gunadigan@gmail.com>
2025-11-05 21:16:58 +0000

    perf: remove pc increment for shift

--- extensions/rv32im/circuit/src/shift/execution.rs
+++ extensions/rv32im/circuit/src/shift/execution.rs
@@ -192,7 +192,6 @@ where
         }
         // General Register -> XMM
         asm_str += &gpr_to_rv32_register(REG_B_W, (a / 4) as u8);
-        asm_str += &format!("   add {}, {}\n", REG_PC, DEFAULT_PC_OFFSET);
         asm_str += &format!("   add {}, {}\n", REG_INSTRET, 1);
         // let it fall to the next instruction
         Ok(asm_str)
