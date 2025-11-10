f151ce7246a331089e085d7ecc02d95d3e065a0b
GunaDD <gunadigan@gmail.com>
2025-11-10 21:39:35 +0000

    fix: fix typo by changing push to pop

--- crates/vm/src/arch/aot/mod.rs
+++ crates/vm/src/arch/aot/mod.rs
@@ -154,15 +154,15 @@ where
     fn pop_internal_registers() -> String {
         let mut asm_str = String::new();
 
-        asm_str += "    push rax\n";
-        asm_str += "    push r11\n";
-        asm_str += "    push r10\n";
-        asm_str += "    push r9\n";
-        asm_str += "    push r8\n";
-        asm_str += "    push rdi\n";
-        asm_str += "    push rsi\n";
-        asm_str += "    push rdx\n";
-        asm_str += "    push rcx\n";
+        asm_str += "    pop rax\n";
+        asm_str += "    pop r11\n";
+        asm_str += "    pop r10\n";
+        asm_str += "    pop r9\n";
+        asm_str += "    pop r8\n";
+        asm_str += "    pop rdi\n";
+        asm_str += "    pop rsi\n";
+        asm_str += "    pop rdx\n";
+        asm_str += "    pop rcx\n";
 
         asm_str
     }
