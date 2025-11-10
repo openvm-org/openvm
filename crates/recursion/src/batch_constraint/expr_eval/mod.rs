4e65522ce125f2ca9f6b226a6e6e915120a90898
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-11-10 08:40:28 -0800

    chore: split `expr_eval` into several files and rename `EqMle*` to `Eq3b*` (#186)
    Cherry-picked from
    https://github.com/axiom-crypto/v2-proof-system/pull/184
    
    - renames `eq_mle_*` into `eq_3b_*` (I hope)
    - splits `expr_eval` into several files
    - some other changes that were hard to separate from above big PR
    
    ---------
    
    Co-authored-by: Alexander Golovanov <Golovanov12345@gmail.com>

---
+++ crates/recursion/src/batch_constraint/expr_eval/mod.rs
@@ -0,0 +1,9 @@
+pub mod column_claim;
+pub mod constraints_folding;
+pub mod interactions_folding;
+pub mod symbolic_expression;
+
+pub use column_claim::*;
+pub use constraints_folding::*;
+pub use interactions_folding::*;
+pub use symbolic_expression::*;
