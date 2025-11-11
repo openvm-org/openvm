e79b25017bbe64f6545e4798e7080dc9cb85909d
GunaDD <gunadigan@gmail.com>
2025-11-11 03:25:05 +0000

    fix: fixed logical error in xmm to gpr helper function

--- crates/vm/src/arch/execution.rs
+++ crates/vm/src/arch/execution.rs
@@ -11,6 +11,11 @@ use rand::rngs::StdRng;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
+use crate::arch::aot::common::{
+        REG_EXEC_STATE_PTR, REG_FIRST_ARG, REG_FOURTH_ARG, REG_INSNS_PTR, REG_INSTRET_END,
+        REG_PC, REG_SECOND_ARG, REG_THIRD_ARG, REG_D, REG_RETURN_VAL
+};
+
 use super::{execution_mode::ExecutionCtxTrait, Streams, VmExecState};
 #[cfg(feature = "aot")]
 use crate::arch::aot::common::{
