3530d86ec4713908c108f1f68b5c9f384b16fbd8
GunaDD <gunadigan@gmail.com>
2025-11-12 22:13:16 +0000

    feat: enable branch_lt asm

--- extensions/rv32im/circuit/src/branch_lt/aot.rs
+++ extensions/rv32im/circuit/src/branch_lt/aot.rs
@@ -3,6 +3,8 @@ use openvm_circuit::arch::{AotError, AotExecutor};
 use openvm_instructions::instruction::Instruction;
 use openvm_stark_backend::p3_field::PrimeField32;
 
+#[cfg(feature = "aot")]
+use crate::common::{xmm_to_gpr, REG_A_W, REG_B_W};
 use crate::BranchLessThanExecutor;
 
 impl<F, A, const NUM_LIMBS: usize, const LIMB_BITS: usize> AotExecutor<F>
@@ -40,9 +42,12 @@ where
         let b_reg = b / 4;
 
         // Calculate the result. Inputs: eax, ecx. Outputs: edx.
-        asm_str += &rv32_register_to_gpr(a_reg, "eax");
-        asm_str += &rv32_register_to_gpr(b_reg, "ecx");
-        asm_str += "   cmp eax, ecx\n";
+        let (reg_a, delta_str_a) = &xmm_to_gpr(a_reg, REG_A_W, false);
+        asm_str += delta_str_a;
+        let (reg_b, delta_str_b) = &xmm_to_gpr(b_reg, REG_B_W, false);
+        asm_str += delta_str_b;
+
+        asm_str += &format!("   cmp {reg_a}, {reg_b}\n");
         let not_jump_label = format!(".asm_execute_pc_{pc}_not_jump");
         match local_opcode {
             BranchLessThanOpcode::BGE => {
@@ -64,14 +69,12 @@ where
         }
         // Jump branch
         asm_str += &format!("   jmp asm_execute_pc_{next_pc}\n");
-
         asm_str += &format!("{not_jump_label}:\n");
 
         Ok(asm_str)
     }
 
     fn is_aot_supported(&self, _inst: &Instruction<F>) -> bool {
-        // true
-        false
+        true
     }
 }
