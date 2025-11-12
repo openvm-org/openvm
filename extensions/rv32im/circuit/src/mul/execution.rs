e4f8ef43b130e06bf7226203625518540be5eb85
GunaDD <gunadigan@gmail.com>
2025-11-12 20:49:25 +0000

    fix: turn off mul and mulh

--- extensions/rv32im/circuit/src/mul/execution.rs
+++ extensions/rv32im/circuit/src/mul/execution.rs
@@ -96,7 +96,7 @@ where
     F: PrimeField32,
 {
     fn is_aot_supported(&self, inst: &Instruction<F>) -> bool {
-        inst.opcode == MulOpcode::MUL.global_opcode()
+        false
     }
 
     fn generate_x86_asm(&self, inst: &Instruction<F>, _pc: u32) -> Result<String, AotError> {
