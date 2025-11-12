94695bfa7527099a7e46a791e0fce6ca7dc7cd09
GunaDD <gunadigan@gmail.com>
2025-11-12 19:52:48 +0000

    fix: sync initial vm memory to xmm regs

--- crates/vm/src/arch/aot/pure.rs
+++ crates/vm/src/arch/aot/pure.rs
@@ -88,7 +88,7 @@ where
 
         asm_str += &Self::pop_internal_registers();
 
-        asm_str += &Self::initialize_xmm_regs();
+        asm_str += &Self::rv32_regs_to_xmm();
 
         asm_str += &format!("   lea {REG_D}, [rip + map_pc_base]\n");
         asm_str += &format!("   movsxd {REG_PC}, [{REG_D} + {REG_PC}]\n");
@@ -200,11 +200,11 @@ where
         // asm_run_end part
         for (pc, _instruction, _) in exe.program.enumerate_by_pc() {
             asm_str += &format!("asm_run_end_{pc}:\n");
-            asm_str += "    mov rdi, rbx\n";
-            asm_str += &format!("    mov rsi, {pc}\n");
-            asm_str += &format!("    mov rax, {set_pc_ptr}\n");
-            asm_str += "    call rax\n";
-            asm_str += "    xor rax, rax\n";
+            asm_str += &Self::xmm_to_rv32_regs();
+            asm_str += &format!("   mov {REG_FIRST_ARG}, rbx\n");
+            asm_str += &format!("   mov {REG_SECOND_ARG}, {pc}\n");
+            asm_str += &format!("   mov {REG_D}, {set_pc_ptr}\n");
+            asm_str += &format!("   call {REG_D}\n");
             asm_str += &Self::pop_external_registers();
             asm_str += &format!("    xor {REG_RETURN_VAL}, {REG_RETURN_VAL}\n");
             asm_str += "    ret\n";
