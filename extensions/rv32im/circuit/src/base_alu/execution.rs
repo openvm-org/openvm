cdef0dc1fdbc776b795bc3391d24ebdc824ed132
GunaDD <gunadigan@gmail.com>
2025-11-12 21:21:54 +0000

    feat: turn on basealu and mulh custom asm

--- extensions/rv32im/circuit/src/base_alu/execution.rs
+++ extensions/rv32im/circuit/src/base_alu/execution.rs
@@ -175,7 +175,7 @@ where
     F: PrimeField32,
 {
     fn is_aot_supported(&self, _instruction: &Instruction<F>) -> bool {
-        false
+        true
     }
 
     fn generate_x86_asm(&self, inst: &Instruction<F>, _pc: u32) -> Result<String, AotError> {
