153b14237e431f2e5578a3cb2b1be28fcd2439ec
Maillew <lin.s.william0147@gmail.com>
2025-11-14 16:28:42 +0000

    branch pred

--- crates/vm/src/arch/aot/metered_execute.rs
+++ crates/vm/src/arch/aot/metered_execute.rs
@@ -171,16 +171,15 @@ where
             asm_str += &format!("asm_execute_pc_{pc}:\n");
 
             asm_str += &format!("    cmp {REG_INSTRET_END}, 0\n");
-            asm_str += &format!("    jne instret_positive_{pc}\n"); // if instret > 0, skip slow path
+            asm_str += &format!("    je instret_zero_{pc}\n"); // if instret == 0, jump to slow path
+            asm_str += &format!("    dec {REG_INSTRET_END}\n");
+            asm_str += &sync_reg_to_instret_until_end(); // TODO: remove this; too many reads from memory, every op
+            asm_str += &format!("    jmp execute_instruction_{pc}\n");
 
+            asm_str += &format!("instret_zero_{pc}:\n");
             asm_str += "    call asm_handle_segment_check\n";
             asm_str += "    test al, al\n";
             asm_str += &format!("    jnz asm_run_end_{pc}\n");
-            asm_str += &format!("    jmp execute_instruction_{pc}\n");
-
-            asm_str += &format!("instret_positive_{pc}:\n");
-            asm_str += &format!("    dec {REG_INSTRET_END}\n");
-            asm_str += &sync_reg_to_instret_until_end();
 
             // continue with execution, as should_suspend returned false
             asm_str += &format!("execute_instruction_{pc}:\n");
@@ -253,6 +252,7 @@ where
                 asm_str += "    cmp rax, 1\n"; // compare the return value with 1
                 asm_str += &Self::pop_internal_registers(); // pop the internal registers from the stack
                 asm_str += &Self::pop_address_space_start();
+                // asm_str += &sync_instret_until_end_to_reg();
                 asm_str += &Self::rv32_regs_to_xmm(); // read the memory from the memory location of the RV32 registers in `GuestMemory`
                                                       // registers, to the appropriate XMM registers
                 asm_str += &format!("   je asm_run_end_{pc}\n");
