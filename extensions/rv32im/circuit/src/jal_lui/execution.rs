884893d91642f1a6dd0834468fd79251dead8fda
Xinding Wei <weixinding@gmail.com>
2025-11-14 04:46:05 +0000

    Implement E2 AOT for jal/lui

--- extensions/rv32im/circuit/src/jal_lui/execution.rs
+++ extensions/rv32im/circuit/src/jal_lui/execution.rs
@@ -183,7 +183,31 @@ where
 }
 
 #[cfg(feature = "aot")]
-impl<F, A> AotMeteredExecutor<F> for Rv32JalLuiExecutor<A> where F: PrimeField32 {}
+impl<F, A> AotMeteredExecutor<F> for Rv32JalLuiExecutor<A>
+where
+    F: PrimeField32,
+{
+    fn is_aot_metered_supported(&self, _inst: &Instruction<F>) -> bool {
+        true
+    }
+    fn generate_x86_metered_asm(
+        &self,
+        inst: &Instruction<F>,
+        pc: u32,
+        chip_idx: usize,
+        config: &SystemConfig,
+    ) -> Result<String, AotError> {
+        use crate::common::{update_adapter_heights_asm, update_height_change_asm};
+        let enabled = !inst.f.is_zero();
+        let mut asm_str = update_height_change_asm(chip_idx, 1)?;
+        if enabled {
+            // write [a:4]_1
+            asm_str += &update_adapter_heights_asm(config, RV32_REGISTER_AS)?;
+        }
+        asm_str += &self.generate_x86_asm(inst, pc)?;
+        Ok(asm_str)
+    }
+}
 
 #[inline(always)]
 unsafe fn execute_e12_impl<
