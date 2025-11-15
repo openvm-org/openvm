7d4feb23590befbceef621be533f904d70dd6eaa
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2025-11-15 03:37:30 +0000

    fix: some CUDA compiler failures

--- extensions/ecc/circuit/src/weierstrass_chip/double/cuda.rs
+++ extensions/ecc/circuit/src/weierstrass_chip/double/cuda.rs
@@ -6,7 +6,7 @@ use openvm_circuit::arch::{AdapterCoreLayout, DenseRecordArena, RecordSeeker};
 use openvm_circuit_primitives::{
     bitwise_op_lookup::BitwiseOperationLookupChipGPU, var_range::VariableRangeCheckerChipGPU,
 };
-use openvm_cuda_backend::{chip::get_empty_air_proving_ctx, prover_backend::GpuBackend, types::F};
+use openvm_cuda_backend::{base::DeviceMatrix, prover_backend::GpuBackend, types::F};
 use openvm_cuda_common::copy::MemCopyH2D;
 use openvm_ecc_transpiler::Rv32WeierstrassOpcode;
 use openvm_instructions::riscv::RV32_CELL_BITS;
