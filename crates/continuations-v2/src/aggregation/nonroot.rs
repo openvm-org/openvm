64ac93b316949fe7ac0ce7a65fb8da84a08e95dc
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2025-11-15 17:28:36 -0500

    feat: v2 SDK (#232)

--- crates/continuations-v2/src/aggregation/nonroot.rs
+++ crates/continuations-v2/src/aggregation/nonroot.rs
@@ -1,27 +1,22 @@
-use std::sync::Arc;
+use std::{iter::once, sync::Arc};
 
 use eyre::Result;
 use itertools::Itertools;
 use openvm_circuit::system::memory::merkle::public_values::UserPublicValuesProof;
-use openvm_stark_backend::AirRef;
-use openvm_stark_sdk::config::baby_bear_poseidon2::BabyBearPoseidon2Config;
-use recursion_circuit::system::VerifierSubCircuit;
+use recursion_circuit::system::{AggregationSubCircuit, VerifierTraceGen};
 use stark_backend_v2::{
-    BabyBearPoseidon2CpuEngineV2, DIGEST_SIZE, F, StarkEngineV2, SystemParams,
+    DIGEST_SIZE, F, SC, StarkWhirEngine, SystemParams,
     keygen::types::{MultiStarkProvingKeyV2, MultiStarkVerifyingKeyV2},
-    poseidon2::sponge::{DuplexSponge, DuplexSpongeRecorder},
+    poseidon2::sponge::DuplexSpongeRecorder,
     proof::Proof,
-    prover::{CommittedTraceDataV2, CpuBackendV2, DeviceDataTransporterV2, ProvingContextV2},
+    prover::{CommittedTraceDataV2, DeviceDataTransporterV2, ProverBackendV2, ProvingContextV2},
 };
 
 use crate::{
     aggregation::{
         AggregationCircuit, AggregationProver, MAX_NUM_PROOFS, trace_heights_tracing_info,
     },
-    public_values::{
-        receiver::{self, UserPvsReceiverAir},
-        verifier::{self, VerifierPvsAir},
-    },
+    public_values::AggNodeTraceGen,
 };
 
 #[derive(Clone, Debug)]
@@ -31,48 +26,49 @@ pub struct NonRootStarkProof {
 }
 
 /*
- * Stateless struct to generate the AIRs of the aggregation circuit
- */
-#[derive(derive_new::new, Clone)]
-pub struct NonRootAggregationCircuit {
-    pub verifier_circuit: Arc<VerifierSubCircuit<MAX_NUM_PROOFS>>,
-}
-
-impl AggregationCircuit for NonRootAggregationCircuit {
-    fn airs(&self) -> Vec<AirRef<BabyBearPoseidon2Config>> {
-        airs(&self.verifier_circuit)
-    }
-}
-
-/*
  * Struct to generate an aggregation proof
  */
-pub struct NonRootAggregationProver<const NUM_CHILDREN: usize> {
+pub struct NonRootAggregationProver<
+    PB: ProverBackendV2,
+    S: AggregationSubCircuit,
+    T: AggNodeTraceGen<PB>,
+> {
     pk: Arc<MultiStarkProvingKeyV2>,
     vk: Arc<MultiStarkVerifyingKeyV2>,
 
+    agg_node_tracegen: T,
+
     // TODO: tracegen currently requires storing these, we should revisit this
     child_vk: Arc<MultiStarkVerifyingKeyV2>,
-    child_vk_pcs_data: CommittedTraceDataV2<CpuBackendV2>,
-    circuit: Arc<NonRootAggregationCircuit>,
+    child_vk_pcs_data: CommittedTraceDataV2<PB>,
+    circuit: Arc<AggregationCircuit<S>>,
 }
 
-impl<const NUM_CHILDREN: usize> AggregationProver<CpuBackendV2>
-    for NonRootAggregationProver<NUM_CHILDREN>
+impl<PB: ProverBackendV2, S: AggregationSubCircuit + VerifierTraceGen<PB>, T: AggNodeTraceGen<PB>>
+    AggregationProver<PB> for NonRootAggregationProver<PB, S, T>
 {
     fn get_vk(&self) -> Arc<MultiStarkVerifyingKeyV2> {
         self.vk.clone()
+    }
+
+    fn get_commit(&self) -> PB::Commitment {
+        self.child_vk_pcs_data.commitment.clone()
     }
 
     fn generate_proving_ctx(
         &self,
         proofs: &[Proof],
         user_pv_commit: Option<[F; DIGEST_SIZE]>,
-    ) -> ProvingContextV2<CpuBackendV2> {
-        assert!(proofs.len() <= NUM_CHILDREN);
+    ) -> ProvingContextV2<PB> {
+        assert!(proofs.len() <= MAX_NUM_PROOFS);
+        let verifier_pvs_ctx = self
+            .agg_node_tracegen
+            .generate_verifier_pvs_ctx(proofs, user_pv_commit);
+        let agg_other_ctxs = self
+            .agg_node_tracegen
+            .generate_other_proving_ctxs(proofs, user_pv_commit);
         ProvingContextV2 {
-            per_trace: vec![verifier::generate_proving_ctx(proofs, user_pv_commit)]
-                .into_iter()
+            per_trace: once(verifier_pvs_ctx)
                 .chain(
                     self.circuit
                         .verifier_circuit
@@ -82,25 +78,22 @@ impl<const NUM_CHILDREN: usize> AggregationProver<CpuBackendV2>
                             proofs,
                         ),
                 )
-                .chain([receiver::generate_proving_ctx(
-                    proofs,
-                    user_pv_commit.is_some(),
-                )])
+                .chain(agg_other_ctxs)
                 .enumerate()
                 .collect_vec(),
         }
     }
 
-    fn agg_prove(
+    fn agg_prove<E: StarkWhirEngine<PB = PB>>(
         &self,
         proofs: &[Proof],
         user_pv_commit: Option<[F; DIGEST_SIZE]>,
     ) -> Result<Proof> {
         let ctx = self.generate_proving_ctx(proofs, user_pv_commit);
         if tracing::enabled!(tracing::Level::INFO) {
-            trace_heights_tracing_info(&ctx.per_trace, &airs(&self.circuit.verifier_circuit));
+            trace_heights_tracing_info(&ctx.per_trace, &self.circuit.airs());
         }
-        let engine = BabyBearPoseidon2CpuEngineV2::<DuplexSponge>::new(self.pk.params);
+        let engine = E::new(self.pk.params);
         Ok(engine.prove(
             &engine.device().transport_pk_to_device(self.pk.as_ref()),
             ctx,
@@ -108,37 +101,29 @@ impl<const NUM_CHILDREN: usize> AggregationProver<CpuBackendV2>
     }
 }
 
-impl<const NUM_CHILDREN: usize> NonRootAggregationProver<NUM_CHILDREN> {
-    pub fn new(child_vk: Arc<MultiStarkVerifyingKeyV2>, system_params: SystemParams) -> Self {
-        let verifier_circuit =
-            VerifierSubCircuit::new_with_set_continuations(child_vk.clone(), true);
-        let engine = BabyBearPoseidon2CpuEngineV2::<DuplexSponge>::new(system_params);
+impl<PB: ProverBackendV2, S: AggregationSubCircuit + VerifierTraceGen<PB>, T: AggNodeTraceGen<PB>>
+    NonRootAggregationProver<PB, S, T>
+{
+    pub fn new<E: StarkWhirEngine<SC = SC, PB = PB>>(
+        child_vk: Arc<MultiStarkVerifyingKeyV2>,
+        system_params: SystemParams,
+    ) -> Self {
+        let verifier_circuit = S::new(child_vk.clone(), true);
+        let engine = E::new(system_params);
         let child_vk_pcs_data = verifier_circuit.commit_child_vk(&engine, &child_vk);
-        let (pk, vk) = engine.keygen(&airs(&verifier_circuit));
+        let circuit = Arc::new(AggregationCircuit::new(Arc::new(verifier_circuit)));
+        let (pk, vk) = engine.keygen(&circuit.airs());
         Self {
             pk: Arc::new(pk),
             vk: Arc::new(vk),
+            agg_node_tracegen: T::new(),
             child_vk,
             child_vk_pcs_data,
-            circuit: Arc::new(NonRootAggregationCircuit::new(Arc::new(verifier_circuit))),
+            circuit,
         }
     }
 
-    pub fn get_circuit(&self) -> Arc<NonRootAggregationCircuit> {
+    pub fn get_circuit(&self) -> Arc<AggregationCircuit<S>> {
         self.circuit.clone()
     }
-}
-
-fn airs(
-    verifier_circuit: &VerifierSubCircuit<MAX_NUM_PROOFS>,
-) -> Vec<AirRef<BabyBearPoseidon2Config>> {
-    let public_values_bus = verifier_circuit.bus_inventory.public_values_bus;
-    [Arc::new(VerifierPvsAir {
-        public_values_bus,
-        cached_commit_bus: verifier_circuit.bus_inventory.cached_commit_bus,
-    }) as AirRef<BabyBearPoseidon2Config>]
-    .into_iter()
-    .chain(verifier_circuit.airs())
-    .chain([Arc::new(UserPvsReceiverAir { public_values_bus }) as AirRef<BabyBearPoseidon2Config>])
-    .collect_vec()
 }
---
+++ crates/sdk-v2/src/stdin.rs
@@ -0,0 +1,66 @@
+use std::{
+    collections::{HashMap, VecDeque},
+    sync::Arc,
+};
+
+use openvm_circuit::arch::Streams;
+use openvm_stark_backend::p3_field::Field;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Default, Serialize, Deserialize)]
+pub struct StdIn<F = crate::F> {
+    pub buffer: VecDeque<Vec<F>>,
+    pub kv_store: HashMap<Vec<u8>, Vec<u8>>,
+}
+
+impl<F: Field> StdIn<F> {
+    pub fn from_bytes(data: &[u8]) -> Self {
+        let mut ret = Self::default();
+        ret.write_bytes(data);
+        ret
+    }
+
+    pub fn read(&mut self) -> Option<Vec<F>> {
+        self.buffer.pop_front()
+    }
+
+    pub fn write<T: Serialize>(&mut self, data: &T) {
+        let words = openvm::serde::to_vec(data).unwrap();
+        let bytes: Vec<u8> = words.into_iter().flat_map(|w| w.to_le_bytes()).collect();
+        self.write_bytes(&bytes);
+    }
+
+    pub fn write_bytes(&mut self, data: &[u8]) {
+        let field_data = data.iter().map(|b| F::from_canonical_u8(*b)).collect();
+        self.buffer.push_back(field_data);
+    }
+
+    pub fn write_field(&mut self, data: &[F]) {
+        self.buffer.push_back(data.to_vec());
+    }
+    pub fn add_key_value(&mut self, key: Vec<u8>, value: Vec<u8>) {
+        self.kv_store.insert(key, value);
+    }
+}
+
+impl<F: Field> From<StdIn<F>> for Streams<F> {
+    fn from(mut std_in: StdIn<F>) -> Self {
+        let mut data = Vec::<Vec<F>>::new();
+        while let Some(input) = std_in.read() {
+            data.push(input);
+        }
+        let mut ret = Streams::new(data);
+        ret.kv_store = Arc::new(std_in.kv_store);
+        ret
+    }
+}
+
+impl<F: Field> From<Vec<Vec<F>>> for StdIn<F> {
+    fn from(inputs: Vec<Vec<F>>) -> Self {
+        let mut ret = StdIn::<F>::default();
+        for input in inputs {
+            ret.write_field(&input);
+        }
+        ret
+    }
+}
