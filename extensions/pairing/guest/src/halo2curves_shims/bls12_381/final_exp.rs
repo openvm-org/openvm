adee03ae1eab0586b47cc9f1e4d3907617062546
Ayush Shukla <ayush@axiom.xyz>
2025-11-19 01:45:41 +0100

    fix: cache bls constants

--- extensions/pairing/guest/src/halo2curves_shims/bls12_381/final_exp.rs
+++ extensions/pairing/guest/src/halo2curves_shims/bls12_381/final_exp.rs
@@ -1,5 +1,9 @@
+use alloc::vec::Vec;
+use core::convert::TryInto;
 use halo2curves_axiom::bls12_381::{Fq, Fq12, Fq2};
+use lazy_static::lazy_static;
 use num_bigint::BigUint;
+use num_traits::Zero;
 use openvm_ecc_guest::{
     algebra::{ExpBytes, Field},
     AffinePoint,
@@ -7,6 +11,38 @@ use openvm_ecc_guest::{
 
 use super::{Bls12_381, FINAL_EXP_FACTOR, LAMBDA, POLY_FACTOR};
 use crate::pairing::{FinalExp, MultiMillerLoop};
+
+lazy_static! {
+    static ref FINAL_EXP_FACTOR_TIMES_27: BigUint = FINAL_EXP_FACTOR.clone() * BigUint::from(27u32);
+    static ref FINAL_EXP_FACTOR_TIMES_27_BE: Vec<u8> = FINAL_EXP_FACTOR_TIMES_27.to_bytes_be();
+    static ref PTH_ROOT_INV_EXP_BE: Vec<u8> = {
+        let exp_inv = FINAL_EXP_FACTOR_TIMES_27
+            .modinv(&POLY_FACTOR.clone())
+            .unwrap();
+        let exp = neg_mod(&exp_inv, &POLY_FACTOR);
+        exp.to_bytes_be()
+    };
+    static ref POLY_FACTOR_TIMES_FINAL_EXP: BigUint =
+        POLY_FACTOR.clone() * FINAL_EXP_FACTOR.clone();
+    static ref POLY_FACTOR_TIMES_FINAL_EXP_BE: Vec<u8> = POLY_FACTOR_TIMES_FINAL_EXP.to_bytes_be();
+    static ref THREE: BigUint = BigUint::from(3u32);
+    static ref THREE_BE: Vec<u8> = THREE.to_bytes_be();
+    static ref ROOT27_EXPONENT_BYTES: [Vec<u8>; 3] = {
+        let exponent = POLY_FACTOR_TIMES_FINAL_EXP.clone();
+        let moduli = [THREE.clone(), THREE.clone().pow(2), THREE.clone().pow(3)];
+        let mut exps = Vec::with_capacity(3);
+        for modulus in moduli.iter() {
+            let inv = exponent.modinv(modulus).unwrap();
+            let exp = neg_mod(&inv, modulus);
+            exps.push(exp.to_bytes_be());
+        }
+        exps.try_into().expect("three exponents")
+    };
+    static ref LAMBDA_INV_MOD_FINAL_EXP_BE: Vec<u8> = {
+        let exponent = LAMBDA.clone().modinv(&FINAL_EXP_FACTOR.clone()).unwrap();
+        exponent.to_bytes_be()
+    };
+}
 
 // The paper only describes the implementation for Bn254, so we use the gnark implementation for
 // Bls12_381.
@@ -49,66 +85,63 @@ impl FinalExp for Bls12_381 {
     // returns c (residueWitness) and s (scalingFactor)
     // The Gnark implementation is based on https://eprint.iacr.org/2024/640.pdf
     fn final_exp_hint(f: &Self::Fp12) -> (Self::Fp12, Self::Fp12) {
-        // 1. get p-th root inverse
-        let mut exp = FINAL_EXP_FACTOR.clone() * BigUint::from(27u32);
-        let mut root = f.exp_bytes(true, &exp.to_bytes_be());
-        let root_pth_inv: Fq12;
-        if root == Fq12::ONE {
-            root_pth_inv = Fq12::ONE;
-        } else {
-            let exp_inv = exp.modinv(&POLY_FACTOR.clone()).unwrap();
-            exp = exp_inv % POLY_FACTOR.clone();
-            root_pth_inv = root.exp_bytes(false, &exp.to_bytes_be());
-        }
+        final_exp_witness(f)
+    }
+}
 
-        // 2.1. get order of 3rd primitive root
-        let three = BigUint::from(3u32);
-        let mut order_3rd_power: u32 = 0;
-        exp = POLY_FACTOR.clone() * FINAL_EXP_FACTOR.clone();
+fn final_exp_witness(f: &Fq12) -> (Fq12, Fq12) {
+    // 1. Compute the p-th root inverse factor.
+    let root = f.exp_bytes(true, FINAL_EXP_FACTOR_TIMES_27_BE.as_slice());
+    let root_pth_inverse = if root == Fq12::ONE {
+        Fq12::ONE
+    } else {
+        root.exp_bytes(true, PTH_ROOT_INV_EXP_BE.as_slice())
+    };
 
-        root = f.exp_bytes(true, &exp.to_bytes_be());
-        let three_be = three.to_bytes_be();
-        // NOTE[yj]: we can probably remove this first check as an optimization since we initialize
-        // order_3rd_power to 0
-        if root == Fq12::ONE {
-            order_3rd_power = 0;
-        }
-        root = root.exp_bytes(true, &three_be);
-        if root == Fq12::ONE {
-            order_3rd_power = 1;
-        }
-        root = root.exp_bytes(true, &three_be);
-        if root == Fq12::ONE {
-            order_3rd_power = 2;
-        }
-        root = root.exp_bytes(true, &three_be);
-        if root == Fq12::ONE {
-            order_3rd_power = 3;
-        }
+    // 2.1 Determine the order of the 3rd primitive root.
+    let mut order_3rd_power = 0u32;
+    let mut root_order = f.exp_bytes(true, POLY_FACTOR_TIMES_FINAL_EXP_BE.as_slice());
+    if root_order == Fq12::ONE {
+        order_3rd_power = 0;
+    }
+    root_order = root_order.exp_bytes(true, THREE_BE.as_slice());
+    if root_order == Fq12::ONE {
+        order_3rd_power = 1;
+    }
+    root_order = root_order.exp_bytes(true, THREE_BE.as_slice());
+    if root_order == Fq12::ONE {
+        order_3rd_power = 2;
+    }
+    root_order = root_order.exp_bytes(true, THREE_BE.as_slice());
+    if root_order == Fq12::ONE {
+        order_3rd_power = 3;
+    }
 
-        // 2.2. get 27th root inverse
-        let root_27th_inv: Fq12;
-        if order_3rd_power == 0 {
-            root_27th_inv = Fq12::ONE;
-        } else {
-            let order_3rd = three.pow(order_3rd_power);
-            exp = POLY_FACTOR.clone() * FINAL_EXP_FACTOR.clone();
-            root = f.exp_bytes(true, &exp.to_bytes_be());
-            let exp_inv = exp.modinv(&order_3rd).unwrap();
-            exp = exp_inv % order_3rd;
-            root_27th_inv = root.exp_bytes(false, &exp.to_bytes_be());
-        }
+    // 2.2 Compute the 27th root inverse.
+    let root_27th_inverse = if order_3rd_power == 0 {
+        Fq12::ONE
+    } else {
+        let exponent_bytes = &ROOT27_EXPONENT_BYTES[(order_3rd_power - 1) as usize];
+        let root = f.exp_bytes(true, POLY_FACTOR_TIMES_FINAL_EXP_BE.as_slice());
+        root.exp_bytes(true, exponent_bytes.as_slice())
+    };
 
-        // 2.3. shift the Miller loop result so that millerLoop * scalingFactor
-        // is of order finalExpFactor
-        let s = root_pth_inv * root_27th_inv;
-        let f = f * s;
+    // 2.3 Shift the Miller loop output by the scaling factor so that the result
+    // has order FINAL_EXP_FACTOR.
+    let scaling_factor = root_pth_inverse * root_27th_inverse;
+    let shifted = f * scaling_factor;
 
-        // 3. get the witness residue
-        // lambda = q - u, the optimal exponent
-        exp = LAMBDA.clone().modinv(&FINAL_EXP_FACTOR.clone()).unwrap();
-        let c = f.exp_bytes(true, &exp.to_bytes_be());
+    // 3. Compute the residue witness with exponent lambda^{-1} mod FINAL_EXP_FACTOR.
+    let residue_witness = shifted.exp_bytes(true, LAMBDA_INV_MOD_FINAL_EXP_BE.as_slice());
 
-        (c, s)
+    (residue_witness, scaling_factor)
+}
+
+fn neg_mod(value: &BigUint, modulus: &BigUint) -> BigUint {
+    let value_mod = value % modulus;
+    if value_mod.is_zero() {
+        BigUint::from(0u32)
+    } else {
+        modulus.clone() - value_mod
     }
 }
