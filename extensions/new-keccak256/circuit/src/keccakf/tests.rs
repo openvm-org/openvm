f4284bdc011998f47a2cbfa9f5aa31de0fd5c662
GunaDD <gunadigan@gmail.com>
2025-12-15 21:27:44 +0000

    feat: keccakf unit test

--- extensions/new-keccak256/circuit/src/keccakf/tests.rs
+++ extensions/new-keccak256/circuit/src/keccakf/tests.rs
@@ -1,1 +1,140 @@
+use openvm_circuit::{arch::{ExecutionBridge, PreflightExecutor, testing::{BITWISE_OP_LOOKUP_BUS, TestBuilder, TestChipHarness, VmChipTestBuilder}}, system::memory::{SharedMemoryHelper, offline_checker::MemoryBridge}, utils::get_random_message};
+use openvm_circuit_primitives::bitwise_op_lookup::{BitwiseOperationLookupAir, BitwiseOperationLookupBus, BitwiseOperationLookupChip, SharedBitwiseOperationLookupChip};
+use openvm_instructions::{LocalOpcode, instruction::Instruction};
+use openvm_new_keccak256_transpiler::{KeccakfOpcode, XorinOpcode};
+use std::sync::Arc;
 
+use crate::xorin::{XorinVmChip, XorinVmExecutor, XorinVmFiller, air::XorinVmAir, columns::{XorinInstructionCols, XorinMemoryCols, XorinSpongeCols}};
+use openvm_instructions::riscv::RV32_CELL_BITS;
+use openvm_stark_sdk::{p3_baby_bear::BabyBear, utils::create_seeded_rng};
+use openvm_circuit::arch::Arena;
+use rand::{Rng, rngs::StdRng};
+use openvm_stark_backend::{p3_field::FieldAlgebra, p3_matrix::dense::RowMajorMatrix};
+
+type F = BabyBear;
+type Harness = TestChipHarness<F, XorinVmExecutor, XorinVmAir, XorinVmChip<F>>;
+use openvm_stark_backend::verifier::VerificationError;
+
+fn create_harness_fields(
+    execution_bridge: ExecutionBridge,
+    memory_bridge: MemoryBridge,
+    bitwise_chip: Arc<BitwiseOperationLookupChip<RV32_CELL_BITS>>,
+    memory_helper: SharedMemoryHelper<F>,
+    address_bits: usize,
+) -> (XorinVmAir, XorinVmExecutor, XorinVmChip<F>) {
+    let air = XorinVmAir::new(
+        execution_bridge, 
+        memory_bridge, 
+        bitwise_chip.bus(), 
+        address_bits,
+        XorinOpcode::CLASS_OFFSET
+    );
+
+    let executor = XorinVmExecutor::new(XorinOpcode::CLASS_OFFSET, address_bits);
+    let chip = XorinVmChip::new(
+        XorinVmFiller::new(bitwise_chip, address_bits),
+        memory_helper
+    );
+    (air, executor, chip)
+}
+
+fn create_test_harness(
+    tester: &mut VmChipTestBuilder<F>
+) -> (
+    Harness, 
+    (
+        BitwiseOperationLookupAir<RV32_CELL_BITS>,
+        SharedBitwiseOperationLookupChip<RV32_CELL_BITS>,
+    )
+) {
+    let bitwise_bus = BitwiseOperationLookupBus::new(BITWISE_OP_LOOKUP_BUS);
+    let bitwise_chip = Arc::new(BitwiseOperationLookupChip::<RV32_CELL_BITS>::new(bitwise_bus));
+
+    let (air, executor, chip) = create_harness_fields(
+        tester.execution_bridge(),
+        tester.memory_bridge(),
+        bitwise_chip.clone(),
+        tester.memory_helper(),
+        tester.address_bits()
+    );
+
+    const MAX_TRACE_ROWS: usize = 4096; 
+
+    let harness = Harness::with_capacity(executor, air, chip, MAX_TRACE_ROWS);
+
+    (harness, (bitwise_chip.air, bitwise_chip))
+}
+
+fn set_and_execute<RA: Arena, E: PreflightExecutor<F, RA>> (
+    tester: &mut impl TestBuilder<F>, 
+    executor: &mut E, 
+    arena: &mut RA, 
+    rng: &mut StdRng, 
+    opcode: KeccakfOpcode,
+) {
+    const MAX_LEN: usize = 200;
+    let rand_buffer = get_random_message(rng, MAX_LEN);
+    let mut rand_buffer_arr = [0u8; MAX_LEN];
+    rand_buffer_arr.copy_from_slice(&rand_buffer);
+    
+    use openvm_circuit::arch::testing::memory::gen_pointer;
+    let rd = gen_pointer(rng, 4);
+
+    let buffer_ptr = gen_pointer(rng, MAX_LEN);
+    
+    println!("rand_buffer_arr {:?}", rand_buffer_arr);
+    
+    let rand_buffer_arr_f = rand_buffer_arr.map(F::from_canonical_u8);
+    
+    for i in 0..(MAX_LEN/4) {
+        let buffer_chunk: [F; 4] = rand_buffer_arr_f[4 * i..4 * i + 4]
+            .try_into()
+            .expect("slice has length 4");
+        tester.write(2, buffer_ptr + 4 * i, buffer_chunk);
+    }
+
+    tester.write(1, rd, buffer_ptr.to_le_bytes().map(F::from_canonical_u8));
+    
+    tester.execute(
+        executor,
+        arena,
+        &Instruction::from_usize(opcode.global_opcode(), [rd, 0, 0, 1, 2])
+    );
+
+    let mut output_buffer = [F::from_canonical_u8(0); MAX_LEN];
+
+    for i in 0..(MAX_LEN/4) {
+        let output_chunk : [F; 4] = tester.read(2, buffer_ptr + 4 * i);
+        output_buffer[4 * i..4 * i + 4].copy_from_slice(&output_chunk);
+    }
+
+    println!("completed keccakf set and execute");
+}   
+
+#[test] 
+fn keccakf_chip_positive_tests() {
+    let num_ops: usize = 1;
+    
+    for _ in 0..num_ops {
+        let mut rng = create_seeded_rng();
+        let mut tester = VmChipTestBuilder::default();
+        let (mut harness, bitwise) = create_test_harness(&mut tester);
+
+        set_and_execute(
+            &mut tester,
+            &mut harness.executor,
+            &mut harness.arena, 
+            &mut rng, 
+            KeccakfOpcode::KECCAKF,
+        );
+    
+        let tester = tester
+            .build()
+            .load(harness)
+            .load_periphery(bitwise)
+            .finalize();
+        tester.simple_test().expect("Verification failed");    
+    }
+
+
+}
--- extensions/new-keccak256/circuit/src/xorin/tests.rs
+++ extensions/new-keccak256/circuit/src/xorin/tests.rs
@@ -119,12 +119,11 @@ fn set_and_execute<RA: Arena, E: PreflightExecutor<F, RA>> (
             .try_into()
             .expect("slice has length 4");
         tester.write(2, input_ptr + 4 * i, input_chunk);
-
-        tester.write(1, rd, buffer_ptr.to_le_bytes().map(F::from_canonical_u8));
-        tester.write(1, rs1, input_ptr.to_le_bytes().map(F::from_canonical_u8));
-        tester.write(1, rs2, buffer_length.to_le_bytes().map(F::from_canonical_u8));
     }
     
+    tester.write(1, rd, buffer_ptr.to_le_bytes().map(F::from_canonical_u8));
+    tester.write(1, rs1, input_ptr.to_le_bytes().map(F::from_canonical_u8));
+    tester.write(1, rs2, buffer_length.to_le_bytes().map(F::from_canonical_u8));
     tester.execute(
         executor,
         arena,
