8c4073813ec4e1426247ad7dc590f60d11ccefaa
GunaDD <gunadigan@gmail.com>
2025-12-18 04:07:33 +0000

    chore: feature gate the guest program example to fix lint

--- examples/keccak/src/main.rs
+++ examples/keccak/src/main.rs
@@ -3,13 +3,19 @@ use openvm as _;
 // [!endregion imports]
 
 // [!region main]
+
+// todo: call the forked tiny keccak library once that is updated instead of directly calling the
+// keccak256_guest native functions
 pub fn main() {
-    let mut buffer = [1u8; 200];
-    let input = [2u8; 136];
-    let output = [3u8; 136];
-    let len: usize = 136;
-    openvm_new_keccak256_guest::native_xorin(buffer.as_mut_ptr(), input.as_ptr(), len);
-    assert_eq!(buffer[..136], output);
-    openvm_new_keccak256_guest::native_keccakf(buffer.as_mut_ptr());
+    #[cfg(target_os = "zkvm")]
+    {
+        let mut buffer = [1u8; 200];
+        let input = [2u8; 136];
+        let output = [3u8; 136];
+        let len: usize = 136;
+        openvm_new_keccak256_guest::native_xorin(buffer.as_mut_ptr(), input.as_ptr(), len);
+        assert_eq!(buffer[..136], output);
+        openvm_new_keccak256_guest::native_keccakf(buffer.as_mut_ptr());
+    }
 }
 // [!endregion main]
