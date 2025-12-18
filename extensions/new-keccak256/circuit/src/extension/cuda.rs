9291e2172e4b70feb6cf3551ff13333c3d5fb90c
Ayush Shukla <ayush@axiom.xyz>
2025-12-18 22:30:46 +0000

    feat: add xorin CUDA trace generation
    - Add CUDA kernel for xorin trace generation (xorin.cu, xorin.cuh)
    - Add XorinVmChipGpu for GPU-accelerated proving
    - Add CUDA ABI bindings and extension integration
    - Add CUDA unit tests for xorin tracegen

---
+++ extensions/new-keccak256/circuit/src/extension/cuda.rs
@@ -0,0 +1,85 @@
+use openvm_circuit::{
+    arch::DenseRecordArena,
+    system::cuda::{
+        extensions::{
+            get_inventory_range_checker, get_or_create_bitwise_op_lookup, SystemGpuBuilder,
+        },
+        SystemChipInventoryGPU,
+    },
+};
+use openvm_cuda_backend::{engine::GpuBabyBearPoseidon2Engine, prover_backend::GpuBackend};
+use openvm_rv32im_circuit::Rv32ImGpuProverExt;
+use openvm_stark_sdk::config::baby_bear_poseidon2::BabyBearPoseidon2Config;
+
+use super::*;
+use crate::cuda::XorinVmChipGpu;
+
+pub struct NewKeccak256GpuProverExt;
+
+impl VmProverExtension<GpuBabyBearPoseidon2Engine, DenseRecordArena, NewKeccak256>
+    for NewKeccak256GpuProverExt
+{
+    fn extend_prover(
+        &self,
+        _extension: &NewKeccak256,
+        inventory: &mut ChipInventory<BabyBearPoseidon2Config, DenseRecordArena, GpuBackend>,
+    ) -> Result<(), ChipInventoryError> {
+        let pointer_max_bits = inventory.airs().pointer_max_bits();
+        let timestamp_max_bits = inventory.timestamp_max_bits();
+
+        let range_checker = get_inventory_range_checker(inventory);
+        let bitwise_lu = get_or_create_bitwise_op_lookup(inventory)?;
+
+        inventory.next_air::<XorinVmAir>()?;
+        let xorin_chip = XorinVmChipGpu::new(
+            range_checker.clone(),
+            bitwise_lu.clone(),
+            pointer_max_bits,
+            timestamp_max_bits as u32,
+        );
+        inventory.add_executor_chip(xorin_chip);
+
+        // Skip KeccakfVmAir for xorin-only branch
+        inventory.next_air::<KeccakfVmAir>()?;
+
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+pub struct NewKeccak256Rv32GpuBuilder;
+
+type E = GpuBabyBearPoseidon2Engine;
+
+impl VmBuilder<E> for NewKeccak256Rv32GpuBuilder {
+    type VmConfig = NewKeccak256Rv32Config;
+    type SystemChipInventory = SystemChipInventoryGPU;
+    type RecordArena = DenseRecordArena;
+
+    fn create_chip_complex(
+        &self,
+        config: &NewKeccak256Rv32Config,
+        circuit: AirInventory<<E as StarkEngine>::SC>,
+    ) -> Result<
+        VmChipComplex<
+            <E as StarkEngine>::SC,
+            Self::RecordArena,
+            <E as StarkEngine>::PB,
+            Self::SystemChipInventory,
+        >,
+        ChipInventoryError,
+    > {
+        let mut chip_complex =
+            VmBuilder::<E>::create_chip_complex(&SystemGpuBuilder, &config.system, circuit)?;
+        let inventory = &mut chip_complex.inventory;
+        VmProverExtension::<E, _, _>::extend_prover(&Rv32ImGpuProverExt, &config.rv32i, inventory)?;
+        VmProverExtension::<E, _, _>::extend_prover(&Rv32ImGpuProverExt, &config.rv32m, inventory)?;
+        VmProverExtension::<E, _, _>::extend_prover(&Rv32ImGpuProverExt, &config.io, inventory)?;
+        VmProverExtension::<E, _, _>::extend_prover(
+            &NewKeccak256GpuProverExt,
+            &config.keccak,
+            inventory,
+        )?;
+        Ok(chip_complex)
+    }
+}
