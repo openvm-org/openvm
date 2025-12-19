63daf4681afb74731238d326ceb4723a7f1529fc
Zach Langley <zach@axiom.xyz>
2025-12-19 18:01:38 -0500

    feat: program to print agg commit

---
+++ crates/sdk/src/bin/print_agg_commit.rs
@@ -0,0 +1,12 @@
+use openvm_native_compiler::ir::DIGEST_SIZE;
+use openvm_sdk::{Sdk, F};
+use openvm_stark_backend::p3_field::PrimeField32;
+
+fn main() {
+    let sdk = Sdk::standard();
+    let (agg_pk, _agg_vk) = sdk.agg_keygen().expect("agg_keygen failed");
+
+    let internal_commit: [F; DIGEST_SIZE] = agg_pk.internal_committed_exe.get_program_commit().into();
+    let internal_u32 = internal_commit.map(|x| x.as_canonical_u32());
+    println!("internal_program_commit_u32: {internal_u32:?}");
+}
