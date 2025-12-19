2bc72ecb476fa757d12b3e1cbb54abbf1f917953
Zach Langley <zach@axiom.xyz>
2025-12-19 18:09:54 -0500

    update

--- crates/sdk/src/bin/print_agg_commit.rs
+++ crates/sdk/src/bin/print_agg_commit.rs
@@ -6,7 +6,8 @@ fn main() {
     let sdk = Sdk::standard();
     let (agg_pk, _agg_vk) = sdk.agg_keygen().expect("agg_keygen failed");
 
-    let internal_commit: [F; DIGEST_SIZE] = agg_pk.internal_committed_exe.get_program_commit().into();
+    let internal_commit: [F; DIGEST_SIZE] =
+        agg_pk.internal_committed_exe.get_program_commit().into();
     let internal_u32 = internal_commit.map(|x| x.as_canonical_u32());
     println!("internal_program_commit_u32: {internal_u32:?}");
 }
