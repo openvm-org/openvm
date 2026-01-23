b955d5a2b3e6832c0605680d1c38fc10af3dc385
Euicheon Hong <euicheon@intrinsictech.xyz>
2026-01-24 08:45:23 +0900

    feat: parse official test vector files

--- guest-libs/sha2/tests/lib.rs
+++ guest-libs/sha2/tests/lib.rs
@@ -2,8 +2,7 @@
 mod tests {
     use eyre::Result;
     use hex::FromHex;
-    use openvm_circuit::arch::Streams;
-    use openvm_circuit::utils::air_test_with_min_segments;
+    use openvm_circuit::{arch::Streams, utils::air_test_with_min_segments};
     use openvm_instructions::exe::VmExe;
     use openvm_rv32im_transpiler::{
         Rv32ITranspilerExtension, Rv32IoTranspilerExtension, Rv32MTranspilerExtension,
@@ -14,11 +13,59 @@ mod tests {
     use openvm_stark_sdk::p3_baby_bear::BabyBear;
     use openvm_toolchain_tests::{build_example_program_at_path, get_programs_dir};
     use openvm_transpiler::{transpiler::Transpiler, FromElf};
+    use std::fs;
 
     type F = BabyBear;
 
-    #[test]
-    fn test_sha2() -> Result<()> {
+    enum Sha2Type {
+        Sha256,
+        Sha384,
+        Sha512,
+    }
+
+    struct TestVector {
+        input: Vec<u8>,
+        expected_output: Vec<u8>,
+    }
+
+    fn parse_test_vectors(file_name: &str) -> Vec<TestVector> {
+        let mut test_vectors = Vec::new();
+
+        let get_attribute_from_line = |line: &str, attribute: &str| -> Option<String> {
+            line.trim()
+                .strip_prefix(&(attribute.to_string() + " = "))
+                .map(|s| s.to_string())
+        };
+
+        let file_content =
+            fs::read_to_string("tests/programs/examples/test_vectors/".to_string() + file_name)
+                .unwrap();
+        let mut lines = file_content.lines();
+        while let Some(line) = lines.next() {
+            let Some(len_str) = get_attribute_from_line(line, "Len") else {
+                continue;
+            };
+            let msg_str = get_attribute_from_line(lines.next().unwrap(), "Msg").unwrap();
+            let md_str = get_attribute_from_line(lines.next().unwrap(), "MD").unwrap();
+
+            let len: usize = len_str.parse().unwrap();
+            let msg = if len == 0 {
+                Vec::new()
+            } else {
+                Vec::from_hex(&msg_str).unwrap()
+            };
+            let md = Vec::from_hex(&md_str).unwrap();
+
+            test_vectors.push(TestVector {
+                input: msg,
+                expected_output: md,
+            });
+        }
+
+        test_vectors
+    }
+
+    fn test_sha2(test_vector_file_name: &str, sha2_type: Sha2Type) -> Result<()> {
         let config = Sha2Rv32Config::default();
         let elf =
             build_example_program_at_path(get_programs_dir!("tests/programs"), "sha2", &config)?;
@@ -30,19 +77,31 @@ mod tests {
                 .with_extension(Rv32IoTranspilerExtension)
                 .with_extension(Sha2TranspilerExtension),
         )?;
-        let mut stdin = StdIn::default();
-        let input_string = "";
-        let output_string = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 
-        stdin.write(&Vec::from_hex(input_string).unwrap());
-        stdin.write(&Vec::from_hex(output_string).unwrap());
-        air_test_with_min_segments(
-            Sha2Rv32Builder,
-            config,
-            openvm_exe,
-            <Streams<F> as From<StdIn<F>>>::from(stdin),
-            1,
-        );
+        for test_vector in parse_test_vectors(test_vector_file_name) {
+            let mut stdin = StdIn::default();
+            stdin.write(&match sha2_type {
+                Sha2Type::Sha256 => 256u32,
+                Sha2Type::Sha384 => 384u32,
+                Sha2Type::Sha512 => 512u32,
+            });
+            stdin.write(&test_vector.input);
+            stdin.write(&test_vector.expected_output);
+
+            air_test_with_min_segments(
+                Sha2Rv32Builder,
+                config.clone(),
+                openvm_exe.clone(),
+                <Streams<F> as From<StdIn<F>>>::from(stdin),
+                1,
+            );
+        }
+
         Ok(())
+    }
+
+    #[test]
+    fn test_sha256_short() -> Result<()> {
+        test_sha2("SHA256ShortMsg.rsp", Sha2Type::Sha256)
     }
 }
