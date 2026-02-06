43b959f24c7ce8bb74a379470cb6433a5a6b3b03
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2026-02-06 11:04:35 -0800

    fix: plonky3 trait updates

--- extensions/native/recursion/tests/recursion.rs
+++ extensions/native/recursion/tests/recursion.rs
@@ -14,7 +14,7 @@
 // use openvm_stark_backend::{
 //     config::{Domain, StarkGenericConfig, Val},
 //     p3_commit::PolynomialSpace,
-//     p3_field::{extension::BinomialExtensionField, FieldAlgebra},
+//     p3_field::{extension::BinomialExtensionField, PrimeCharacteristicRing},
 //     prover::{
 //         cpu::{CpuBackend, CpuDevice},
 //         hal::{DeviceDataTransporter, ProverBackend, TraceCommitter},
@@ -37,8 +37,8 @@
 
 //     let mut builder = AsmBuilder::<F, EF>::default();
 
-//     let prev: Felt<_> = builder.constant(F::from_canonical_u32(a));
-//     let next: Felt<_> = builder.constant(F::from_canonical_u32(b));
+//     let prev: Felt<_> = builder.constant(F::from_u32(a));
+//     let next: Felt<_> = builder.constant(F::from_u32(b));
 
 //     builder.commit_public_value(prev);
 //     builder.commit_public_value(next);
