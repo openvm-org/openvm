c3bbf4db7d9e483595dbef0177e3e56d00cdd897
Euicheon Hong <78904698+gdmlcjs@users.noreply.github.com>
2026-02-08 10:12:59 +0900

    chore: feature gate dependency on external sha2 crate (#2395)
    The dependency on the RustCrypto sha2 crate is now gated by the
    "import_sha2" feature .
    The feature is enabled by default and when it is, the `sha2::Digest`
    trait is implemented for our Sha2 structs.
    To avoid compile errors, the "import_sha2" feature must be kept enabled
    when the target is not ZKVM.
    
    closes INT-5999

--- guest-libs/sha2/src/lib.rs
+++ guest-libs/sha2/src/lib.rs
@@ -1,5 +1,9 @@
 #![no_std]
 
+#[cfg(not(any(target_os = "zkvm", feature = "import_sha2")))]
+compile_error!("openvm-sha2 requires the 'import_sha2' feature to be enabled on non-zkvm targets");
+
+#[cfg(feature = "import_sha2")]
 pub use sha2::Digest;
 
 #[cfg(not(target_os = "zkvm"))]
