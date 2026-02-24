77febb4aa2145d1e044fd2bfca3ca838a965019b
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2026-02-24 14:04:30 -0500

    feat: combine root pv DAG commits into a single commit (#2460)

--- crates/continuations-v2/src/circuit/root/mod.rs
+++ crates/continuations-v2/src/circuit/root/mod.rs
@@ -18,6 +18,7 @@ pub mod bus;
 pub mod commit;
 pub mod memory;
 pub mod verifier;
+
 #[repr(C)]
 #[derive(AlignedBorrow, Debug)]
 pub struct RootVerifierPvs<F> {
@@ -25,15 +26,9 @@ pub struct RootVerifierPvs<F> {
     /// the starting app memory state (i.e. initial_root), and the initial app program counter
     /// (i.e. initial_pc).
     pub app_exe_commit: [F; DIGEST_SIZE],
-    /// Cached trace commit of the leaf verifier circuit's SymbolicExpressionAir, which is
-    /// derived from the app_vk
-    pub app_dag_commit: [F; DIGEST_SIZE],
-    /// Cached trace commit of the internal-for-leaf verifier circuit's SymbolicExpressionAir,
-    /// which is derived from the leaf_vk
-    pub leaf_dag_commit: [F; DIGEST_SIZE],
-    /// Cached trace commit of the first (i.e. index 0) internal-recursive layer verifier
-    /// circuit's SymbolicExpressionAir, which is derived from the internal_for_leaf_vk
-    pub internal_for_leaf_dag_commit: [F; DIGEST_SIZE],
+    /// Commit to the app-level verifying key, computed by compressing together the app, leaf,
+    /// and internal-for-leaf DAG commits.
+    pub app_vk_commit: [F; DIGEST_SIZE],
 }
 
 // Trait that root provers use to remain generic in PB. Tracegen returns both the AIR proving
