2b790589ffa472817baac1d02bb1697c1e81fb42
GunaDD <gunadigan@gmail.com>
2026-02-27 21:41:28 +0000

    feat: new keccak (#2303)
    New Keccak with Xorin and Keccakf chip and opcode
    
    - [ ] I have performed a self-review of my own code
    - [ ] Add negative tests for xorin chip
    - [ ] Add negative tests for keccakf chip
    - [x] Add unit test to CI
    - [x] Add new guest code for E2E test to CI (the keccak example is
    updated, but I am thinking of adding another one)
    - [ ] Check with Ayush if I implemented the SizedRecord trait correctly
    - [ ] Rebase to include Zach's new Plonky3 update and update the keccakf
    trace gen to not have to transpose any more before giving it into the
    input
    - [ ] Maybe add comments to justify the correctness of the
    constrain_input_read and constraint_output_write function
    
    To reviewer: I will still have to complete the above checklist. But you
    can start reviewing if you would like to.
    
    Closes INT-5017, INT-5721, INT-5720, INT-5718, INT-5717, INT-5646,
    INT-5018

---
+++ extensions/new-keccak256/circuit/src/lib.rs
@@ -0,0 +1,10 @@
+#![cfg_attr(feature = "tco", allow(incomplete_features))]
+#![cfg_attr(feature = "tco", feature(explicit_tail_calls))]
+#![cfg_attr(feature = "tco", allow(internal_features))]
+#![cfg_attr(feature = "tco", feature(core_intrinsics))]
+
+mod keccakf;
+mod xorin;
+
+mod extension;
+pub use extension::*;
