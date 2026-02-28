2d53d06261fb1e4c09685d9036d8de6bf9dbe355
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2026-02-28 09:09:38 +0000

    fix: keccak cuda tests

--- guest-libs/keccak256/tests/lib.rs
+++ guest-libs/keccak256/tests/lib.rs
@@ -15,7 +15,7 @@ mod tests {
     use openvm_rv32im_transpiler::{
         Rv32ITranspilerExtension, Rv32IoTranspilerExtension, Rv32MTranspilerExtension,
     };
-    use openvm_sdk::StdIn;
+    use sdk_v2::StdIn;
     use openvm_stark_sdk::p3_baby_bear::BabyBear;
     use openvm_toolchain_tests::{build_example_program_at_path, get_programs_dir};
     use openvm_transpiler::{transpiler::Transpiler, FromElf};
