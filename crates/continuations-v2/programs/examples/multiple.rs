200cd60508a000a53975813e15ae7e2385f91f49
Stephen Hwang <stephenh@intrinsictech.xyz>
2026-03-03 21:47:41 +0000

    fix: clippy issue with guest programs

--- crates/continuations-v2/programs/examples/multiple.rs
+++ crates/continuations-v2/programs/examples/multiple.rs
@@ -1,5 +1,5 @@
-#![cfg_attr(not(feature = "std"), no_main)]
-#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(target_os = "zkvm", no_main)]
+#![cfg_attr(target_os = "zkvm", no_std)]
 
 use openvm_deferral_guest::{deferred_compute, get_deferred_output, Commit};
 
