a07180d2a8684ebc0f02908398f78621a4f7122f
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2026-03-04 22:16:13 -0800

    fix: deferral address space should have 0 cells by default

--- crates/vm/src/arch/testing/cpu.rs
+++ crates/vm/src/arch/testing/cpu.rs
@@ -305,7 +305,6 @@ impl<F: VmField> VmChipTestBuilder<F> {
     pub fn default_persistent() -> Self {
         let mut mem_config = MemoryConfig::default();
         mem_config.addr_spaces[RV32_REGISTER_AS as usize].num_cells = 1 << 29;
-        mem_config.addr_spaces[DEFERRAL_AS as usize].num_cells = 0;
         Self::persistent(mem_config)
     }
 
@@ -379,7 +378,6 @@ impl<F: VmField> Default for VmChipTestBuilder<F> {
         // TODO[jpw]: this is because old tests use `gen_pointer` on address space 1; this can be
         // removed when tests are updated.
         mem_config.addr_spaces[RV32_REGISTER_AS as usize].num_cells = 1 << 29;
-        mem_config.addr_spaces[DEFERRAL_AS as usize].num_cells = 0;
         Self::volatile(mem_config)
     }
 }
