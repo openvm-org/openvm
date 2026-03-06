d496e15a71deec7ffb13205d5f18d43913d4530b
Stephen Hwang <stephenh@intrinsictech.xyz>
2026-03-06 16:22:13 +0000

    chore: rename is_proof_transition for clarity

--- crates/recursion/src/batch_constraint/sumcheck/multilinear/air.rs
+++ crates/recursion/src/batch_constraint/sumcheck/multilinear/air.rs
@@ -119,8 +119,7 @@ where
         let is_last_eval =
             LoopSubAir::local_is_last(local.is_valid, next.is_valid, next.is_first_eval);
 
-        let is_proof_transition =
-            LoopSubAir::local_is_transition(next.is_valid, next.is_proof_start);
+        let is_same_proof = LoopSubAir::local_is_transition(next.is_valid, next.is_proof_start);
         let is_proof_end =
             LoopSubAir::local_is_last(local.is_valid, next.is_valid, next.is_proof_start);
 
@@ -247,7 +246,7 @@ where
         ///////////////////////////////////////////////////////////////////////
 
         builder
-            .when(is_proof_transition)
+            .when(is_same_proof)
             .assert_eq(next.tidx, local.tidx + AB::Expr::from_usize(D_EF));
 
         ///////////////////////////////////////////////////////////////////////
