8f2ceb879a87d34d629da4f0e33fccd7cb1ca6f2
stephenh-axiom-xyz <stephenh@intrinsictech.xyz>
2026-03-18 15:19:34 -0400

    fix: constrain F values in rv32 memory are canonically decomposed (#2576)

---
+++ extensions/deferral/circuit/src/canonicity/mod.rs
@@ -0,0 +1,26 @@
+use openvm_circuit_primitives_derive::AlignedBorrow;
+
+use crate::utils::F_NUM_BYTES;
+
+mod air;
+mod trace;
+
+pub use air::*;
+pub use trace::*;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug)]
+pub struct CanonicityIo<T> {
+    pub x: [T; F_NUM_BYTES],
+    /// Assumed boolean by caller.
+    pub count: T,
+}
+
+#[repr(C)]
+#[derive(AlignedBorrow, Clone, Copy, Debug)]
+pub struct CanonicityAuxCols<T> {
+    /// Marker for the first index where x[i] != order[i] (big-endian).
+    pub diff_marker: [T; F_NUM_BYTES],
+    /// order[i] - x[i] at the first differing index, constrained to [1, 255].
+    pub diff_val: T,
+}
