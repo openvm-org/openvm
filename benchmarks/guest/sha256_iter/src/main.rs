91aa3f6c89cd5f15aaaf43d86fef0a73827b35ed
Ayush Shukla <shuklaayush247@gmail.com>
2026-03-25 23:35:28 +0100

    chore(v2-rc.1): update benchmark guest programs (#2623)
    - update guest programs to use new sha interface
    - update elfs
    
    resolves int-5895

--- benchmarks/guest/sha256_iter/src/main.rs
+++ benchmarks/guest/sha256_iter/src/main.rs
@@ -1,11 +1,11 @@
 use core::hint::black_box;
 
 use openvm as _;
-use openvm_sha2::sha256;
+use openvm_sha2::Sha256;
 
 const ITERATIONS: usize = 150_000;
 
-fn sha256(input: &[u8]) -> [u8; 32] {
+fn sha256_digest(input: &[u8]) -> [u8; 32] {
     let mut sha256 = Sha256::new();
     sha256.update(black_box(input));
     sha256.finalize()
@@ -13,11 +13,11 @@ fn sha256(input: &[u8]) -> [u8; 32] {
 
 pub fn main() {
     // Initialize with hash of an empty vector
-    let mut hash = black_box(sha256(&[]));
+    let mut hash = black_box(sha256_digest(&[]));
 
     // Iteratively apply sha256
     for _ in 0..ITERATIONS {
-        hash = black_box(sha256(&hash));
+        hash = black_box(sha256_digest(&hash));
     }
 
     // Prevent optimizer from optimizing away the computation
