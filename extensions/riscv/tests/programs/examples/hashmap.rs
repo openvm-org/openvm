5702c79401d357cb508b65c7ad2798b5d8c62751
Ayush Shukla <ayush@axiom.xyz>
2026-04-22 22:22:41 +0200

    chore(v2.1-rv64): update hashmap guest to be no_std and use hashbrown

--- extensions/riscv/tests/programs/examples/hashmap.rs
+++ extensions/riscv/tests/programs/examples/hashmap.rs
@@ -3,7 +3,7 @@
 
 extern crate alloc;
 
-use std::collections::HashMap;
+use hashbrown::HashMap;
 
 openvm::entry!(main);
 
@@ -14,5 +14,4 @@ fn main() {
     assert!(map.get(&1) == Some(&2));
     assert!(map.get(&2) == Some(&8));
     assert!(!map.contains_key(&3));
-    println!("{:?}", map.get(&1).unwrap());
 }
