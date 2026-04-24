14020f0a0a4a559d19435727719e71f43891bcfe
Paul Chen <77174148+876pol@users.noreply.github.com>
2026-04-24 17:16:05 -0400

    feat: rv64 cuda base_alu_w, shift_w, mul_w, divrem_w (#2743)
    Resolves INT-7551.

--- extensions/riscv/circuit/src/divrem_w/mod.rs
+++ extensions/riscv/circuit/src/divrem_w/mod.rs
@@ -14,12 +14,12 @@ pub type DivRemWCoreAir = DivRemCoreAir<RV64_WORD_NUM_LIMBS, RV64_CELL_BITS>;
 pub type DivRemWExecutor<A> = DivRemExecutor<A, RV64_WORD_NUM_LIMBS, RV64_CELL_BITS>;
 pub type DivRemWFiller<A> = DivRemFiller<A, RV64_WORD_NUM_LIMBS, RV64_CELL_BITS>;
 
-#[cfg(all(feature = "cuda", not(feature = "cuda")))] // TODO: RV64 GPU port
+#[cfg(feature = "cuda")]
 mod cuda;
-#[cfg(all(feature = "cuda", not(feature = "cuda")))] // TODO: RV64 GPU port
+#[cfg(feature = "cuda")]
 pub use cuda::*;
 
-#[cfg(all(test, any()))] // TODO: port tests to RV64
+#[cfg(test)]
 mod tests;
 
 pub type Rv64DivRemWAir = VmAirWrapper<Rv64MultWAdapterAir, DivRemWCoreAir>;
