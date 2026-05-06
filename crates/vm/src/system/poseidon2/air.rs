b595e4807e6415499ce8a36c29e8f515236c6aca
Georg Wiese <georgwiese@gmail.com>
2026-05-06 23:01:22 +0200

    Fmt

--- crates/vm/src/system/poseidon2/air.rs
+++ crates/vm/src/system/poseidon2/air.rs
@@ -34,7 +34,10 @@ impl<F: Field, const SBOX_REGISTERS: usize> PartitionedBaseAir<F>
     for Poseidon2PeripheryAir<F, SBOX_REGISTERS>
 {
 }
-impl<F: Field, const SBOX_REGISTERS: usize> ColumnsAir for Poseidon2PeripheryAir<F, SBOX_REGISTERS> {}
+impl<F: Field, const SBOX_REGISTERS: usize> ColumnsAir
+    for Poseidon2PeripheryAir<F, SBOX_REGISTERS>
+{
+}
 impl<F: Field, const SBOX_REGISTERS: usize> BaseAir<F>
     for Poseidon2PeripheryAir<F, SBOX_REGISTERS>
 {
