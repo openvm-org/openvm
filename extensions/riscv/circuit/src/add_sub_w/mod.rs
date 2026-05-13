86d3b8d23b76a10eb3bf4212bffbc5ed700e8a54
GunaDD <gunadigan@gmail.com>
2026-05-13 13:45:01 -0400

    feat: add_sub_w chip to replace base_alu_w chip

---
+++ extensions/riscv/circuit/src/add_sub_w/mod.rs
@@ -0,0 +1,27 @@
+use openvm_circuit::arch::{VmAirWrapper, VmChipWrapper};
+
+use super::{
+    add_sub::{AddSubCoreAir, AddSubExecutor, AddSubFiller},
+    adapters::{
+        Rv64BaseAluWAdapterAir, Rv64BaseAluWAdapterExecutor, Rv64BaseAluWAdapterFiller,
+        RV64_CELL_BITS, RV64_WORD_NUM_LIMBS,
+    },
+};
+
+mod execution;
+
+pub type AddSubWCoreAir = AddSubCoreAir<RV64_WORD_NUM_LIMBS, RV64_CELL_BITS>;
+pub type AddSubWExecutor<A> = AddSubExecutor<A, RV64_WORD_NUM_LIMBS, RV64_CELL_BITS>;
+pub type AddSubWFiller<A> = AddSubFiller<A, RV64_WORD_NUM_LIMBS, RV64_CELL_BITS>;
+
+#[cfg(feature = "cuda")]
+mod cuda;
+#[cfg(feature = "cuda")]
+pub use cuda::*;
+
+#[cfg(test)]
+mod tests;
+
+pub type Rv64AddSubWAir = VmAirWrapper<Rv64BaseAluWAdapterAir, AddSubWCoreAir>;
+pub type Rv64AddSubWExecutor = AddSubWExecutor<Rv64BaseAluWAdapterExecutor>;
+pub type Rv64AddSubWChip<F> = VmChipWrapper<F, AddSubWFiller<Rv64BaseAluWAdapterFiller>>;
