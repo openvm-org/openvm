ecf194e751c3eec1422578778af2c987133a0848
Euicheon Hong <euicheon@intrinsictech.xyz>
2026-05-15 23:56:21 +0000

    chore: fix lint

--- crates/vm/src/arch/vm.rs
+++ crates/vm/src/arch/vm.rs
@@ -17,6 +17,8 @@ use openvm_instructions::{
     exe::{SparseMemoryImage, VmExe},
     program::Program,
 };
+#[cfg(any(debug_assertions, feature = "test-utils", feature = "stark-debug"))]
+use openvm_stark_backend::AirRef;
 use openvm_stark_backend::{
     keygen::{
         types::{MultiStarkProvingKey, MultiStarkVerifyingKey},
@@ -30,7 +32,7 @@ use openvm_stark_backend::{
         MatrixDimensions, ProverBackend, ProverDevice, ProvingContext, TraceCommitter,
     },
     verifier::VerifierError,
-    AirRef, Com, StarkEngine, StarkProtocolConfig, Val,
+    Com, StarkEngine, StarkProtocolConfig, Val,
 };
 use p3_baby_bear::BabyBear;
 #[cfg(feature = "rvr")]
