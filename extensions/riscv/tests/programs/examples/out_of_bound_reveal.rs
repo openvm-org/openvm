676725dbef65a1be28ea7e6441029f6e2e3a0e03
Ayush Shukla <ayush@axiom.xyz>
2026-06-05 15:40:12 +0200

    test(riscv): update out-of-bounds examples for rv64

--- extensions/riscv/tests/programs/examples/out_of_bound_reveal.rs
+++ extensions/riscv/tests/programs/examples/out_of_bound_reveal.rs
@@ -1,13 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_main)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use openvm::io::reveal_u32;
+use openvm::io::reveal_u64;
 
 openvm::entry!(main);
 
 pub fn main() {
     // Write past the public_values buffer.
     // Default PV size is small.
-    // reveal_u32 converts this index to a byte offset into public_values.
-    reveal_u32(0xDEADBEEF, 9999);
+    // reveal_u64 converts this index to a byte offset into public_values.
+    reveal_u64(0xDEADBEEF, 9999);
 }
