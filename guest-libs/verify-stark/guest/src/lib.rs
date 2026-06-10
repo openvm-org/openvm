1572fdd5745086bf719b12802e22f00b23ffead0
Stephen Hwang <stephenh@intrinsictech.xyz>
2026-06-10 11:08:38 -0400

    fix: clarify and use deferral output_len authentication contract

--- guest-libs/verify-stark/guest/src/lib.rs
+++ guest-libs/verify-stark/guest/src/lib.rs
@@ -16,14 +16,13 @@ pub struct ProofOutput {
 pub fn verify_stark_unchecked<const DEF_IDX: u16>(input_commit: &Commit) -> ProofOutput {
     let output_key = deferred_compute::<DEF_IDX>(input_commit);
     let output_len = output_key.output_len as usize;
+    let mut output_bytes = vec![0u8; output_len];
+    get_deferred_output::<DEF_IDX>(&mut output_bytes, &output_key);
 
     const MIN_OUTPUT_BYTES: usize = 2 * COMMIT_NUM_BYTES;
     if output_len < MIN_OUTPUT_BYTES {
         panic!("output_len too small for a ProofOutput");
     }
-
-    let mut output_bytes = vec![0u8; output_len];
-    get_deferred_output::<DEF_IDX>(&mut output_bytes, &output_key);
 
     let app_exe_commit = output_bytes[..COMMIT_NUM_BYTES].try_into().unwrap();
     let app_vm_commit = output_bytes[COMMIT_NUM_BYTES..MIN_OUTPUT_BYTES]
