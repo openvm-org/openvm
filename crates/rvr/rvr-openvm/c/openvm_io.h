2ccc405a7f25ea7c4557042791e254b8a84d75fa
Mansur Mukimbekov <57624622+mansur20478@users.noreply.github.com>
2026-06-30 17:08:48 -0400

    refactor(rvr): move ext_hint_stream_set function from RV64I extension to core rvr (#2933)
    move `ext_hint_stream_set` function from RV64I extension to core rvr.
    `ext_hint_stream_set` is also used by other extensions' phantom
    instructions, so it might be better to have it in core rvr.
    
    Changes that were done:
    1) move `host_hint_stream_set` into `crates/vm/src/arch/rvr/io` and
    `ext_hint_stream_set` into `openvm_io.c`
    2) modify `register_openvm_io_ctx ` to register `host_hint_stream_set`.
    
    resolves INT-8214

--- crates/rvr/rvr-openvm/c/openvm_io.h
+++ crates/rvr/rvr-openvm/c/openvm_io.h
@@ -1,6 +1,11 @@
 #ifndef OPENVM_IO_H
 #define OPENVM_IO_H
 
+#include <stdint.h>
+
 void* openvm_get_io_ctx(void);
+
+/* Replace the hint stream contents. Called by extension FFI staticlibs. */
+void ext_hint_stream_set(const uint8_t* data, uint32_t len);
 
 #endif /* OPENVM_IO_H */
