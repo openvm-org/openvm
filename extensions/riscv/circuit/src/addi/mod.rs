17a8418db16990399cc2a569c91af4f98eed0373
Mansur Mukimbekov <mansur20478@gmail.com>
2026-06-30 18:24:33 -0400

    chore: match add_sub structure

--- extensions/riscv/circuit/src/addi/mod.rs
+++ extensions/riscv/circuit/src/addi/mod.rs
@@ -6,13 +6,13 @@ mod core;
 mod execution;
 pub use core::*;
 
-#[cfg(test)]
-mod tests;
-
 #[cfg(feature = "cuda")]
 mod cuda;
 #[cfg(feature = "cuda")]
 pub use cuda::*;
+
+#[cfg(test)]
+mod tests;
 
 pub type Rv64AddIAir = VmAirWrapper<Rv64AddIAdapterAir, AddICoreAir<BLOCK_FE_WIDTH, U16_BITS>>;
 pub type Rv64AddIExecutor = AddIExecutor<Rv64AddIAdapterExecutor, BLOCK_FE_WIDTH, U16_BITS>;
