76a6901f7b46a8ddf81359e7a39182749bd6e467
Mansur Mukimbekov <mansur20478@gmail.com>
2026-06-30 10:05:07 -0400

    refactor(rvr): remove register_hint_stream_set_fn from the header

--- crates/rvr/rvr-openvm/c/openvm_io.h
+++ crates/rvr/rvr-openvm/c/openvm_io.h
@@ -5,9 +5,6 @@
 
 void* openvm_get_io_ctx(void);
 
-/* Called once at execute-start to install the field-typed hint-stream writer. */
-void register_hint_stream_set_fn(void (*fn)(void*, const uint8_t*, uint32_t));
-
 /* Replace the hint stream contents. Called by extension FFI staticlibs. */
 void ext_hint_stream_set(const uint8_t* data, uint32_t len);
 
