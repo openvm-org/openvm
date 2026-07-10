d20d10d946053c97e5427a503f5317bb60f95e94
Mansur Mukimbekov <mansur20478@gmail.com>
2026-07-10 10:11:40 -0400

    refactor: remove redundant code in executor

--- extensions/riscv/circuit/src/addi/execution.rs
+++ extensions/riscv/circuit/src/addi/execution.rs
@@ -66,7 +66,7 @@ where
     {
         let data: &mut AddIPreCompute = data.borrow_mut();
         self.pre_compute_impl(pc, inst, data)?;
-        Ok(execute_e1_handler::<F, Ctx, true, AddOp>)
+        Ok(execute_e1_handler::<F, Ctx>)
     }
 
     #[cfg(feature = "tco")]
@@ -81,7 +81,7 @@ where
     {
         let data: &mut AddIPreCompute = data.borrow_mut();
         self.pre_compute_impl(pc, inst, data)?;
-        Ok(execute_e1_handler::<F, Ctx, true, AddOp>)
+        Ok(execute_e1_handler::<F, Ctx>)
     }
 }
 
@@ -109,7 +109,7 @@ where
         let data: &mut E2PreCompute<AddIPreCompute> = data.borrow_mut();
         data.chip_idx = chip_idx as u32;
         self.pre_compute_impl(pc, inst, &mut data.data)?;
-        Ok(execute_e2_handler::<F, Ctx, true, AddOp>)
+        Ok(execute_e2_handler::<F, Ctx>)
     }
 
     #[cfg(feature = "tco")]
@@ -126,20 +126,19 @@ where
         let data: &mut E2PreCompute<AddIPreCompute> = data.borrow_mut();
         data.chip_idx = chip_idx as u32;
         self.pre_compute_impl(pc, inst, &mut data.data)?;
-        Ok(execute_e2_handler::<F, Ctx, true, AddOp>)
+        Ok(execute_e2_handler::<F, Ctx>)
     }
 }
 
 #[inline(always)]
-unsafe fn execute_e12_impl<F: PrimeField32, CTX: ExecutionCtxTrait, OP: AluOp>(
+unsafe fn execute_e12_impl<F: PrimeField32, CTX: ExecutionCtxTrait>(
     pre_compute: &AddIPreCompute,
     exec_state: &mut VmExecState<F, GuestMemory, CTX>,
 ) {
-    let rs1 = exec_state.vm_read_bytes::<8>(RV64_REGISTER_AS, pre_compute.b as u32);
-    let rs2: [u8; 8] = pre_compute.c.to_le_bytes();
-    let rs1 = u64::from_le_bytes(rs1);
-    let rs2 = u64::from_le_bytes(rs2);
-    let rd = <OP as AluOp>::compute(rs1, rs2);
+    let rs1 =
+        u64::from_le_bytes(exec_state.vm_read_bytes::<8>(RV64_REGISTER_AS, pre_compute.b as u32));
+    let rs2 = pre_compute.c;
+    let rd = rs1.wrapping_add(rs2);
     exec_state.vm_write_bytes::<8>(RV64_REGISTER_AS, pre_compute.a as u32, &rd.to_le_bytes());
     let pc = exec_state.pc();
     exec_state.set_pc(pc.wrapping_add(DEFAULT_PC_STEP));
@@ -147,28 +146,18 @@ unsafe fn execute_e12_impl<F: PrimeField32, CTX: ExecutionCtxTrait, OP: AluOp>(
 
 #[create_handler]
 #[inline(always)]
-unsafe fn execute_e1_impl<
-    F: PrimeField32,
-    CTX: ExecutionCtxTrait,
-    const IS_IMM: bool,
-    OP: AluOp,
->(
+unsafe fn execute_e1_impl<F: PrimeField32, CTX: ExecutionCtxTrait>(
     pre_compute: *const u8,
     exec_state: &mut VmExecState<F, GuestMemory, CTX>,
 ) {
     let pre_compute: &AddIPreCompute =
         std::slice::from_raw_parts(pre_compute, size_of::<AddIPreCompute>()).borrow();
-    execute_e12_impl::<F, CTX, OP>(pre_compute, exec_state);
+    execute_e12_impl::<F, CTX>(pre_compute, exec_state);
 }
 
 #[create_handler]
 #[inline(always)]
-unsafe fn execute_e2_impl<
-    F: PrimeField32,
-    CTX: MeteredExecutionCtxTrait,
-    const IS_IMM: bool,
-    OP: AluOp,
->(
+unsafe fn execute_e2_impl<F: PrimeField32, CTX: MeteredExecutionCtxTrait>(
     pre_compute: *const u8,
     exec_state: &mut VmExecState<F, GuestMemory, CTX>,
 ) {
@@ -177,16 +166,5 @@ unsafe fn execute_e2_impl<
     exec_state
         .ctx
         .on_height_change(pre_compute.chip_idx as usize, 1);
-    execute_e12_impl::<F, CTX, OP>(&pre_compute.data, exec_state);
-}
-
-trait AluOp {
-    fn compute(rs1: u64, rs2: u64) -> u64;
-}
-struct AddOp;
-impl AluOp for AddOp {
-    #[inline(always)]
-    fn compute(rs1: u64, rs2: u64) -> u64 {
-        rs1.wrapping_add(rs2)
-    }
+    execute_e12_impl::<F, CTX>(&pre_compute.data, exec_state);
 }
