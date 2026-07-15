11055e8a7261136140aa8f8d067660e24e83dcf4
Ayush Shukla <ayush@axiom.xyz>
2026-07-15 16:31:30 +0200

    fix(riscv): adapt immediate executors to execution API

--- extensions/riscv/circuit/src/bitwise_logic_imm/core.rs
+++ extensions/riscv/circuit/src/bitwise_logic_imm/core.rs
@@ -190,7 +190,7 @@ where
 
     fn execute(
         &self,
-        state: VmStateMut<F, TracingMemory, RA>,
+        state: VmStateMut<TracingMemory, RA>,
         instruction: &Instruction<F>,
     ) -> Result<(), ExecutionError> {
         let Instruction { opcode, c, .. } = instruction;
--- extensions/riscv/circuit/src/shift_logical_imm/core.rs
+++ extensions/riscv/circuit/src/shift_logical_imm/core.rs
@@ -265,7 +265,7 @@ where
 
     fn execute(
         &self,
-        state: VmStateMut<F, TracingMemory, RA>,
+        state: VmStateMut<TracingMemory, RA>,
         instruction: &Instruction<F>,
     ) -> Result<(), ExecutionError> {
         let Instruction { opcode, c, .. } = instruction;
