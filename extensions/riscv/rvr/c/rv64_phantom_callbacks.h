48c9cf088319904824423902d16e68895b6074d6
Ayush Shukla <ayush@axiom.xyz>
2026-07-23 10:35:03 +0200

    feat(rvr): preflight callback phantoms

--- extensions/riscv/rvr/c/rv64_phantom_callbacks.h
+++ extensions/riscv/rvr/c/rv64_phantom_callbacks.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 typedef struct {
-  void (*hint_input)(void* ctx);
+  bool (*hint_input)(void* ctx);
   bool (*print_str)(void* ctx, uint64_t ptr, uint64_t len);
   bool (*hint_random)(void* ctx, uint64_t num_words);
 } Rv64PhantomHostCallbacks;
@@ -15,7 +15,7 @@ void register_rv64_phantom_host_callbacks(const Rv64PhantomHostCallbacks* cb);
 
 /* Forwarding stubs for the RV64 HINT_INPUT, PRINT_STR, and HINT_RANDOM
  * phantoms. A runtime hook installs the thread-local callback table. */
-void openvm_hint_input(void);
+bool openvm_hint_input(void);
 bool openvm_print_str(uint64_t ptr, uint64_t len);
 bool openvm_hint_random(uint64_t num_words);
 
